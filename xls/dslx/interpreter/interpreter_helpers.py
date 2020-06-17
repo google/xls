aaf72bb523d17587733219637890707cbd8c27ab
Hans Montero <hjm2133@columbia.edu>
2020-06-17 14:22:14 -0400

    Replace SimpleInterpreter with a callback function

--- xls/dslx/interpreter/interpreter_helpers.py
+++ xls/dslx/interpreter/interpreter_helpers.py
@@ -16,29 +16,18 @@
 
 """Helper utilities for use with the interpreter.Interpreter."""
 
+from xls.dslx import ast
+from xls.dslx import deduce
 from xls.dslx.interpreter import interpreter
 
-class SimpleInterpreter(object):
-  """Wrapper class for Interpreter that exposes basic functionality.
+def interpret_expr(module: ast.Module, node_to_type: deduce.NodeToType,
+                   env: Sequence[[str, interpreter.Value]],
+                   expr: ast.Expr) -> int:
+  """Creates an Interpreter on-the-fly to evaluate expr with env"""
+  interp = interpreter.Interpreter(module, node_to_type, f_import=None)
+  bindings = interpreter.Bindings()
+  for ident, val in env:
+    bindings.add_value(ident, interpreter.Value.make_ubits(value=val,
+                                                           bit_count=32))
+  return interp.evaluate_expr(expr, bindings).bits_payload.value
 
-  Attributes:
-    interp: Interpreter object
-    bindings: Bindings object to use with the interpreter (e.g. in expr evaluation)
-  """
-
-  def __init__(self, module, node_to_type):
-    self.interp = interpreter.Interpreter(module, node_to_type, f_import=None)
-    self.bindings = interpreter.Bindings()
-
-  def add_binding(self, ident, val):
-    """Adds {ident: (val as u32)} to this instance's Bindings"""
-    self.bindings.add_value(ident, interpreter.Value.make_ubits(value=val,
-                                                                bit_count=32))
-
-  def evaluate(self, expr) -> int:
-    """Evaluates expr using current Bindings and returns resulting bit value"""
-    return self.interp.evaluate_expr(expr, self.bindings).bits_payload.value
-
-def make_interpreter(mod, node_to_type) -> SimpleInterpreter:
-  """Return a new SimpleInterpreter. To be used as a callback function."""
-  return SimpleInterpreter(mod, node_to_type)
