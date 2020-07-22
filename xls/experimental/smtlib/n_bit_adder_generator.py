fa3f67d72f9042f67fef6b4eba2351d866f5d285
Julian Viera <julianviera@google.com>
2020-07-22 18:50:33 +0000

    Changed to f-strings and argv use in n_bit_adder_generator.py, removed files outside smtlib directory.

--- xls/experimental/smtlib/n_bit_adder_generator.py
+++ xls/experimental/smtlib/n_bit_adder_generator.py
@@ -1,9 +1,12 @@
 """
-This file receives a number n from the user and creates an smt2 file. The smt2 file
-produced contains an n-bit adder, and checks the adder's equivalence with SMT-LIB's 
-builtin addition. Instructions for using CVC4 can be found in cvc4.md. Once an smt2
-file
-is created, we can run:
+This file receives a number n from the user in argv and creates an smt2 file. When 
+we run:
+
+$ python3 n_bit_adder_generator.py <n>
+
+the smt2 file produced contains an n-bit adder that checks the adder's equivalence 
+with SMT-LIB's builtin addition. Instructions for using CVC4 can be found in cvc4.md. 
+Once an smt2 file is created, we can run:
 
 $ cvc4 <filename>
 
@@ -16,59 +19,68 @@ meaning the adder and the builtin addition never produce different results. They
 logically equivalent. 
 """
 
+import sys
+
 def description_comments(n, f):
-  f.write("; The following SMT-LIB verifies that a {bits}-bit adder is equivalent\n; to CVC4's built in bit-vector addition.\n\n""".format(bits = n))
+  print(
+f"""; The following SMT-LIB verifies that a {n}-bit adder is equivalent
+; to CVC4's built in bit-vector addition.
+""", file=f)
 
 def logic_and_variables(n, f):
-  f.write("(set-logic ALL)\n\n")
-  f.write("; Declare bit-vectors and proxies for indices\n")
-  f.write("(declare-fun x () (_ BitVec {bits}))\n".format(bits = n))
-  f.write("(declare-fun y () (_ BitVec {bits}))\n".format(bits = n))
+  print(
+f"""(set-logic ALL)
+
+; Declare bit-vectors and proxies for indices
+(declare-fun x () (_ BitVec {n}))
+(declare-fun y () (_ BitVec {n}))""", file=f)
   for i in range(n):
     for var in ["x", "y"]:
-      f.write("(define-fun {v}{b} () (_ BitVec 1) ((_ extract {b} {b}) {v}))\n".format(v = var, b = i))
-  f.write("\n")
+      print(f"(define-fun {var}{i} () (_ BitVec 1) ((_ extract {i} {i}) {var}))", file=f)
+  print("", file=f)
 
 def half_adder(n, f):
-  f.write("; Half adder for bit {b}\n".format(b = n))
-  f.write("(define-fun s{b} () (_ BitVec 1) (bvxor x{b} y{b}))\n".format(b = n))
-  f.write("(define-fun c{b} () (_ BitVec 1) (bvand x{b} y{b}))\n\n".format(b = n))
+  print(
+f"""; Half adder for bit {n}
+(define-fun s{n} () (_ BitVec 1) (bvxor x{n} y{n}))
+(define-fun c{n} () (_ BitVec 1) (bvand x{n} y{n}))
+""", file=f)
 
 def full_adder(n, f):
-  f.write("; Full adder for bit {b}\n".format(b = n))
-  f.write("(define-fun s{b} () (_ BitVec 1) (bvxor c{b_1} (bvxor x{b} y{b})))\n".format(b = n, b_1 = n - 1))
-  f.write("(define-fun c{b} () (_ BitVec 1) (bvor (bvand (bvxor x{b} y{b}) c{b_1}) (bvand x{b} y{b})))\n\n".format(b = n, b_1 = n - 1))
+  print(
+f"""; Full adder for bit {n}
+(define-fun s{n} () (_ BitVec 1) (bvxor c{n - 1} (bvxor x{n} y{n})))
+(define-fun c{n} () (_ BitVec 1) (bvor (bvand (bvxor x{n} y{n}) c{n - 1}) (bvand x{n} y{n})))
+""", file=f)
 
 def get_result_bits(n):
-  string = ""
+  bits = []
   for i in range(n-1, -1, -1):
-    string += "s{} ".format(i)
-  return string[:-1]
+    bits.append(f"s{i}")
+  return " ".join(bits)
 
 def make_sum(n, f):
-  f.write("; Concatenate s bits to create sum\n")
-  f.write("(define-fun sum () (_ BitVec {bits}) (concat {s}))\n\n".format(bits = n, s = get_result_bits(n)))
+  print(
+f"""; Concatenate s bits to create sum
+(define-fun sum () (_ BitVec {n}) (concat {get_result_bits(n)}))
+""", file=f)
 
 def assert_and_check_sat(n, f):
-  f.write("; Compare {bits}-bit adder result and internal addition and solve\n".format(bits = n))
-  f.write("(assert (not (= sum (bvadd x y))))\n")
-  f.write("(check-sat)")
+  print(
+f"""; Compare {n}-bit adder result and internal addition and solve
+(assert (not (= sum (bvadd x y))))
+(check-sat)""", file=f)
 
 def n_bit_adder(n):
-  f = open("adder_2x{}.smt2".format(n), "w")
-  description_comments(n, f)
-  logic_and_variables(n, f)
-  half_adder(0, f)
-  for i in range(1, n):
-    full_adder(i, f)
-  make_sum(n, f)
-  assert_and_check_sat(n, f)
-  f.close()
-
-def main(argv):
-  if len(argv) > 1:
-    raise app.UsageError('Too many command-line arguments.')
+  with open(f"adder_2x{n}.smt2", "w") as f:
+    description_comments(n, f)
+    logic_and_variables(n, f)
+    half_adder(0, f)
+    for i in range(1, n):
+      full_adder(i, f)
+    make_sum(n, f)
+    assert_and_check_sat(n, f)
 
 if __name__ == '__main__':
-  n = input("Number of bits for each adder input: ")
-  n_bit_adder(int(n))
+  n_bit_adder(int(sys.argv[1]))
+    
