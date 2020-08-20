1b6859dc384fe8fa39fb901af3de8453661ff345
Mark Heffernan <meheff@google.com>
2020-08-20 13:01:27 -0700

    Add utility for binary searching for max frequency of a Verilog module.
    PiperOrigin-RevId: 327682385

---
+++ xls/synthesis/synthesis_utils.py
@@ -0,0 +1,115 @@
+# Lint as: python3
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Utility functions used in synthesis."""
+
+from typing import Sequence, Callable, TypeVar, Optional
+
+from absl import logging
+
+import grpc
+
+from xls.synthesis import synthesis_pb2
+from xls.synthesis import synthesis_service_pb2_grpc
+
+T = TypeVar('T')
+U = TypeVar('U')
+
+
+def run_bisect(start_index: int, limit_index: int, considered: Sequence[T],
+               frun: Callable[[T], U],
+               fresult: Callable[[T, U], bool]) -> Optional[int]:
+  """Performs bisection on 'considered', using 'frun'/'fresult'.
+
+  Args:
+    start_index: Starting index to use in 'considered'.
+    limit_index: Limit index to use in 'considered'
+    considered: Ordered space of inputs to consider running.
+    frun: Runs a value from 'considered' to produce a result.
+    fresult: Processes a result to say whether it is accepted.
+
+  Returns:
+    The highest index of the 'considered' which is accepted. Returns None
+    if no elements are accepted.
+  """
+  max_ok_index = None
+  while start_index != limit_index:
+    index = start_index + (limit_index - start_index) // 2
+    t = considered[index]
+    u = frun(t)
+    ok = fresult(t, u)
+    if ok:
+      start_index = index + 1
+      max_ok_index = index if max_ok_index is None else max(max_ok_index, index)
+    else:
+      limit_index = index
+  return max_ok_index
+
+
+def bisect_frequency(
+    verilog_text: str, top_module_name: str, start_hz: int, limit_hz: int,
+    step_hz: int,
+    grpc_channel: grpc.Channel) -> synthesis_pb2.SynthesisSweepResult:
+  """Binary searches to determine maximum frequency of a given Verilog module.
+
+  Args:
+    verilog_text: Text of the Verilog.
+    top_module_name: The name of the top module to synthesize.
+    start_hz: Lowest frequency (inclusive) to search.
+    limit_hz: Highest frequency (inclusive) to search.
+    step_hz: The minimum frequency step to use when searching.
+    grpc_channel: A channel to the SynthesisService GRPC service to use for
+      synthesizing the verilog.
+
+  Returns:
+    A SynthesisSweepResult containing the result of the sweep (max frequency and
+    CompileResponses).
+  """
+  sweep_result = synthesis_pb2.SynthesisSweepResult()
+
+  def run_sample(target_hz: int) -> synthesis_pb2.CompileResponse:
+    logging.info(
+        'Running with target frequency %0.3fGHz. Range: [%0.3fGHz, %0.3fGHz]',
+        target_hz / 1e9, start_hz / 1e9, limit_hz / 1e9)
+    grpc.channel_ready_future(grpc_channel).result()
+    stub = synthesis_service_pb2_grpc.SynthesisServiceStub(grpc_channel)
+    request = synthesis_pb2.CompileRequest()
+    request.module_text = verilog_text
+    request.top_module_name = top_module_name
+    request.target_frequency_hz = target_hz
+
+    response = stub.Compile(request)
+
+    synthesis_result = sweep_result.results.add()
+    synthesis_result.target_frequency_hz = target_hz
+    synthesis_result.response.CopyFrom(response)
+
+    if response.slack_ps >= 0:
+      logging.info('  PASSED TIMING')
+      sweep_result.max_frequency_hz = max(sweep_result.max_frequency_hz,
+                                          target_hz)
+    else:
+      logging.info('  FAILED TIMING (slack %dps)', response.slack_ps)
+    return response
+
+  def sample_meets_timing(target_hz: int,
+                          response: synthesis_pb2.CompileResponse) -> bool:
+    del target_hz
+    return response.slack_ps >= 0
+
+  frequencies = list(range(start_hz, limit_hz, step_hz))
+  run_bisect(0,
+             len(frequencies) - 1, frequencies, run_sample, sample_meets_timing)
+  return sweep_result
---
+++ xls/synthesis/synthesis_utils_test.py
@@ -0,0 +1,119 @@
+# Lint as: python3
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for xls.synthesis.synthesis_utils."""
+
+import subprocess
+
+from typing import Sequence, Tuple, List, Any, Optional
+
+import grpc
+import portpicker
+
+from absl.testing import absltest
+from xls.common import runfiles
+from xls.synthesis import client_credentials
+from xls.synthesis import synthesis_utils
+
+SERVER_PATH = runfiles.get_path('xls/synthesis/dummy_synthesis_server_main')
+
+
+class SynthesisUtilsTest(absltest.TestCase):
+
+  def _start_server(self, args):
+    port = portpicker.pick_unused_port()
+    proc = subprocess.Popen([runfiles.get_path(SERVER_PATH), f'--port={port}'] +
+                            args)
+    return port, proc
+
+  def _run_bisect(self,
+                  seq: Sequence[Any],
+                  frun=None) -> Tuple[Optional[int], List[Tuple[Any, Any]]]:
+    frun = frun or (lambda x: -1.0 if x >= 29 else 1.0)
+    results = []
+
+    def fresult(i: int, x: float) -> bool:
+      results.append((i, x))
+      return x >= 0.0
+
+    index = synthesis_utils.run_bisect(0, len(seq), seq, frun, fresult)
+    return index, results
+
+  def test_bisect_1_to_3_ghz_by_100mhz(self):
+    seq = [10 + i for i in range(21)]
+    max_index, results = self._run_bisect(seq)
+    self.assertEqual(seq[max_index], 28)
+    self.assertEqual(results, [(20, 1.0), (26, 1.0), (29, -1.0), (28, 1.0)])
+
+  def test_bisect_2_to_3_ghz_by_100mhz(self):
+    seq = [20 + i for i in range(11)]
+    max_index, results = self._run_bisect(seq)
+    self.assertEqual(seq[max_index], 28)
+    self.assertEqual(results, [(25, 1.0), (28, 1.0), (30, -1.0), (29, -1.0)])
+
+  def test_bisect_3_to_4_ghz_by_100mhz_does_not_meet_timing(self):
+    seq = [30 + i for i in range(11)]
+    max_index, results = self._run_bisect(seq)
+    self.assertIsNone(max_index)
+    self.assertEqual(results, [(35, -1.0), (32, -1.0), (31, -1.0), (30, -1.0)])
+
+  def test_bisect_floats(self):
+    seq = list(
+        x / 100.0
+        for x in range(int(1.0 * 100),
+                       int(3.0 * 100) + 1, int(0.1 * 100)))
+    frun = lambda x: -1.0 if x >= 2.9 else 0
+    max_index, results = self._run_bisect(seq, frun=frun)
+    self.assertAlmostEqual(seq[max_index], 2.8)
+    self.assertEqual(results, [(2.0, 0), (2.6, 0), (2.9, -1.0), (2.8, 0)])
+
+  def test_bisect_frequencies(self):
+    port, proc = self._start_server(['--max_frequency_ghz=2.0'])
+
+    channel_creds = client_credentials.get_credentials()
+    with grpc.secure_channel(f'localhost:{port}', channel_creds) as channel:
+      result = synthesis_utils.bisect_frequency('verilog', 'main', int(1.5e9),
+                                                int(3e9), int(0.1e9), channel)
+    self.assertEqual(result.max_frequency_hz, int(2e9))
+    self.assertLen(result.results, 4)
+    proc.terminate()
+    proc.wait()
+
+  def test_bisect_frequencies_infeasible(self):
+    port, proc = self._start_server(['--max_frequency_ghz=2.0'])
+
+    channel_creds = client_credentials.get_credentials()
+    with grpc.secure_channel(f'localhost:{port}', channel_creds) as channel:
+      result = synthesis_utils.bisect_frequency('verilog', 'main', int(3e9),
+                                                int(4e9), int(0.1e9), channel)
+    self.assertEqual(result.max_frequency_hz, 0)
+    proc.terminate()
+    proc.wait()
+
+  def test_bisect_frequencies_with_error(self):
+    port, proc = self._start_server(
+        ['--max_frequency_ghz=2.0', '--serve_errors'])
+
+    channel_creds = client_credentials.get_credentials()
+    with grpc.secure_channel(f'localhost:{port}', channel_creds) as channel:
+      with self.assertRaises(grpc.RpcError):
+        _ = synthesis_utils.bisect_frequency('verilog', 'main', int(1.5e9),
+                                             int(3e9), int(0.1e9), channel)
+    proc.terminate()
+    proc.wait()
+
+
+if __name__ == '__main__':
+  absltest.main()
