ac4693e828dbddee7af3e95100be7f4186435f3a
Johan Euphrosine <proppy@google.com>
2021-08-12 12:10:18 +0900

    [public/python] add argument name

--- xls/public/python/runtime_build_actions.cc
+++ xls/public/python/runtime_build_actions.cc
@@ -27,23 +27,18 @@ PYBIND11_MODULE(runtime_build_actions, m) {
 
   m.def("convert_dslx_to_ir", [](absl::string_view dslx, absl::string_view path, absl::string_view package, const std::vector<absl::string_view>& additional_search_paths) -> absl::StatusOr<std::string> {
       return ConvertDslxToIr(dslx, path, package, std::vector<std::filesystem::path>(additional_search_paths.begin(), additional_search_paths.end()));
-    });
+    }, py::arg("dslx"), py::arg("path"), py::arg("package"), py::arg("additional_search_paths"));
 
   m.def("convert_dslx_path_to_ir", [](absl::string_view path, const std::vector<absl::string_view>& additional_search_paths) -> absl::StatusOr<std::string> {
       return ConvertDslxPathToIr(path, std::vector<std::filesystem::path>(additional_search_paths.begin(), additional_search_paths.end()));
-    });
+    }, py::arg("path"), py::arg("additional_search_paths"));
 
-  m.def("optimize_ir", [](absl::string_view ir, absl::string_view entry) -> absl::StatusOr<std::string> {
-      return OptimizeIr(ir, entry);
-    });
+  m.def("optimize_ir", &OptimizeIr, py::arg("ir"), py::arg("entry"));
 
-  m.def("mangle_dslx_name", [](absl::string_view module_name, absl::string_view function_name) -> absl::StatusOr<std::string> {
-      return MangleDslxName(module_name, function_name);
-    });
+  m.def("mangle_dslx_name", &MangleDslxName, py::arg("module_name"), py::arg("function_name"));
 
-  m.def("proto_to_dslx", [](absl::string_view proto_def, absl::string_view message_name, absl::string_view text_proto, absl::string_view binding_name) -> absl::StatusOr<std::string> {
-      return ProtoToDslx(proto_def, message_name, text_proto, binding_name);
-    });
+  m.def("proto_to_dslx", &ProtoToDslx, py::arg("proto_def"), py::arg("message_name"), py::arg("text_proto"), py::arg("binding_name"));
+
 }
 
 }  // namespace xls
