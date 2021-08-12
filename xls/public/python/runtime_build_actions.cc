69f2b5817c7fe06e5f221b489ccc49459855dd6d
Johan Euphrosine <proppy@google.com>
2021-08-12 12:27:00 +0900

    [public/python] add docstrings

--- xls/public/python/runtime_build_actions.cc
+++ xls/public/python/runtime_build_actions.cc
@@ -27,17 +27,42 @@ PYBIND11_MODULE(runtime_build_actions, m) {
 
   m.def("convert_dslx_to_ir", [](absl::string_view dslx, absl::string_view path, absl::string_view package, const std::vector<absl::string_view>& additional_search_paths) -> absl::StatusOr<std::string> {
       return ConvertDslxToIr(dslx, path, package, std::vector<std::filesystem::path>(additional_search_paths.begin(), additional_search_paths.end()));
-    }, py::arg("dslx"), py::arg("path"), py::arg("package"), py::arg("additional_search_paths"));
+    }, R"(Converts the specified DSLX text into XLS IR text.
+
+Args:
+ dslx: DSL (module) text to convert to IR.
+ path: Path to use for source location information for the given text.
+   Since text may be generated, an empty string or a pseudo path like
+   "<generated>" is acceptable.
+ module_name: Name of the DSL module, will be used in the name of the
+   converted IR package text.
+ additional_search_paths: Additional filesystem paths to search for imported
+   modules.)", py::arg("dslx"), py::arg("path"), py::arg("package"), py::arg("additional_search_paths"));
 
   m.def("convert_dslx_path_to_ir", [](absl::string_view path, const std::vector<absl::string_view>& additional_search_paths) -> absl::StatusOr<std::string> {
       return ConvertDslxPathToIr(path, std::vector<std::filesystem::path>(additional_search_paths.begin(), additional_search_paths.end()));
-    }, py::arg("path"), py::arg("additional_search_paths"));
+    }, R"(As convert_dslx_to_ir, but uses a filesystem path to retrieve the DSLX module contents.
+"path" should end with ".x" suffix, the path will determine the module name.)", py::arg("path"), py::arg("additional_search_paths"));
 
-  m.def("optimize_ir", &OptimizeIr, py::arg("ir"), py::arg("entry"));
+  m.def("optimize_ir", &OptimizeIr, R"(Optimizes the generated XLS IR with the given entry point (which should be a
+function present inside the IR text).)", py::arg("ir"), py::arg("entry"));
 
-  m.def("mangle_dslx_name", &MangleDslxName, py::arg("module_name"), py::arg("function_name"));
+  m.def("mangle_dslx_name", &MangleDslxName, R"(Mangles the given DSL module/function name combination so it can be resolved
+as a corresponding symbol in converted IR.)", py::arg("module_name"), py::arg("function_name"));
 
-  m.def("proto_to_dslx", &ProtoToDslx, py::arg("proto_def"), py::arg("message_name"), py::arg("text_proto"), py::arg("binding_name"));
+  m.def("proto_to_dslx", &ProtoToDslx, R"(Converts protocol buffer data into its equivalent DSLX text, as a
+module-level constant.
+
+Note: this currently only supports "standalone" protobuf schemas; i.e. this
+cannot translate schemas that import other `.proto` files. If this limitation
+affects you, please file an issue at `github.com/google/xls/issues`
+
+Args:
+ proto_def: Protobuf schema (e.g. contents of `.proto` file).
+ message_name: Name of the message type (inside the protobuf schema) to emit.
+ text_proto: Protobuf text to translate to DSLX.
+ binding_name: Name of the (const) DSLX binding (i.e. const variable name) to
+   make in the output text.)", py::arg("proto_def"), py::arg("message_name"), py::arg("text_proto"), py::arg("binding_name"));
 
 }
 
