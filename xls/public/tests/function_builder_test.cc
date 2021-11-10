fcd58cfde7aa9bad7b9669361961c2a7a945615a
Chris Leary <leary@google.com>
2021-11-10 11:30:42 -0800

    [public] Add a test that reflects public API consumers.
    This test directory is not considered part of internal XLS and thus has the
    same access perms as a "public" user in the repo, so it reflects the headers
    and access capabilities available via the public APIs and tests them for
    functional usability.
    
    Since we'd expect most functionality to be covered via internal tests, this
    will likely just hold some key smoke tests that functionality is accessible and
    demos of how it can be used from a "public" user location.
    
    PiperOrigin-RevId: 408932946

---
+++ xls/public/tests/function_builder_test.cc
@@ -0,0 +1,40 @@
+// Copyright 2021 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/public/function_builder.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "xls/public/status_matchers.h"
+
+namespace {
+
+TEST(FunctionBuilderTest, SimpleFunctionToIrText) {
+  xls::Package package("test_package");
+  xls::FunctionBuilder builder("f", &package);
+  xls::BitsType* b32 = package.GetBitsType(32);
+  xls::BValue x = builder.Param("x", b32);
+  xls::BValue y = builder.Param("y", b32);
+  builder.Add(x, y);
+  XLS_ASSERT_OK_AND_ASSIGN(xls::Function * f, builder.Build());
+  ASSERT_NE(f, nullptr);
+  EXPECT_EQ(package.DumpIr(), R"(package test_package
+
+fn f(x: bits[32], y: bits[32]) -> bits[32] {
+  ret add.3: bits[32] = add(x, y, id=3)
+}
+)");
+}
+
+}  // namespace
