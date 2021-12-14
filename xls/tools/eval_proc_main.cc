779e30f57ff6abc5f32fb014036758aee007e771
Sean Purser-Haskell <seanhaskell@google.com>
2021-12-14 12:20:43 -0800

    Add IO and test to eval_proc_main
    PiperOrigin-RevId: 416365507

--- xls/tools/eval_proc_main.cc
+++ xls/tools/eval_proc_main.cc
@@ -14,12 +14,16 @@
 
 // Tool to evaluate the behavior of a Proc network.
 
+#include <cstdint>
 #include <iostream>
 #include <vector>
 
+#include "absl/container/flat_hash_map.h"
 #include "absl/flags/flag.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/match.h"
+#include "absl/strings/str_split.h"
 #include "absl/strings/string_view.h"
 #include "xls/common/file/filesystem.h"
 #include "xls/common/init_xls.h"
@@ -27,7 +31,9 @@
 #include "xls/common/status/status_macros.h"
 #include "xls/interpreter/channel_queue.h"
 #include "xls/interpreter/proc_network_interpreter.h"
+#include "xls/ir/bits.h"
 #include "xls/ir/ir_parser.h"
+#include "xls/jit/jit_channel_queue.h"
 #include "xls/jit/serial_proc_runtime.h"
 
 constexpr const char* kUsage = R"(
@@ -43,12 +49,35 @@ ABSL_FLAG(std::string, backend, "serial_jit",
           "Backend to use for evaluation. Valid options are:\n"
           " - serial_jit : JIT-backed single-stepping runtime.\n"
           " - ir_interpreter     : Interpreter at the IR level.");
+ABSL_FLAG(
+    std::vector<std::string>, inputs_for_channels, {},
+    "Comma separated list of channel=filename pairs, for example: ch_a=foo.ir. "
+    "Files contain one XLS Value in human-readable form per line.");
+ABSL_FLAG(
+    std::vector<std::string>, expected_outputs_for_channels, {},
+    "Comma separated list of channel=filename pairs, for example: ch_a=foo.ir. "
+    "Files contain one XLS Value in human-readable form per line");
 
 namespace xls {
 
-absl::Status RunIrInterpreter(Package* package, int64_t ticks) {
+absl::Status RunIrInterpreter(
+    Package* package, int64_t ticks,
+    absl::flat_hash_map<std::string, std::vector<Value>> inputs_for_channels,
+    absl::flat_hash_map<std::string, std::vector<Value>>
+        expected_outputs_for_channels) {
   XLS_ASSIGN_OR_RETURN(auto interpreter,
                        ProcNetworkInterpreter::Create(package, {}));
+
+  ChannelQueueManager& queue_manager = interpreter->queue_manager();
+  for (const auto& [channel_name, values] : inputs_for_channels) {
+    XLS_ASSIGN_OR_RETURN(ChannelQueue * in_queue,
+                         queue_manager.GetQueueByName(channel_name));
+    for (const Value& value : values) {
+      XLS_RETURN_IF_ERROR(in_queue->Enqueue(value));
+    }
+  }
+
+  XLS_CHECK_GT(ticks, 0);
   for (int i = 0; i < ticks; i++) {
     XLS_RETURN_IF_ERROR(interpreter->Tick());
   }
@@ -66,11 +95,35 @@ absl::Status RunIrInterpreter(Package* package, int64_t ticks) {
     std::cout << "Proc " << proc->name() << " : " << states.at(proc)
               << std::endl;
   }
+
+  for (const auto& [channel_name, values] : expected_outputs_for_channels) {
+    XLS_ASSIGN_OR_RETURN(ChannelQueue * out_queue,
+                         queue_manager.GetQueueByName(channel_name));
+    for (const Value& value : values) {
+      XLS_ASSIGN_OR_RETURN(Value out_val, out_queue->Dequeue());
+      XLS_RET_CHECK_EQ(value, out_val);
+    }
+  }
+
   return absl::OkStatus();
 }
 
-absl::Status RunSerialJit(Package* package, int64_t ticks) {
-  XLS_ASSIGN_OR_RETURN(auto runtime, SerialProcRuntime::Create(package));
+absl::Status RunSerialJit(
+    Package* package, int64_t ticks,
+    absl::flat_hash_map<std::string, std::vector<Value>> inputs_for_channels,
+    absl::flat_hash_map<std::string, std::vector<Value>>
+        expected_outputs_for_channels) {
+  XLS_ASSIGN_OR_RETURN(std::unique_ptr<SerialProcRuntime> runtime,
+                       SerialProcRuntime::Create(package));
+
+  for (const auto& [channel_name, values] : inputs_for_channels) {
+    XLS_ASSIGN_OR_RETURN(Channel * in_ch, package->GetChannel(channel_name));
+    for (const Value& value : values) {
+      XLS_RETURN_IF_ERROR(runtime->EnqueueValueToChannel(in_ch, value));
+    }
+  }
+
+  XLS_CHECK_GT(ticks, 0);
   // If Tick() semantics change such that it returns once all Procs have run
   // _at_all_ (instead of only returning when all procs have fully completed),
   // then number-of-ticks-based timing won't work and we'll need to run based on
@@ -84,18 +137,84 @@ absl::Status RunSerialJit(Package* package, int64_t ticks) {
     XLS_ASSIGN_OR_RETURN(Value v, runtime->ProcState(i));
     std::cout << "Proc " << p->name() << " : " << v << std::endl;
   }
-  return absl::OkStatus();
+
+  bool checked_any_output = false;
+
+  for (const auto& [channel_name, values] : expected_outputs_for_channels) {
+    XLS_ASSIGN_OR_RETURN(Channel * out_ch, package->GetChannel(channel_name));
+    for (const Value& value : values) {
+      XLS_ASSIGN_OR_RETURN(Value out_val,
+                           runtime->DequeueValueFromChannel(out_ch));
+      XLS_RET_CHECK_EQ(value, out_val);
+      checked_any_output = true;
+    }
+  }
+
+  return checked_any_output
+             ? absl::OkStatus()
+             : absl::UnknownError(
+                   "No output verified (empty expected values?)");
 }
 
-absl::Status RealMain(absl::string_view ir_file, absl::string_view backend,
-                      int64_t ticks) {
+absl::StatusOr<std::vector<Value>> ParseValuesFile(std::string_view filename) {
+  XLS_ASSIGN_OR_RETURN(std::string contents, GetFileContents(filename));
+  std::vector<Value> ret;
+  for (const auto& line :
+       absl::StrSplit(contents, '\n', absl::SkipWhitespace())) {
+    XLS_ASSIGN_OR_RETURN(Value expected_status, Parser::ParseTypedValue(line));
+    ret.push_back(expected_status);
+  }
+  return ret;
+}
+
+absl::StatusOr<absl::flat_hash_map<std::string, std::string>>
+ParseChannelFilenames(std::vector<std::string> files_raw) {
+  absl::flat_hash_map<std::string, std::string> ret;
+  for (const std::string& file : files_raw) {
+    std::vector<std::string> split = absl::StrSplit(file, '=');
+    if (split.size() != 2) {
+      return absl::InvalidArgumentError(
+          "Format of argument should be channel=file");
+    }
+
+    ret[split[0]] = split[1];
+  }
+  return ret;
+}
+
+absl::Status RealMain(
+    absl::string_view ir_file, absl::string_view backend, int64_t ticks,
+    std::vector<std::string> inputs_for_channels_text,
+    std::vector<std::string> expected_outputs_for_channels_text) {
   XLS_ASSIGN_OR_RETURN(std::string ir_text, GetFileContents(ir_file));
   XLS_ASSIGN_OR_RETURN(auto package, Parser::ParsePackage(ir_text));
 
+  absl::flat_hash_map<std::string, std::string> input_filenames;
+  XLS_ASSIGN_OR_RETURN(input_filenames,
+                       ParseChannelFilenames(inputs_for_channels_text));
+  absl::flat_hash_map<std::string, std::vector<Value>> inputs_for_channels;
+  for (const auto& [channel_name, filename] : input_filenames) {
+    XLS_ASSIGN_OR_RETURN(std::vector<Value> values, ParseValuesFile(filename));
+    inputs_for_channels[channel_name] = values;
+  }
+
+  absl::flat_hash_map<std::string, std::string> expected_filenames;
+  XLS_ASSIGN_OR_RETURN(
+      expected_filenames,
+      ParseChannelFilenames(expected_outputs_for_channels_text));
+  absl::flat_hash_map<std::string, std::vector<Value>>
+      expected_outputs_for_channels;
+  for (const auto& [channel_name, filename] : expected_filenames) {
+    XLS_ASSIGN_OR_RETURN(std::vector<Value> values, ParseValuesFile(filename));
+    expected_outputs_for_channels[channel_name] = values;
+  }
+
   if (backend == "serial_jit") {
-    return RunSerialJit(package.get(), ticks);
+    return RunSerialJit(package.get(), ticks, inputs_for_channels,
+                        expected_outputs_for_channels);
   } else {
-    return RunIrInterpreter(package.get(), ticks);
+    return RunIrInterpreter(package.get(), ticks, inputs_for_channels,
+                            expected_outputs_for_channels);
   }
 }
 
@@ -118,7 +237,10 @@ int main(int argc, char* argv[]) {
     XLS_LOG(QFATAL) << "--ticks must be specified (and > 0).";
   }
 
-  XLS_QCHECK_OK(xls::RealMain(positional_args[0], backend, ticks));
+  XLS_QCHECK_OK(
+      xls::RealMain(positional_args[0], backend, ticks,
+                    absl::GetFlag(FLAGS_inputs_for_channels),
+                    absl::GetFlag(FLAGS_expected_outputs_for_channels)));
 
   return 0;
 }
---
+++ xls/tools/eval_proc_main_test.py
@@ -0,0 +1,86 @@
+# Lint as: python3
+#
+# Copyright 2020 The XLS Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+
+from xls.common import runfiles
+from absl.testing import absltest
+
+EVAL_PROC_MAIN_PATH = runfiles.get_path("xls/tools/eval_proc_main")
+
+PROC_IR = """package foo
+
+chan in_ch(bits[64], id=1, kind=streaming, ops=receive_only, flow_control=ready_valid, metadata=\"\"\"\"\"\")
+chan in_ch_2(bits[64], id=2, kind=streaming, ops=receive_only, flow_control=ready_valid, metadata=\"\"\"\"\"\")
+chan out_ch(bits[64], id=3, kind=streaming, ops=send_only, flow_control=ready_valid, metadata=\"\"\"\"\"\")
+chan out_ch_2(bits[64], id=4, kind=streaming, ops=send_only, flow_control=ready_valid, metadata=\"\"\"\"\"\")
+
+proc test_proc(tkn: token, st: (), init=()) {
+  receive.1: (token, bits[64]) = receive(tkn, channel_id=1, id=1)
+
+  literal.3: bits[1] = literal(value=1, id=3)
+  tuple_index.7: token = tuple_index(receive.1, index=0, id=7)
+  tuple_index.4: bits[64] = tuple_index(receive.1, index=1, id=4)
+  receive.9: (token, bits[64]) = receive(tuple_index.7, channel_id=2, id=9)
+  tuple_index.10: bits[64] = tuple_index(receive.9, index=1, id=10)
+  add.8: bits[64] = add(tuple_index.4, tuple_index.10, id=8)
+
+  tuple_index.11: token = tuple_index(receive.9, index=0, id=11)
+  send.2: token = send(tuple_index.11, add.8, predicate=literal.3, channel_id=3, id=2)
+  literal.14: bits[64] = literal(value=55, id=14)
+  send.12: token = send(send.2, literal.14, predicate=literal.3, channel_id=4, id=12)
+
+  next(send.12, st)
+}
+"""
+
+
+class EvalProcTest(absltest.TestCase):
+
+  def test_give_me_a_name(self):
+    ir_file = self.create_tempfile(content=PROC_IR)
+    input_file = self.create_tempfile(content="""
+bits[64]:42
+bits[64]:101
+""")
+    input_file_2 = self.create_tempfile(content="""
+bits[64]:10
+bits[64]:6
+""")
+    output_file = self.create_tempfile(content="""
+bits[64]:52
+bits[64]:107
+""")
+    output_file_2 = self.create_tempfile(content="""
+bits[64]:55
+bits[64]:55
+""")
+
+    shared_args = [
+        EVAL_PROC_MAIN_PATH, ir_file.full_path, "--ticks", "2",
+        "--inputs_for_channels", "in_ch={infile1},in_ch_2={infile2}".format(
+            infile1=input_file.full_path,
+            infile2=input_file_2.full_path), "--expected_outputs_for_channels",
+        "out_ch={outfile},out_ch_2={outfile2}".format(
+            outfile=output_file.full_path, outfile2=output_file_2.full_path)
+    ]
+
+    subprocess.check_call(shared_args + ["--backend", "ir_interpreter"])
+    subprocess.check_call(shared_args + ["--backend", "serial_jit"])
+
+
+if __name__ == "__main__":
+  absltest.main()
