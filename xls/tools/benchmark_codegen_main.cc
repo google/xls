48a7dba8069b8262bb09478fa2db470fdb1d2ba8
Mark Heffernan <meheff@google.com>
2022-03-08 09:52:30 -0800

    Add codegen benchmark binary. Similar to bechmark_main this binary is invoked on an IR file (and verilog file) and reports various metrics.
    Later a build rule will be added for codegen benchmarks. These benchmarks will depend on a verilog target. This replaces the current scheme where codegen arguments are copied and passed into the xls_benchmark_ir. By depending directly on a verilog target there will be no divergence between the generated verilog and the benchmark.
    
    PiperOrigin-RevId: 433238417

---
+++ xls/tools/benchmark_codegen_main.cc
@@ -0,0 +1,133 @@
+// Copyright 2022 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <numeric>
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_split.h"
+#include "xls/codegen/block_metrics.h"
+#include "xls/common/file/filesystem.h"
+#include "xls/common/init_xls.h"
+#include "xls/common/logging/logging.h"
+#include "xls/common/status/status_macros.h"
+#include "xls/delay_model/delay_estimator.h"
+#include "xls/delay_model/delay_estimators.h"
+#include "xls/ir/ir_parser.h"
+
+const char kUsage[] = R"(
+Dumps various codegen-related metrics about a block and corresponding Verilog
+file. Designed to be used with run_benchmarks.py script.
+
+Usage:
+   benchmark_codegen_main --delay_model=DELAY_MODEL IR_FILE VERILOG_FILE
+)";
+
+ABSL_FLAG(std::string, top, "",
+          "Name of top block to use in lieu of the default.");
+ABSL_FLAG(std::string, delay_model, "",
+          "Delay model name to use from registry.");
+
+namespace xls {
+namespace {
+
+absl::StatusOr<Block*> GetTopBlock(Package* package) {
+  if (!absl::GetFlag(FLAGS_top).empty()) {
+    return package->GetBlock(absl::GetFlag(FLAGS_top));
+  }
+  std::optional<FunctionBase*> top = package->GetTop();
+  if (!top.has_value()) {
+    return absl::InvalidArgumentError("Package has no top defined");
+  }
+
+  if (!top.value()->IsBlock()) {
+    return absl::InvalidArgumentError(absl::StrFormat(
+        "Top entity of package is not a block: %s", top.value()->name()));
+  }
+  return top.value()->AsBlockOrDie();
+}
+
+absl::Status RealMain(absl::string_view ir_path, absl::string_view verilog_path,
+                      const DelayEstimator& delay_estimator) {
+  XLS_VLOG(1) << "Reading IR file: " << ir_path;
+  XLS_ASSIGN_OR_RETURN(std::string ir_contents, GetFileContents(ir_path));
+
+  XLS_VLOG(1) << "Reading Verilog file: " << verilog_path;
+  XLS_ASSIGN_OR_RETURN(std::string verilog_contents,
+                       GetFileContents(verilog_path));
+
+  XLS_ASSIGN_OR_RETURN(std::unique_ptr<Package> package,
+                       Parser::ParsePackage(ir_contents));
+  XLS_ASSIGN_OR_RETURN(Block * top, GetTopBlock(package.get()));
+
+  XLS_ASSIGN_OR_RETURN(verilog::BlockMetricsProto metrics,
+                       verilog::GenerateBlockMetrics(top, &delay_estimator));
+  std::cout << absl::StreamFormat("Flop count: %d\n", metrics.flop_count());
+  std::cout << absl::StreamFormat(
+      "Has feedthrough path: %s\n",
+      metrics.feedthrough_path_exists() ? "true" : "false");
+  if (metrics.has_max_reg_to_reg_delay_ps()) {
+    std::cout << absl::StreamFormat("Max reg-to-reg delay: %dps\n",
+                                    metrics.max_reg_to_reg_delay_ps());
+  }
+  if (metrics.has_max_input_to_reg_delay_ps()) {
+    std::cout << absl::StreamFormat("Max input-to-reg delay: %dps\n",
+                                    metrics.max_input_to_reg_delay_ps());
+  }
+  if (metrics.has_max_reg_to_output_delay_ps()) {
+    std::cout << absl::StreamFormat("Max reg-to-output delay: %dps\n",
+                                    metrics.max_reg_to_output_delay_ps());
+  }
+  if (metrics.has_max_feedthrough_path_delay_ps()) {
+    std::cout << absl::StreamFormat("Max feedthrough path delay: %dps\n",
+                                    metrics.max_feedthrough_path_delay_ps());
+  }
+  std::cout << absl::StreamFormat(
+      "Lines of Verilog: %d\n",
+      std::vector<std::string>(absl::StrSplit(verilog_contents, '\n')).size());
+
+  return absl::OkStatus();
+}
+
+absl::StatusOr<const DelayEstimator*> FetchDelayEstimator() {
+  const DelayEstimator* delay_estimator;
+  if (absl::GetFlag(FLAGS_delay_model).empty()) {
+    delay_estimator = &GetStandardDelayEstimator();
+  } else {
+    XLS_ASSIGN_OR_RETURN(delay_estimator,
+                         GetDelayEstimator(absl::GetFlag(FLAGS_delay_model)));
+  }
+  return delay_estimator;
+}
+
+}  // namespace
+}  // namespace xls
+
+int main(int argc, char** argv) {
+  std::vector<absl::string_view> positional_arguments =
+      xls::InitXls(kUsage, argc, argv);
+
+  if (positional_arguments.size() != 2) {
+    XLS_LOG(QFATAL) << absl::StreamFormat(
+        "Expected invocation:\n  %s IR_FILE VERILOG_FILE", argv[0]);
+  }
+
+  absl::StatusOr<const xls::DelayEstimator*> delay_estimator_or =
+      xls::FetchDelayEstimator();
+  XLS_QCHECK_OK(delay_estimator_or.status());
+
+  XLS_QCHECK_OK(xls::RealMain(positional_arguments[0], positional_arguments[1],
+                              *delay_estimator_or.value()));
+  return EXIT_SUCCESS;
+}
---
+++ xls/tools/benchmark_codegen_main_test.py
@@ -0,0 +1,81 @@
+# Lint as: python3
+#
+# Copyright 2022 The XLS Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for xls.tools.codegen_main."""
+
+import subprocess
+
+from absl.testing import absltest
+from xls.common import runfiles
+from xls.common import test_base
+
+BENCHMARK_CODEGEN_MAIN_PATH = runfiles.get_path(
+    'xls/tools/benchmark_codegen_main')
+
+SIMPLE_IR = """package add
+
+top block my_block(clk: clock, a: bits[32], b: bits[32], out: bits[32]) {
+  reg a_reg(bits[32])
+  reg b_reg(bits[32])
+  reg sum_reg(bits[32])
+
+  a: bits[32] = input_port(name=a)
+  a_d: () = register_write(a, register=a_reg)
+  a_q: bits[32] = register_read(register=a_reg)
+
+  b: bits[32] = input_port(name=b)
+  b_d: () = register_write(b, register=b_reg)
+  b_q: bits[32] = register_read(register=b_reg)
+
+  sum: bits[32] = add(a_q, b_q)
+  sum_d: () = register_write(sum, register=sum_reg)
+  sum_q: bits[32] = register_read(register=sum_reg)
+
+  not_sum_q: bits[32] = not(sum_q)
+  not_not_sum_q: bits[32] = not(not_sum_q)
+
+  out: () = output_port(not_not_sum_q, name=out)
+}
+"""
+
+SIMPLE_VERILOG = """module main(
+  input wire [31:0] x,
+  output wire [31:0] out
+);
+  assign out = x;
+endmodule
+"""
+
+
+class CodeGenMainTest(test_base.TestCase):
+
+  def test_simple_block(self):
+    ir_file = self.create_tempfile(content=SIMPLE_IR)
+    verilog_file = self.create_tempfile(content=SIMPLE_VERILOG)
+    output = subprocess.check_output([
+        BENCHMARK_CODEGEN_MAIN_PATH, '--delay_model=unit', ir_file.full_path,
+        verilog_file.full_path
+    ]).decode('utf-8')
+
+    self.assertIn('Flop count: 96', output)
+    self.assertIn('Has feedthrough path: false', output)
+    self.assertIn('Max reg-to-reg delay: 1ps', output)
+    self.assertIn('Max input-to-reg delay: 0ps', output)
+    self.assertIn('Max reg-to-output delay: 2ps', output)
+    self.assertIn('Lines of Verilog: 7', output)
+
+
+if __name__ == '__main__':
+  absltest.main()
