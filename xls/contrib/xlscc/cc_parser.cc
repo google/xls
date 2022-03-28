989acb1fcacd9510eb2909c851a89c4e3dad3591
Sean Purser-Haskell <seanhaskell@google.com>
2022-03-28 10:41:17 -0700

    Fix for new clang error with loop unrolling. xls/contrib/xlscc:translator_test started failing.
    PiperOrigin-RevId: 437797687

--- xls/contrib/xlscc/cc_parser.cc
+++ xls/contrib/xlscc/cc_parser.cc
@@ -362,6 +362,7 @@ void LibToolThread::Run() {
   argv.emplace_back("-nostdinc");
   argv.emplace_back("-Wno-unused-label");
   argv.emplace_back("-Wno-constant-logical-operand");
+  argv.emplace_back("-Wno-unused-but-set-variable");
 
   llvm::IntrusiveRefCntPtr<clang::FileManager> libtool_files;
 
