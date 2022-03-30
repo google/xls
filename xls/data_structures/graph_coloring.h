5960d206f79c87811773048430a61dd3e432689d
Remy Goldschmidt <taktoa@google.com>
2022-03-30 06:56:31 -0700

    Added an implementation of the recursive largest first graph coloring heuristic
    PiperOrigin-RevId: 438291471

---
+++ xls/data_structures/graph_coloring.h
@@ -0,0 +1,152 @@
+// Copyright 2022 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef XLS_DATA_STRUCTURES_GRAPH_COLORING_H_
+#define XLS_DATA_STRUCTURES_GRAPH_COLORING_H_
+
+#include <optional>
+
+#include "absl/container/btree_set.h"
+#include "absl/container/flat_hash_set.h"
+#include "xls/common/logging/log_message.h"
+#include "xls/common/logging/logging.h"
+
+namespace xls {
+
+// This finds a maximal independent set in the given graph, using heuristics
+// appropriate for the purpose of implementing the Recursive Largest First
+// graph coloring algorithm.
+//
+// `vertices` is the set of vertices of the graph, which must not be empty.
+// `neighborhood` is a function that, given a vertex in the graph, returns a set
+// containing its neighbors. This is agnostic to graph representation.
+//
+// This returns a set of nodes that comprises an independent set of the
+// given graph.
+//
+// This algorithm is explained on page 60 of "Guide to Graph Colouring" second
+// edition by R. M. R. Lewis. https://doi.org/10.1007%2F978-3-030-81054-2
+template <typename V>
+absl::flat_hash_set<V> FindMaximalIndependentSet(
+    const absl::flat_hash_set<V>& vertices,
+    std::function<absl::flat_hash_set<V>(const V&)> neighborhood) {
+  static_assert(!std::is_pointer<V>::value,
+                "To avoid nondetermistic behavior V cannot be a pointer type");
+
+  XLS_CHECK(!vertices.empty());
+
+  absl::flat_hash_set<V> result;  // named S in the book
+  absl::btree_set<V> available(vertices.begin(), vertices.end());  // named X
+  absl::flat_hash_set<V> neighboring_result;                       // named Y
+
+  auto add_to_result = [&](const V& vertex) {
+    result.insert(vertex);
+    for (const V& neighbor : neighborhood(vertex)) {
+      neighboring_result.insert(neighbor);
+    }
+    available.erase(vertex);
+  };
+
+  // Initialize result to contain only the vertex with highest degree.
+  {
+    int64_t largest_neighborhood = 0;
+    std::optional<V> vertex_with_most_neighbors;
+    for (const V& vertex : available) {
+      int64_t neighborhood_size = neighborhood(vertex).size();
+      if (neighborhood_size >= largest_neighborhood) {
+        largest_neighborhood = neighborhood_size;
+        vertex_with_most_neighbors = vertex;
+      }
+    }
+    add_to_result(vertex_with_most_neighbors.value());
+  }
+
+  while (!available.empty()) {
+    // We want to choose the best vertex V as measured by a tuple (a, b) where
+    // a is the number of neighbors in Y and b is the negated number of
+    // neighbors in X, and these tuples are ordered lexicographically
+    // (i.e.: sort by a, and then resolve ties with b).
+    //
+    // The first element in a valid tuple is always positive, so setting this to
+    // (-1, -1) will result in it getting replaced in the first iteration of the
+    // `for` loop below.
+    std::pair<int64_t, int64_t> measure = {-1, -1};
+    std::optional<V> best;
+    for (const V& vertex : available) {
+      if (neighboring_result.contains(vertex)) {
+        continue;
+      }
+      std::pair<int64_t, int64_t> vertex_measure{0, 0};
+      for (const V& neighbor : neighborhood(vertex)) {
+        if (neighboring_result.contains(neighbor)) {
+          ++vertex_measure.first;
+        }
+        if (available.contains(neighbor)) {
+          --vertex_measure.second;
+        }
+      }
+      if (vertex_measure > measure) {
+        best = vertex;
+        measure = vertex_measure;
+      }
+    }
+    if (!best.has_value()) {
+      break;
+    }
+    add_to_result(best.value());
+  }
+
+  return result;
+}
+
+// Color the given graph using the Recursive Largest First (RLF) algorithm.
+//
+// `vertices` is the set of vertices of the graph.
+// `neighborhood` is a function that, given a vertex in the graph, returns a set
+// containing its neighbors. This is agnostic to graph representation.
+//
+// This returns a vector of sets of nodes, each of which represents a color
+// in the colored graph.
+//
+// This algorithm is explained on page 60 of "Guide to Graph Colouring" second
+// edition by R. M. R. Lewis. https://doi.org/10.1007%2F978-3-030-81054-2
+template <typename V>
+std::vector<absl::flat_hash_set<V>> RecursiveLargestFirstColoring(
+    const absl::flat_hash_set<V>& vertices,
+    std::function<absl::flat_hash_set<V>(const V&)> neighborhood) {
+  std::vector<absl::flat_hash_set<V>> result;
+  absl::flat_hash_set<V> available = vertices;
+  while (!available.empty()) {
+    // Find the maximal independent set in the subgraph induced by `available`.
+    absl::flat_hash_set<V> chosen = FindMaximalIndependentSet<V>(
+        available, [&](const V& v) -> absl::flat_hash_set<V> {
+          absl::flat_hash_set<V> result;
+          for (const V& neighbor : neighborhood(v)) {
+            if (available.contains(neighbor)) {
+              result.insert(neighbor);
+            }
+          }
+          return result;
+        });
+    for (const V& vertex : chosen) {
+      available.erase(vertex);
+    }
+    result.push_back(chosen);
+  }
+  return result;
+}
+
+}  // namespace xls
+
+#endif  // XLS_DATA_STRUCTURES_GRAPH_COLORING_H_
---
+++ xls/data_structures/graph_coloring_test.cc
@@ -0,0 +1,119 @@
+// Copyright 2022 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/data_structures/graph_coloring.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+
+namespace xls {
+namespace {
+
+using V = std::string;
+
+std::vector<absl::flat_hash_set<V>> RLFFromMap(
+    const absl::flat_hash_map<V, absl::flat_hash_set<V>>& neighborhood) {
+  absl::flat_hash_map<V, absl::flat_hash_set<V>> symmetric_neighborhood;
+  absl::flat_hash_set<V> nodes;
+  for (const auto& [node, neighbors] : neighborhood) {
+    for (const auto& neighbor : neighbors) {
+      nodes.insert(node);
+      nodes.insert(neighbor);
+      symmetric_neighborhood[node].insert(neighbor);
+      symmetric_neighborhood[neighbor].insert(node);
+    }
+  }
+
+  return RecursiveLargestFirstColoring<V>(
+      nodes, [&](const V& node) -> absl::flat_hash_set<V> {
+        return symmetric_neighborhood.at(node);
+      });
+}
+
+bool IsValidColoring(
+    const absl::flat_hash_map<V, absl::flat_hash_set<V>>& neighborhood,
+    const std::vector<absl::flat_hash_set<V>>& coloring) {
+  absl::flat_hash_set<V> nodes;
+  for (const auto& [node, neighbors] : neighborhood) {
+    for (const auto& neighbor : neighbors) {
+      nodes.insert(node);
+      nodes.insert(neighbor);
+    }
+  }
+
+  // Every node must be used exactly once.
+  for (const absl::flat_hash_set<V>& color_class : coloring) {
+    for (const V& element : color_class) {
+      if (!nodes.contains(element)) {
+        return false;
+      }
+      nodes.erase(element);
+    }
+  }
+  return nodes.empty();
+}
+
+TEST(GraphColoringTest, Bipartite) {
+  absl::flat_hash_map<V, absl::flat_hash_set<V>> graph;
+  graph["a"].insert("x");
+  graph["a"].insert("y");
+  graph["a"].insert("z");
+  graph["b"].insert("x");
+  graph["c"].insert("w");
+  EXPECT_LE(RLFFromMap(graph).size(), 2);
+  EXPECT_TRUE(IsValidColoring(graph, RLFFromMap(graph)));
+}
+
+TEST(GraphColoringTest, Cycle) {
+  absl::flat_hash_map<V, absl::flat_hash_set<V>> graph;
+  graph["a"].insert("b");
+  graph["b"].insert("c");
+  graph["c"].insert("d");
+  graph["d"].insert("e");
+  graph["e"].insert("a");
+  EXPECT_EQ(RLFFromMap(graph).size(), 3);
+  EXPECT_TRUE(IsValidColoring(graph, RLFFromMap(graph)));
+  graph.erase("e");
+  graph["d"].erase("e");
+  graph["d"].insert("a");
+  EXPECT_EQ(RLFFromMap(graph).size(), 2);
+  EXPECT_TRUE(IsValidColoring(graph, RLFFromMap(graph)));
+}
+
+TEST(GraphColoringTest, Wheel) {
+  absl::flat_hash_map<V, absl::flat_hash_set<V>> graph;
+  graph["a"].insert("b");
+  graph["b"].insert("c");
+  graph["c"].insert("d");
+  graph["d"].insert("e");
+  graph["e"].insert("a");
+  graph["center"].insert("a");
+  graph["center"].insert("b");
+  graph["center"].insert("c");
+  graph["center"].insert("d");
+  graph["center"].insert("e");
+  EXPECT_EQ(RLFFromMap(graph).size(), 4);
+  EXPECT_TRUE(IsValidColoring(graph, RLFFromMap(graph)));
+  graph.erase("e");
+  graph["d"].erase("e");
+  graph["d"].insert("a");
+  graph["center"].erase("e");
+  EXPECT_EQ(RLFFromMap(graph).size(), 3);
+  EXPECT_TRUE(IsValidColoring(graph, RLFFromMap(graph)));
+}
+
+}  // namespace
+}  // namespace xls
