e5e1e700793f70b54c612c2feb1ecdb164d55c63
Yilei Yang <yileiyang@google.com>
2022-04-21 10:09:48 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 443409496

--- xls/delay_model/delay_model_test.py
+++ xls/delay_model/delay_model_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Lint as: python3
 
 """Tests for xls.delay_model.models.delay_model."""
 
--- xls/fuzzer/sample_runner_main.py
+++ xls/fuzzer/sample_runner_main.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 r"""Sample runner program.
 
 Runs a fuzzer code sample in the given run directory. Files are copied into the
