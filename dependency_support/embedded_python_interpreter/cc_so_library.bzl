google/xls

commit f2ec0907bc206624fa499842edb09d287388f4eb

Update rules which declare the cpp toolchain to use a helper method instead.
PiperOrigin-RevId: 461001569

dependency_support/embedded_python_interpreter/cc_so_library.bzl
@@ -14,7 +14,7 @@
 
 """Provides a rule that creates a C++ library from an .so file."""
 
-load("@rules_cc//cc:find_cc_toolchain.bzl", "find_cc_toolchain")
+load("@rules_cc//cc:find_cc_toolchain.bzl", "find_cc_toolchain", "use_cpp_toolchain")
 
 def _impl(ctx):
     cc_toolchain = find_cc_toolchain(ctx)
@@ -44,5 +44,5 @@ cc_so_library = rule(
         "_cc_toolchain": attr.label(default = "@bazel_tools//tools/cpp:current_cc_toolchain"),
     },
     incompatible_use_toolchain_transition = True,
-    toolchains = ["@bazel_tools//tools/cpp:toolchain_type"],
+    toolchains = use_cpp_toolchain(),
 )

commit f1eed78cc6fce76a1f4818abf8dc12e5b7b04fbd

Fix CPP toolchain configuration rules.
f2ec0907bc206624fa499842edb09d287388f4eb introduced a change that broke the build for me. It seems @rules_cc deprecated toolchain_utils and the current recommendation is to use @bazel_tools//tools/cpp:toolchain_utils.bzl.

PiperOrigin-RevId: 461271149

dependency_support/embedded_python_interpreter/cc_so_library.bzl
@@ -14,10 +14,10 @@
 
 """Provides a rule that creates a C++ library from an .so file."""
 
-load("@rules_cc//cc:find_cc_toolchain.bzl", "find_cc_toolchain", "use_cpp_toolchain")
+load("@bazel_tools//tools/cpp:toolchain_utils.bzl", "find_cpp_toolchain", "use_cpp_toolchain")
 
 def _impl(ctx):
-    cc_toolchain = find_cc_toolchain(ctx)
+    cc_toolchain = find_cpp_toolchain(ctx)
     feature_configuration = cc_common.configure_features(ctx = ctx, cc_toolchain = cc_toolchain)
     linker_inputs = [cc_common.create_linker_input(
         owner = ctx.label,