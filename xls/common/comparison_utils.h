6f213b395f9156696a27bb4497b759ea0f79ed2b
Vincent Mirian <vmirian@google.com>
2022-07-25 14:00:44 -0700

    [COMMON] Utilities for comparing C++ primitive types and selected containers. The comparisons return a human readable string useful during debugging.
    PiperOrigin-RevId: 463171389

---
+++ xls/common/comparison_utils.h
@@ -0,0 +1,144 @@
+// Copyright 2022 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef XLS_COMMON_COMPARISON_UTILS_H_
+#define XLS_COMMON_COMPARISON_UTILS_H_
+
+#include <deque>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_format.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/span.h"
+#include "xls/common/to_string_helpers.h"
+
+// Utilities for comparing C++ primitive types and selected containers. The
+// comparisons return a human readable string useful during debugging.
+
+namespace xls {
+
+// Utilities for comparing C++ primitive types and selected containers.
+//
+// The comparisons are performed recursively. As a result, when comparing a
+// container, the elements within the container are also compared.
+//
+// To leverage the recursive mechanism when performing comparisons for
+// user-defined types such as Classes and Structs, the comparison function must
+// have the following signature, where 'MyClassOrStruct' is a user-defined class
+// or struct:
+//
+//     std::string Compare(absl::string_view element_name,
+//                         const MyClassOrStruct& expected,
+//                         const MyClassOrStruct& computed);
+//
+// Moreover, the function must be placed in the 'xls' namespace.
+//
+// In addition, when defining a user-defined class or struct, the '!=' operator
+// for the user-defined class or struct must be defined in the 'xls' namespace.
+// The following is an example of a user-defined struct declaration:
+//     namespace userspace {
+//       struct UserStruct {
+//        int64_t integer_value;
+//        bool boolean_value;
+//       };
+//     }  // namespace userspace
+//     namespace xls {
+//      bool operator!=(const ::userspace::UserStruct& lhs,
+//                       const ::userspace::UserStruct& rhs);
+//     }  // namespace xls
+//
+// In practice, the user may define the '==' operator and define a '!=' operator
+// that negates the result from the '==' operator. For example, for the
+// UserStruct struct may have the following definition:
+//     namespace xls {
+//       bool operator==(const ::userspace::UserStruct& lhs,
+//                       const ::userspace::UserStruct& rhs) {
+//         return lhs.integer_value == rhs.integer_value &&
+//                lhs.boolean_value == rhs.boolean_value;
+//       }
+//       bool operator!=(const ::userspace::UserStruct& lhs,
+//                       const ::userspace::UserStruct& rhs) {
+//         return !(lhs == rhs);
+//       }
+//     }  // namespace xls
+//
+// Given the UserStruct struct defined above, a sample comparison function would
+// be as follows:
+//
+//     namespace xls {
+//       std::string Compare(absl::string_view element_name,
+//                           const ::userspace::UserStruct& expected,
+//                           const ::userspace::UserStruct& computed) {
+//         std::string comparison;
+//         std::string ref_element_name;
+//         if (!element_name.empty()) {
+//           ref_element_name = absl::StrCat(element_name, ".");
+//         }
+//         absl::StrAppend(
+//           &comparison,
+//           absl::StrFormat("%s",
+//             ::xls::Compare(absl::StrCat(ref_element_name, "integer_value"),
+//                            expected.integer_value, computed.integer_value)));
+//         absl::StrAppend(
+//           &comparison,
+//           absl::StrFormat("%s",
+//             ::xls::Compare(absl::StrCat(ref_element_name, "boolean_value"),
+//                            expected.boolean_value, computed.boolean_value)));
+//         return comparison;
+//       }
+//     }  // namespace xls
+//
+// Note that the comparison function leverages the comparison function for C++
+// primitives.
+
+template <typename T,
+          typename std::enable_if<std::is_integral_v<T>, T>::type* = nullptr>
+std::string Compare(absl::string_view element_name, T expected, T computed) {
+  if (computed == expected) return "";
+  return absl::StrFormat("Element %s differ: expected (%s), got (%s).\n",
+                         element_name, ToString(expected), ToString(computed));
+}
+
+// TODO(vmirian): 7-25-2022 Add iterator trait to detect containers.
+template <
+    typename T, template <class...> class Container,
+    typename std::enable_if<std::is_same_v<Container<T>, absl::Span<const T>> ||
+                                std::is_same_v<Container<T>, std::vector<T>> ||
+                                std::is_same_v<Container<T>, std::deque<T>>,
+                            T>::type* = nullptr>
+std::string Compare(absl::string_view element_name,
+                    const Container<T>& expected,
+                    const Container<T>& computed) {
+  if (computed.size() != expected.size()) {
+    return absl::StrFormat(
+        "Size of element %s differ: expected (%s), got (%s).\n", element_name,
+        ToString(expected.size()), ToString(computed.size()));
+  }
+  std::string comparison;
+  for (int64_t i = 0; i < computed.size(); ++i) {
+    if (computed[i] != expected[i]) {
+      absl::StrAppend(&comparison,
+                      Compare(absl::StrCat(element_name, "[", i, "]"),
+                              expected[i], computed[i]));
+    }
+  }
+  return comparison;
+}
+
+}  // namespace xls
+
+#endif  // XLS_COMMON_COMPARISON_UTILS_H_
---
+++ xls/common/comparison_utils_test.cc
@@ -0,0 +1,167 @@
+// Copyright 2022 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/common/comparison_utils.h"
+
+#include <cstdint>
+#include <deque>
+#include <string>
+#include <vector>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/strings/str_cat.h"
+
+namespace xls {
+namespace {
+
+using testing::HasSubstr;
+using testing::IsEmpty;
+
+TEST(ComparisonUtilsTest, CompareBool) {
+  EXPECT_THAT(::xls::Compare("boolean", true, true), IsEmpty());
+  EXPECT_THAT(
+      ::xls::Compare("boolean", true, false),
+      HasSubstr("Element boolean differ: expected (true), got (false)."));
+}
+
+template <typename T>
+class ComparisonUtilsIntegralTypedTest : public ::testing::Test {};
+
+TYPED_TEST_SUITE_P(ComparisonUtilsIntegralTypedTest);
+
+TYPED_TEST_P(ComparisonUtilsIntegralTypedTest, CompareIntegrals) {
+  EXPECT_THAT(::xls::Compare("Integral value", static_cast<TypeParam>(8),
+                             static_cast<TypeParam>(8)),
+              IsEmpty());
+  EXPECT_THAT(
+      ::xls::Compare("Integral value", static_cast<TypeParam>(8),
+                     static_cast<TypeParam>(42)),
+      HasSubstr("Element Integral value differ: expected (8), got (42)."));
+}
+
+REGISTER_TYPED_TEST_SUITE_P(ComparisonUtilsIntegralTypedTest, CompareIntegrals);
+
+using MyIntegralTypes = ::testing::Types<int8_t, int16_t, int32_t, int64_t,
+                                         uint8_t, uint16_t, uint32_t, uint64_t>;
+INSTANTIATE_TYPED_TEST_SUITE_P(Integrals, ComparisonUtilsIntegralTypedTest,
+                               MyIntegralTypes);
+
+template <typename T>
+class ComparisonUtilsContainerTypedTest : public ::testing::Test {};
+
+TYPED_TEST_SUITE_P(ComparisonUtilsContainerTypedTest);
+
+TYPED_TEST_P(ComparisonUtilsContainerTypedTest, Compare1DContainerIntegral) {
+  EXPECT_THAT(
+      ::xls::Compare("Container is equal", TypeParam{42, 1}, TypeParam{42, 1}),
+      IsEmpty());
+  EXPECT_THAT(
+      ::xls::Compare("Container", TypeParam{42, 1}, TypeParam{42, 42}),
+      HasSubstr("Element Container[1] differ: expected (1), got (42)."));
+}
+
+REGISTER_TYPED_TEST_SUITE_P(ComparisonUtilsContainerTypedTest,
+                            Compare1DContainerIntegral);
+
+// Test a single integral type per container type, since the integral types were
+// thoroughly tested in a previous tests.
+using My1DContainerIntegralTypes =
+    ::testing::Types<absl::Span<const int8_t>, std::vector<int8_t>,
+                     std::deque<int8_t>>;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(Integrals, ComparisonUtilsContainerTypedTest,
+                               My1DContainerIntegralTypes);
+
+// Test a single container type for a multi-dimensional containers, since the
+// container types were thoroughly tested in a previous tests.
+TEST(ComparisonUtilsTest, Compare2DVectorIntegral) {
+  EXPECT_THAT(
+      ::xls::Compare("Vector is equal",
+                     std::vector<std::vector<int64_t>>{{42, 42}, {42, 42}},
+                     std::vector<std::vector<int64_t>>{{42, 42}, {42, 42}}),
+      IsEmpty());
+  EXPECT_THAT(
+      ::xls::Compare("Vector",
+                     std::vector<std::vector<int64_t>>{{42, 42}, {42, 42}},
+                     std::vector<std::vector<int64_t>>{{42, 42}, {42, 1}}),
+      HasSubstr("Element Vector[1][1] differ: expected "
+                "(42), got (1)."));
+}
+
+struct UserDefinedType {
+  int64_t integer;
+  bool boolean;
+};
+static bool operator==(const UserDefinedType& lhs, const UserDefinedType& rhs) {
+  return lhs.integer == rhs.integer && lhs.boolean == rhs.boolean;
+}
+static bool operator!=(const UserDefinedType& lhs, const UserDefinedType& rhs) {
+  return !(lhs == rhs);
+}
+
+std::string Compare(absl::string_view element_name,
+                    const UserDefinedType& expected,
+                    const UserDefinedType& computed) {
+  std::string comparison;
+  std::string ref_element_name;
+  if (!element_name.empty()) {
+    ref_element_name = absl::StrCat(element_name, ".");
+  }
+  absl::StrAppend(
+      &comparison,
+      absl::StrFormat("%s",
+                      ::xls::Compare(absl::StrCat(ref_element_name, "integer"),
+                                     expected.integer, computed.integer)));
+  absl::StrAppend(
+      &comparison,
+      absl::StrFormat("%s",
+                      ::xls::Compare(absl::StrCat(ref_element_name, "boolean"),
+                                     expected.boolean, computed.boolean)));
+  return comparison;
+}
+
+TEST(ComparisonUtilsTest, CompareUserDefinedType) {
+  EXPECT_THAT(Compare("UserDefinedType is equal", UserDefinedType{42, true},
+                      UserDefinedType{42, true}),
+              IsEmpty());
+  EXPECT_THAT(Compare("UserDefinedType", UserDefinedType{42, true},
+                      UserDefinedType{42, false}),
+              HasSubstr("Element UserDefinedType.boolean differ: expected "
+                        "(true), got (false)."));
+  // Differ with no name reference.
+  EXPECT_THAT(Compare("", UserDefinedType{42, true}, UserDefinedType{1, true}),
+              HasSubstr("Element integer differ: expected (42), got (1)."));
+}
+
+TEST(ComparisonUtilsTest, CompareSpanUserDefinedType) {
+  EXPECT_THAT(
+      ::xls::Compare("Span is equal",
+                     absl::Span<const UserDefinedType>{
+                         UserDefinedType{42, true}, UserDefinedType{1, false}},
+                     absl::Span<const UserDefinedType>{
+                         UserDefinedType{42, true}, UserDefinedType{1, false}}),
+      IsEmpty());
+  EXPECT_THAT(
+      ::xls::Compare("Span",
+                     absl::Span<const UserDefinedType>{
+                         UserDefinedType{42, true}, UserDefinedType{42, false}},
+                     absl::Span<const UserDefinedType>{
+                         UserDefinedType{1, true}, UserDefinedType{42, false}}),
+      HasSubstr("Element Span[0].integer differ: expected "
+                "(42), got (1)."));
+}
+
+}  // namespace
+}  // namespace xls
