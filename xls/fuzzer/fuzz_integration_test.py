af2eb34825782dd74d83a249f4c47133d36f62e9
Mark Heffernan <meheff@google.com>
2022-08-19 10:57:04 -0700

    Add fuzzer integration test.
    PiperOrigin-RevId: 468739367

---
+++ xls/fuzzer/fuzz_integration_test.py
@@ -0,0 +1,144 @@
+#
+# Copyright 2022 The XLS Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for xls.fuzzer.run_fuzz."""
+
+import datetime
+import hashlib
+import os
+import tempfile
+
+from absl import flags
+from absl import logging
+
+from absl.testing import absltest
+from xls.common import gfile
+from xls.fuzzer import run_fuzz
+from xls.fuzzer import sample_runner
+from xls.fuzzer.python import cpp_ast_generator as ast_generator
+from xls.fuzzer.python.cpp_sample import Sample
+from xls.fuzzer.python.cpp_sample import SampleOptions
+
+_SEED = flags.DEFINE_integer('seed', 0, 'Seed value for generation')
+_SAMPLE_COUNT = flags.DEFINE_integer('sample_count', 10,
+                                     'Number of samples to generate')
+_CALLS_PER_SAMPLE = flags.DEFINE_integer('calls_per_sample', 128,
+                                         'Arguments to generate per sample')
+_SIMULATE = flags.DEFINE_boolean('simulate', False, 'Run Verilog simulation.')
+_SIMULATOR = flags.DEFINE_string(
+    'simulator', None, 'Verilog simulator to use. For example: "iverilog".')
+_MAX_WIDTH_BITS_TYPES = flags.DEFINE_integer(
+    'max_width_bits_types', 64,
+    'The maximum width of bits types in the generated samples.')
+_MAX_WIDTH_AGGREGATE_TYPES = flags.DEFINE_integer(
+    'max_width_aggregate_types', 1024,
+    'The maximum width of aggregate types (tuples and arrays) in the generated '
+    'samples.')
+_FORCE_FAILURE = flags.DEFINE_bool(
+    'force_failure', False,
+    'Forces the samples to fail. Can be used to test failure code paths.')
+
+# The maximum number of failures before the test aborts.
+MAX_FAILURES = 10
+
+
+def _get_crasher_directory() -> str:
+  """Returns the directory in which to write crashers.
+
+  Crashers are written to the undeclared outputs directory, if it is
+  available. Otherwise a temporary directory is created.
+  """
+  if 'TEST_UNDECLARED_OUTPUTS_DIR' in os.environ:
+    crasher_dir = os.path.join(os.environ['TEST_UNDECLARED_OUTPUTS_DIR'],
+                               'crashers')
+    os.mkdir(crasher_dir)
+    return crasher_dir
+  else:
+    return tempfile.mkdtemp(prefix='run_fuzz_crashers')
+
+
+def _save_crasher(run_dir: str, sample: Sample,
+                  exception: sample_runner.SampleError, crasher_dir: str):
+  """Saves the sample into a new directory in the crasher directory."""
+  digest = hashlib.sha256(sample.input_text.encode('utf-8')).hexdigest()[:8]
+  sample_crasher_dir = os.path.join(crasher_dir, digest)
+  logging.info('Saving crasher to %s', sample_crasher_dir)
+  gfile.recursively_copy_dir(
+      run_dir, sample_crasher_dir, preserve_file_mask=True)
+  with gfile.open(os.path.join(sample_crasher_dir, 'exception.txt'), 'w') as f:
+    f.write(str(exception))
+  crasher_path = os.path.join(
+      sample_crasher_dir,
+      'crasher_{}_{}.x'.format(datetime.date.today().strftime('%Y-%m-%d'),
+                               digest[:4]))
+  with gfile.open(crasher_path, 'w') as f:
+    f.write(sample.to_crasher(str(exception)))
+
+
+class FuzzIntegrationTest(absltest.TestCase):
+
+  def test(self):
+    """Runs the fuzzer based on flag values."""
+    crasher_dir = _get_crasher_directory()
+    rng = ast_generator.RngState(_SEED.value)
+    generator_options = ast_generator.AstGeneratorOptions(
+        max_width_bits_types=_MAX_WIDTH_BITS_TYPES.value,
+        max_width_aggregate_types=_MAX_WIDTH_AGGREGATE_TYPES.value,
+        generate_empty_tuples=True,
+        emit_gate=not _SIMULATE.value)
+    default_sample_options = SampleOptions(
+        input_is_dslx=True,
+        ir_converter_args=['--top=main'],
+        convert_to_ir=True,
+        optimize_ir=True,
+        use_jit=True,
+        codegen=_SIMULATE.value,
+        simulate=_SIMULATE.value,
+        simulator=_SIMULATOR.value,
+        use_system_verilog=_SIMULATE.value,
+        timeout_seconds=None)
+
+    crasher_count = 0
+    for i in range(_SAMPLE_COUNT.value):
+      logging.info('Running sample %d', i)
+      sample = ast_generator.generate_sample(generator_options,
+                                             _CALLS_PER_SAMPLE.value,
+                                             default_sample_options, rng)
+      with tempfile.TemporaryDirectory(prefix='run_fuzz_') as run_dir:
+        try:
+          run_fuzz.run_sample(sample, run_dir)
+          if _FORCE_FAILURE.value:
+            raise sample_runner.SampleError('Forced sample failure.')
+
+        except sample_runner.SampleError as e:
+          logging.error('Sample failed: %s', str(e))
+          crasher_count += 1
+          if not e.is_timeout:
+            logging.info('Attempting to minimize IR...')
+            ir_minimized = run_fuzz.minimize_ir(sample, run_dir)
+            if ir_minimized:
+              logging.info('...minimization successful.')
+            else:
+              logging.info('...minimization failed.')
+          _save_crasher(run_dir, sample, e, crasher_dir)
+
+      if crasher_count >= MAX_FAILURES:
+        break
+
+    if crasher_count > 0:
+      self.fail(f'Fuzzing found {crasher_count} failing samples')
+
+
+if __name__ == '__main__':
+  absltest.main()
