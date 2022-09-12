66147e60828d47c3c23184b295deebc43433e43b
Remy Goldschmidt <taktoa@google.com>
2022-09-12 13:57:26 -0700

    Add strongly connected components to data_structures/
    This is needed for backedge constraint calculation in the scheduler.
    
    PiperOrigin-RevId: 473837638

---
+++ xls/data_structures/strongly_connected_components.h
@@ -0,0 +1,99 @@
+// Copyright 2022 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef XLS_DATA_STRUCTURES_STRONGLY_CONNECTED_COMPONENTS_H_
+#define XLS_DATA_STRUCTURES_STRONGLY_CONNECTED_COMPONENTS_H_
+
+#include <stack>
+#include <vector>
+
+#include "absl/container/btree_map.h"
+#include "absl/container/btree_set.h"
+
+namespace xls {
+
+// Computes the strongly connected components of a graph using Tarjan's strongly
+// connected components algorithm.
+//
+// The parameter `graph` is an arbitrary adjacency matrix represented as a
+// map from nodes to the set of out-neighbors of that node. Self-edges are
+// permitted.
+//
+// A description of the Tarjan SCC algorithm exists on Wikipedia:
+// https://w.wiki/5h9U
+// This implementation is directly based off of that pseudocode, so please read
+// that to make sense of this code.
+template <typename V>
+std::vector<absl::btree_set<V>> StronglyConnectedComponents(
+    const absl::btree_map<V, absl::btree_set<V>>& graph) {
+  absl::btree_set<V> vertices;
+  for (const auto& [source, targets] : graph) {
+    for (const V& target : targets) {
+      vertices.insert(source);
+      vertices.insert(target);
+    }
+  }
+
+  int64_t index = 0;
+  std::stack<V, std::vector<V>> stack;
+  std::vector<absl::btree_set<V>> result;
+  absl::btree_map<V, int64_t> indexes;
+  absl::btree_map<V, int64_t> low_links;
+  absl::btree_set<V> on_stack;
+
+  std::function<void(const V&)> strong_connect = [&](const V& vertex) {
+    indexes[vertex] = index;
+    low_links[vertex] = index;
+    ++index;
+    stack.push(vertex);
+    on_stack.insert(vertex);
+
+    if (graph.contains(vertex)) {
+      for (const V& neighbor : graph.at(vertex)) {
+        if (!indexes.contains(neighbor)) {
+          strong_connect(neighbor);
+          low_links.at(vertex) =
+              std::min(low_links.at(vertex), low_links.at(neighbor));
+        } else if (on_stack.contains(neighbor)) {
+          low_links.at(vertex) =
+              std::min(low_links.at(vertex), indexes.at(neighbor));
+        }
+      }
+    }
+
+    if (low_links.at(vertex) == indexes.at(vertex)) {
+      absl::btree_set<V> scc;
+      V v;
+      do {
+        v = stack.top();
+        stack.pop();
+        on_stack.erase(v);
+        scc.insert(v);
+      } while (v != vertex);
+      result.push_back(scc);
+    }
+  };
+
+  for (const V& vertex : vertices) {
+    if (!indexes.contains(vertex)) {
+      strong_connect(vertex);
+    }
+  }
+
+  return result;
+}
+
+}  // namespace xls
+
+#endif  // XLS_DATA_STRUCTURES_STRONGLY_CONNECTED_COMPONENTS_H
---
+++ xls/data_structures/strongly_connected_components_test.cc
@@ -0,0 +1,144 @@
+// Copyright 2022 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/data_structures/strongly_connected_components.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace xls {
+namespace {
+
+using V = std::string;
+
+absl::btree_set<V> FlattenSCCs(const std::vector<absl::btree_set<V>>& sccs) {
+  absl::btree_set<V> result;
+  for (const absl::btree_set<V>& scc : sccs) {
+    for (const V& vertex : scc) {
+      result.insert(vertex);
+    }
+  }
+  return result;
+}
+
+int64_t GraphSize(const absl::btree_map<V, absl::btree_set<V>>& graph) {
+  absl::btree_set<V> vertices;
+  for (const auto& [source, targets] : graph) {
+    for (const V& target : targets) {
+      vertices.insert(source);
+      vertices.insert(target);
+    }
+  }
+  return vertices.size();
+}
+
+TEST(StronglyConnectedComponentsTest, Cycle) {
+  absl::btree_map<V, absl::btree_set<V>> graph;
+  graph["a"].insert("b");
+  graph["b"].insert("c");
+  graph["c"].insert("d");
+  graph["d"].insert("a");
+  std::vector<absl::btree_set<V>> sccs = StronglyConnectedComponents<V>(graph);
+  EXPECT_EQ(sccs.size(), 1);
+  EXPECT_EQ(FlattenSCCs(sccs).size(), GraphSize(graph));
+}
+
+TEST(StronglyConnectedComponentsTest, CycleWithSpike) {
+  absl::btree_map<V, absl::btree_set<V>> graph;
+  graph["a"].insert("b");
+  graph["b"].insert("c");
+  graph["c"].insert("d");
+  graph["d"].insert("a");
+  graph["a"].insert("spike0");
+  graph["a"].insert("spike1");
+  graph["a"].insert("spike2");
+  graph["a"].insert("spike3");
+  std::vector<absl::btree_set<V>> sccs = StronglyConnectedComponents<V>(graph);
+  EXPECT_EQ(sccs.size(), 5);
+  EXPECT_EQ(FlattenSCCs(sccs).size(), GraphSize(graph));
+}
+
+TEST(StronglyConnectedComponentsTest, Figure8) {
+  absl::btree_map<V, absl::btree_set<V>> graph;
+  graph["a"].insert("b1");
+  graph["b1"].insert("c1");
+  graph["c1"].insert("d1");
+  graph["d1"].insert("a");
+  graph["a"].insert("b2");
+  graph["b2"].insert("c2");
+  graph["c2"].insert("d2");
+  graph["d2"].insert("a");
+  std::vector<absl::btree_set<V>> sccs = StronglyConnectedComponents<V>(graph);
+  EXPECT_EQ(sccs.size(), 1);
+  EXPECT_EQ(FlattenSCCs(sccs).size(), GraphSize(graph));
+}
+
+TEST(StronglyConnectedComponentsTest, DisjointUnionOfCycles) {
+  absl::btree_map<V, absl::btree_set<V>> graph;
+  graph["a1"].insert("b1");
+  graph["b1"].insert("c1");
+  graph["c1"].insert("d1");
+  graph["d1"].insert("a1");
+  graph["a2"].insert("b2");
+  graph["b2"].insert("c2");
+  graph["c2"].insert("d2");
+  graph["d2"].insert("a2");
+  std::vector<absl::btree_set<V>> sccs = StronglyConnectedComponents<V>(graph);
+  EXPECT_EQ(sccs.size(), 2);
+  EXPECT_EQ(FlattenSCCs(sccs).size(), GraphSize(graph));
+}
+
+TEST(StronglyConnectedComponentsTest, BarBell) {
+  absl::btree_map<V, absl::btree_set<V>> graph;
+  graph["a1"].insert("b1");
+  graph["b1"].insert("c1");
+  graph["c1"].insert("d1");
+  graph["d1"].insert("a1");
+  graph["a2"].insert("b2");
+  graph["b2"].insert("c2");
+  graph["c2"].insert("d2");
+  graph["d2"].insert("a2");
+  graph["a1"].insert("a2");  // connect the two cycles with a directed edge
+  std::vector<absl::btree_set<V>> sccs = StronglyConnectedComponents<V>(graph);
+  EXPECT_EQ(sccs.size(), 2);
+  EXPECT_EQ(FlattenSCCs(sccs).size(), GraphSize(graph));
+}
+
+TEST(StronglyConnectedComponentsTest, Chain) {
+  absl::btree_map<V, absl::btree_set<V>> graph;
+  graph["a"].insert("b");
+  graph["b"].insert("c");
+  graph["c"].insert("d");
+  std::vector<absl::btree_set<V>> sccs = StronglyConnectedComponents<V>(graph);
+  EXPECT_EQ(sccs.size(), GraphSize(graph));
+  EXPECT_EQ(FlattenSCCs(sccs).size(), GraphSize(graph));
+}
+
+TEST(StronglyConnectedComponentsTest, Tree) {
+  absl::btree_map<V, absl::btree_set<V>> graph;
+  graph["root"].insert("l");
+  graph["root"].insert("r");
+  graph["l"].insert("ll");
+  graph["l"].insert("lr");
+  graph["lr"].insert("lrl");
+  graph["lr"].insert("lrr");
+  graph["r"].insert("rl");
+  graph["r"].insert("rr");
+  std::vector<absl::btree_set<V>> sccs = StronglyConnectedComponents<V>(graph);
+  EXPECT_EQ(sccs.size(), GraphSize(graph));
+  EXPECT_EQ(FlattenSCCs(sccs).size(), GraphSize(graph));
+}
+
+}  // namespace
+}  // namespace xls
