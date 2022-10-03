b4a0b8ecfe093e91d2033bb9b91d2ee7e83ac8c4
Łukasz Pater <lpater@google.com>
2022-10-03 01:23:12 -0700

    Cleanup: avoid referring to `std::vector<T>` members when `T` is incomplete.
    PiperOrigin-RevId: 478437172

--- xls/noc/simulation/network_graph.h
+++ xls/noc/simulation/network_graph.h
@@ -41,58 +41,86 @@ namespace noc {
 // Dangling ports/connections are possible,  in that case, the corresponding
 // connection/port has an invalid ID.
 //
-class NetworkComponent;
-class Connection;
-class Network;
-class Port;
+class NetworkManager;
 
-// A NetworkManager is responsible for storing all networks and serves
-// as the entry port for accessing and creating network graph objects.
-class NetworkManager {
+// A port is how a NetworkComponent connects to other components.
+class Port {
  public:
-  // Create a new network.
-  absl::StatusOr<NetworkId> CreateNetwork();
+  // Construct a Port object.
+  Port(PortId id, PortDirection dir) : id_(id), dir_(dir) {}
 
-  // Create a new network component under a network.
-  absl::StatusOr<NetworkComponentId> CreateNetworkComponent(
-      NetworkId network, NetworkComponentKind kind);
+  // Returns id of this Port.
+  PortId id() const { return id_; }
+
+  // Returns id of the connection attached to this Port.
+  ConnectionId connection() const { return connection_; }
+
+  // Returns direction of this Port.
+  PortDirection direction() const { return dir_; }
+
+  // Print to stdout information about this object.
+  void Dump(int indent_level = 0) const;
+
+  // Set connection of this Port.
+  //  - used by Connection::Attach()
+  void SetAttached(ConnectionId conn) { connection_ = conn; }
+
+  // Set connection of this Port to an invalid Connection.
+  //  - used by Connection::DetachSrc()
+  //  - used by Connection::DetachSink()
+  //  - used by Connection::Attach()
+  void SetDetached() { connection_ = ConnectionId::kInvalid; }
+
+ private:
+  PortId id_;
+  PortDirection dir_;  // Input or output.
+  ConnectionId connection_;
+};
+
+// A connection is a relationship between two ports.
+class Connection {
+ public:
+  // Construct a Connection object.
+  Connection(NetworkManager* mgr, ConnectionId id) : mgr_(mgr), id_(id) {}
+
+  // Returns id of this Connection.
+  ConnectionId id() const { return id_; }
+
+  // Returns src port id.
+  PortId src() const { return src_; }
+
+  // Returns sink port id.
+  PortId sink() const { return sink_; }
+
+  // Detaches this connection from its src port.
+  void DetachSrc();
+
+  // Detaches this connection from its sink port.
+  void DetachSink();
+
+  // Attaches this connection to the port, detaching it from the prior
+  // attached port if needed.
+  PortId Attach(PortId port);
+
+  // Print to stdout information about this object.
+  void Dump(int indent_level = 0) const;
+
+ private:
+  NetworkManager* mgr_;
+  ConnectionId id_;
+  PortId src_;
+  PortId sink_;
+};
+
+// A NetworkComponent is a specific network block and has ports.
+class NetworkComponent {
+ public:
+  // Construct a NetworkComponent object.
+  NetworkComponent(NetworkComponentId id, NetworkComponentKind kind)
+      : id_(id), kind_(kind) {}
 
   // Creates/adds a port to a network component.
-  absl::StatusOr<PortId> CreatePort(NetworkComponentId component,
-                                    PortDirection dir);
-
-  // Create a connection between two ports.
-  // Either from or to PortIds may be invalid to create a dangling connection.
-  absl::StatusOr<ConnectionId> CreateConnection(NetworkId network, PortId src,
-                                                PortId sink);
-
-  // Create a floating connection associated with a network.
-  // Afterwards, Attach() may be used to associate the connection
-  // with a port.
-  absl::StatusOr<ConnectionId> CreateConnection(NetworkId network);
-
-  // Associate a connection with a specific port.
-  // If necessary, this will dissociate the connection with its exiting
-  // port.
-  void Attach(ConnectionId conn, PortId port);
-
-  // Get Network object given an id.
-  Network& GetNetwork(NetworkId id);
-
-  // Get Network object given an id.
-  const Network& GetNetwork(NetworkId id) const;
-
-  // Get NetworkComponent object given an id.
-  NetworkComponent& GetNetworkComponent(NetworkComponentId id);
-
-  // Get NetworkComponent object given an id.
-  const NetworkComponent& GetNetworkComponent(NetworkComponentId id) const;
-
-  // Get Connection object given an id.
-  Connection& GetConnection(ConnectionId id);
-
-  // Get Connection object given an id.
-  const Connection& GetConnection(ConnectionId id) const;
+  absl::StatusOr<PortId> CreatePort(PortDirection dir);
 
   // Get Port object given an id.
   Port& GetPort(PortId id);
@@ -100,40 +128,51 @@ class NetworkManager {
   // Get Port object given an id.
   const Port& GetPort(PortId id) const;
 
-  // Get the id of the i-th network.
-  NetworkId GetNetworkIdByIndex(int64_t i) const { return NetworkId(i); }
+  // Returns id of this NetworkComponent.
+  NetworkComponentId id() const { return id_; }
 
-  // Get the object of the i-th network.
-  Network& GetNetworkByIndex(int64_t i) { return networks_[i]; }
+  // Returns kind (switch, link, etc...) of this NetworkCompnent.
+  NetworkComponentKind kind() const { return kind_; }
 
-  // Get the object of the i-th network.
-  const Network& GetNetworkByIndex(int64_t i) const { return networks_[i]; }
-
-  // Returns a vector of all managed network ids.
-  std::vector<NetworkId> GetNetworkIds() const {
-    std::vector<NetworkId> ret(networks_.size());
-    for (int64_t i = 0; i < networks_.size(); ++i) {
-      ret[i] = NetworkId(i);
-    }
-    return ret;
+  // Get the id of the i-th Port.
+  PortId GetPortIdByIndex(int64_t i) const {
+    return PortId(id_.GetNetworkId().id(),  // network
+                  id_.id(),                 // component
+                  i);
   }
 
-  // Returns a span of all network objects.
-  absl::Span<Network> GetNetworks() { return absl::MakeSpan(networks_); }
+  // Get the object of the i-th Port.
+  Port& GetPortByIndex(int64_t i) { return ports_[i]; }
 
-  // Returns a span of all network objects.
-  absl::Span<const Network> GetNetworks() const {
-    return absl::MakeSpan(networks_);
-  }
+  // Get the object of the i-th Port.
+  const Port& GetPortByIndex(int64_t i) const { return ports_[i]; }
 
-  // Count of networks managed by this object.
-  int64_t GetNetworkCount() const { return networks_.size(); }
+  // Returns a vector of all managed port ids.
+  std::vector<PortId> GetPortIds() const;
 
-  // Print to stdout information about the network graph.
+  // Returns a vector of all managed output port ids.
+  std::vector<PortId> GetOutputPortIds() const;
+
+  // Returns a vector of all managed output port ids.
+  std::vector<PortId> GetInputPortIds() const;
+
+  // Returns a span of all Port objects.
+  absl::Span<Port> GetPorts() { return absl::MakeSpan(ports_); }
+
+  // Returns a span of all Port objects.
+  absl::Span<const Port> GetPorts() const { return absl::MakeSpan(ports_); }
+
+  // Count of ports.
+  int64_t GetPortCount() const { return ports_.size(); }
+
+  // Print to stdout information about this object.
   void Dump(int indent_level = 0) const;
 
  private:
-  std::vector<Network> networks_;
+  NetworkComponentId id_;
+  NetworkComponentKind kind_;
+
+  std::vector<Port> ports_;
 };
 
 // A Network is responsible for storing connections and components.
@@ -269,15 +308,53 @@ class Network {
   std::vector<Connection> connections_;
 };
 
-// A NetworkComponent is a specific network block and has ports.
-class NetworkComponent {
+// A NetworkManager is responsible for storing all networks and serves
+// as the entry port for accessing and creating network graph objects.
+class NetworkManager {
  public:
-  // Construct a NetworkComponent object.
-  NetworkComponent(NetworkComponentId id, NetworkComponentKind kind)
-      : id_(id), kind_(kind) {}
+  // Create a new network.
+  absl::StatusOr<NetworkId> CreateNetwork();
+
+  // Create a new network component under a network.
+  absl::StatusOr<NetworkComponentId> CreateNetworkComponent(
+      NetworkId network, NetworkComponentKind kind);
 
   // Creates/adds a port to a network component.
-  absl::StatusOr<PortId> CreatePort(PortDirection dir);
+  absl::StatusOr<PortId> CreatePort(NetworkComponentId component,
+                                    PortDirection dir);
+
+  // Create a connection between two ports.
+  // Either from or to PortIds may be invalid to create a dangling connection.
+  absl::StatusOr<ConnectionId> CreateConnection(NetworkId network, PortId src,
+                                                PortId sink);
+
+  // Create a floating connection associated with a network.
+  // Afterwards, Attach() may be used to associate the connection
+  // with a port.
+  absl::StatusOr<ConnectionId> CreateConnection(NetworkId network);
+
+  // Associate a connection with a specific port.
+  // If necessary, this will dissociate the connection with its exiting
+  // port.
+  void Attach(ConnectionId conn, PortId port);
+
+  // Get Network object given an id.
+  Network& GetNetwork(NetworkId id);
+
+  // Get Network object given an id.
+  const Network& GetNetwork(NetworkId id) const;
+
+  // Get NetworkComponent object given an id.
+  NetworkComponent& GetNetworkComponent(NetworkComponentId id);
+
+  // Get NetworkComponent object given an id.
+  const NetworkComponent& GetNetworkComponent(NetworkComponentId id) const;
+
+  // Get Connection object given an id.
+  Connection& GetConnection(ConnectionId id);
+
+  // Get Connection object given an id.
+  const Connection& GetConnection(ConnectionId id) const;
 
   // Get Port object given an id.
   Port& GetPort(PortId id);
@@ -285,120 +362,40 @@ class NetworkComponent {
   // Get Port object given an id.
   const Port& GetPort(PortId id) const;
 
-  // Returns id of this NetworkComponent.
-  NetworkComponentId id() const { return id_; }
+  // Get the id of the i-th network.
+  NetworkId GetNetworkIdByIndex(int64_t i) const { return NetworkId(i); }
 
-  // Returns kind (switch, link, etc...) of this NetworkCompnent.
-  NetworkComponentKind kind() const { return kind_; }
+  // Get the object of the i-th network.
+  Network& GetNetworkByIndex(int64_t i) { return networks_[i]; }
 
-  // Get the id of the i-th Port.
-  PortId GetPortIdByIndex(int64_t i) const {
-    return PortId(id_.GetNetworkId().id(),  // network
-                  id_.id(),                 // component
-                  i);
+  // Get the object of the i-th network.
+  const Network& GetNetworkByIndex(int64_t i) const { return networks_[i]; }
+
+  // Returns a vector of all managed network ids.
+  std::vector<NetworkId> GetNetworkIds() const {
+    std::vector<NetworkId> ret(networks_.size());
+    for (int64_t i = 0; i < networks_.size(); ++i) {
+      ret[i] = NetworkId(i);
+    }
+    return ret;
   }
 
-  // Get the object of the i-th Port.
-  Port& GetPortByIndex(int64_t i) { return ports_[i]; }
+  // Returns a span of all network objects.
+  absl::Span<Network> GetNetworks() { return absl::MakeSpan(networks_); }
 
-  // Get the object of the i-th Port.
-  const Port& GetPortByIndex(int64_t i) const { return ports_[i]; }
+  // Returns a span of all network objects.
+  absl::Span<const Network> GetNetworks() const {
+    return absl::MakeSpan(networks_);
+  }
 
-  // Returns a vector of all managed port ids.
-  std::vector<PortId> GetPortIds() const;
+  // Count of networks managed by this object.
+  int64_t GetNetworkCount() const { return networks_.size(); }
 
-  // Returns a vector of all managed output port ids.
-  std::vector<PortId> GetOutputPortIds() const;
-
-  // Returns a vector of all managed output port ids.
-  std::vector<PortId> GetInputPortIds() const;
-
-  // Returns a span of all Port objects.
-  absl::Span<Port> GetPorts() { return absl::MakeSpan(ports_); }
-
-  // Returns a span of all Port objects.
-  absl::Span<const Port> GetPorts() const { return absl::MakeSpan(ports_); }
-
-  // Count of ports.
-  int64_t GetPortCount() const { return ports_.size(); }
-
-  // Print to stdout information about this object.
+  // Print to stdout information about the network graph.
   void Dump(int indent_level = 0) const;
 
  private:
-  NetworkComponentId id_;
-  NetworkComponentKind kind_;
-
-  std::vector<Port> ports_;
-};
-
-// A port is how a NetworkComponent connects to other components.
-class Port {
- public:
-  // Construct a Port object.
-  Port(PortId id, PortDirection dir) : id_(id), dir_(dir) {}
-
-  // Returns id of this Port.
-  PortId id() const { return id_; }
-
-  // Returns id of the connection attached to this Port.
-  ConnectionId connection() const { return connection_; }
-
-  // Returns direction of this Port.
-  PortDirection direction() const { return dir_; }
-
-  // Print to stdout information about this object.
-  void Dump(int indent_level = 0) const;
-
-  // Set connection of this Port.
-  //  - used by Connection::Attach()
-  void SetAttached(ConnectionId conn) { connection_ = conn; }
-
-  // Set connection of this Port to an invalid Connection.
-  //  - used by Connection::DetachSrc()
-  //  - used by Connection::DetachSink()
-  //  - used by Connection::Attach()
-  void SetDetached() { connection_ = ConnectionId::kInvalid; }
-
- private:
-  PortId id_;
-  PortDirection dir_;  // Input or output.
-  ConnectionId connection_;
-};
-
-// A connection is a relationship between two ports.
-class Connection {
- public:
-  // Construct a Connection object.
-  Connection(NetworkManager* mgr, ConnectionId id) : mgr_(mgr), id_(id) {}
-
-  // Returns id of this Connection.
-  ConnectionId id() const { return id_; }
-
-  // Returns src port id.
-  PortId src() const { return src_; }
-
-  // Returns sink port id.
-  PortId sink() const { return sink_; }
-
-  // Detaches this connection from its src port.
-  void DetachSrc();
-
-  // Detaches this connection from its sink port.
-  void DetachSink();
-
-  // Attaches this connection to the port, detaching it from the prior
-  // attached port if needed.
-  PortId Attach(PortId port);
-
-  // Print to stdout information about this object.
-  void Dump(int indent_level = 0) const;
-
- private:
-  NetworkManager* mgr_;
-  ConnectionId id_;
-  PortId src_;
-  PortId sink_;
+  std::vector<Network> networks_;
 };
 
 }  // namespace noc
