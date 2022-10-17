b4ba77d40255b20299a84c04734af89d30f74d25
Mark Heffernan <meheff@google.com>
2022-10-17 10:02:19 -0700

    Preemptively zero out buffer prior to filling with value data in JitRuntime. This is a cleaner more efficient solution than what was done before. No functional change.
    PiperOrigin-RevId: 481669518

--- xls/jit/jit_runtime.cc
+++ xls/jit/jit_runtime.cc
@@ -143,6 +143,9 @@ Value JitRuntime::UnpackBufferInternal(const uint8_t* buffer,
 void JitRuntime::BlitValueToBuffer(const Value& value, const Type* type,
                                    absl::Span<uint8_t> buffer) {
   absl::MutexLock lock(&mutex_);
+  // Zero the buffer before filling in values. This ensures all padding bytes
+  // are cleared.
+  memset(buffer.data(), 0, type_converter_->GetTypeByteSize(type));
   BlitValueToBufferInternal(value, type, buffer);
 }
 
@@ -154,17 +157,15 @@ void JitRuntime::BlitValueToBufferInternal(const Value& value, const Type* type,
     bits.ToBytes(absl::MakeSpan(buffer.data(), byte_count),
                  data_layout_.isBigEndian());
 
-    // Zero out any padding bits and bytes. Bits type are stored in the JIT as
-    // the next power of two size.  LLVM requires all padding bits to be zero
-    // for safe operation, e.g., for a 42 bit type (which is padded out to a
-    // 64-bit word in the jit) bits 42 through 64 must be zero.
+    // Zero out any padding bits. Bits type are stored in the JIT as the next
+    // power of two size.  LLVM requires all padding bits to be zero for safe
+    // operation, e.g., for a 42 bit type (which is padded out to a 64-bit word
+    // in the jit) bits 42 through 64 must be zero. The entire buffer is zeroed
+    // prior to calling this function so we only need to handle the remainder
+    // bits in the most-significant byte of value data here.
     int remainder_bits = bits.bit_count() % kCharBit;
     if (remainder_bits != 0) {
       buffer[byte_count - 1] &= static_cast<uint8_t>(Mask(remainder_bits));
-    }
-    int64_t allocated_size = type_converter_->GetTypeByteSize(type);
-    for (int64_t i = byte_count; i < allocated_size; ++i) {
-      buffer[i] = 0;
     }
   } else if (value.IsArray()) {
     const ArrayType* array_type = type->AsArrayOrDie();
