64f0cdd07a610c680c25162b98fb22f3b93f7108
Vincent Mirian <vmirian@google.com>
2022-11-30 01:12:48 -0800

    Makes the code emitted by XLS (codegen) default to System Verilog, placing the code into a file with a '.sv' extension. Non-System-Verilog code (e.g. Verilog) is emitted into a file with '.v' extension.
    Update affected targets. This is an interim step of a cleanup phase.
    
    PiperOrigin-RevId: 491847236

--- xls/contrib/xlscc/examples/mux3_v_test.cc
+++ xls/contrib/xlscc/examples/mux3_v_test.cc
@@ -51,7 +51,7 @@ void IOSignalsPresent(std::string_view verilog_text) {
 }
 
 TEST(Mux3VerilogTest, BasicCombSignaturePresent) {
-  constexpr const char kVerPath[] = "xls/contrib/xlscc/examples/mux3_comb_v.v";
+  constexpr const char kVerPath[] = "xls/contrib/xlscc/examples/mux3_comb_v.sv";
 
   {
     SCOPED_TRACE("BasicCombSignaturePresent");
@@ -69,7 +69,7 @@ TEST(Mux3VerilogTest, BasicCombSignaturePresent) {
 
 TEST(Mux3VerilogTest, BasicPipelineSignaturePresent) {
   constexpr const char kVerPath[] =
-      "xls/contrib/xlscc/examples/mux3_stages_5_v.v";
+      "xls/contrib/xlscc/examples/mux3_stages_5_v.sv";
 
   {
     SCOPED_TRACE("BasicPipelineSignaturePresent");
