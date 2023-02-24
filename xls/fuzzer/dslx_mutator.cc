d2cfe53e4fab49906a75f1cd62e1365512a45d02
XLS Team <noreply@google.com>
2023-02-24 11:13:36 -0800

    Function and unit tests to mutate DSLX by deleting a random token.
    PiperOrigin-RevId: 512116545

---
+++ xls/fuzzer/dslx_mutator.cc
@@ -0,0 +1,49 @@
+// Copyright 2023 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/fuzzer/dslx_mutator.h"
+
+#include "absl/random/random.h"
+#include "xls/common/status/status_macros.h"
+#include "xls/dslx/scanner.h"
+
+using absl::InvalidArgumentError;
+using absl::StatusOr;
+
+namespace xls::dslx {
+namespace {
+std::string TokensToString(absl::Span<const Token> tokens) {
+  std::string result;
+  for (const Token& t : tokens) {
+    result += t.ToString();
+  }
+  return result;
+}
+}  // namespace
+
+StatusOr<std::string> RemoveDslxToken(std::string_view dslx,
+                                      absl::BitGenRef gen) {
+  if (dslx.empty()) {
+    return InvalidArgumentError("dslx is empty");
+  }
+  Scanner scan("x.x", std::string(dslx),
+               /*include_whitespace_and_comments=*/true);
+  XLS_ASSIGN_OR_RETURN(std::vector<Token> tokens, scan.PopAll());
+  int64_t index_to_remove =
+      absl::Uniform(gen, 0, static_cast<int64_t>(tokens.size()));
+  tokens.erase(tokens.begin() + index_to_remove);
+  return TokensToString(tokens);
+}
+
+}  // namespace xls::dslx
---
+++ xls/fuzzer/dslx_mutator_test.cc
@@ -0,0 +1,52 @@
+// Copyright 2023 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/fuzzer/dslx_mutator.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/random/mock_distributions.h"
+#include "absl/random/random.h"
+#include "absl/status/status.h"
+#include "xls/common/status/matchers.h"
+
+using testing::_;
+using testing::HasSubstr;
+using testing::Return;
+using xls::status_testing::IsOkAndHolds;
+using xls::status_testing::StatusIs;
+
+namespace xls::dslx {
+namespace {
+
+TEST(DslxMutator, RemoveDslxToken) {
+  absl::MockingBitGen always_zero;
+  ON_CALL(absl::MockUniform<int64_t>(), Call(always_zero, _, _))
+      .WillByDefault(Return(0));
+
+  EXPECT_THAT(RemoveDslxToken("", always_zero),
+              StatusIs(absl::StatusCode::kInvalidArgument, HasSubstr("empty")));
+  EXPECT_THAT(RemoveDslxToken(":", always_zero), IsOkAndHolds(""));
+  EXPECT_THAT(RemoveDslxToken("+5", always_zero), IsOkAndHolds("5"));
+
+  absl::MockingBitGen always_two;
+  ON_CALL(absl::MockUniform<int64_t>(), Call(always_two, _, _))
+      .WillByDefault(Return(2));
+  // "add" is the third token, it should be deleted.
+  EXPECT_THAT(RemoveDslxToken("fn add(x: u32, y: u32) -> u32 {", always_two),
+              IsOkAndHolds("fn (x: u32, y: u32) -> u32 {"));
+}
+
+}  // namespace
+}  // namespace xls::dslx
