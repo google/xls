3ee6f512572818d0ff244efe059443a7abcfec58
Henner Zeller <hzeller@google.com>
2023-07-19 17:03:29 -0700

    Add 'Instantiations' headline in generated code.
    Instantiations are currently always emitted at the end of
    the block, not tied to the pipeline stages they might be
    active in. This can be confusing when manually inspecting
    the generated code. So add a headline that is always emitted
    whenever there are instantiations.
    
    PiperOrigin-RevId: 549465984

--- xls/codegen/block_generator.cc
+++ xls/codegen/block_generator.cc
@@ -687,6 +687,12 @@ class BlockGenerator {
   // Emit each instantation in the block into the separate instantation module
   // section.
   absl::Status EmitInstantiations() {
+    // Since instantiations are emitted at the end, and not the pipeline stages
+    // they are in, separate with headline to reduce confusion.
+    if (!block_->GetInstantiations().empty()) {
+      mb_.instantiation_section()->Add<Comment>(SourceInfo(),
+                                                "===== Instantiations");
+    }
     for (xls::Instantiation* instantiation : block_->GetInstantiations()) {
       std::vector<Connection> connections;
       for (InstantiationInput* input :
