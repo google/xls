10dd322c6daf2ff406254703d18f22abe205c781
XLS Team <noreply@google.com>
2023-08-31 22:28:51 -0700

    Internal Code Change
    PiperOrigin-RevId: 561855675

--- xls/common/strerror.cc
+++ xls/common/strerror.cc
@@ -45,7 +45,7 @@ std::string Strerror(int error_num) {
 
   if constexpr (kXsiCompliant) {
     if (strerror_r(error_num, buffer, kBufferSize) !=
-        static_cast<strerror_r_type>(0)) {
+        static_cast<strerror_r_type>(nullptr)) {
       return absl::StrFormat(
           "Unknown error, strerror_r failed. error number %d", error_num);
     }
