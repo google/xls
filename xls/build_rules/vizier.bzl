8365d3101c13dc8f4661a23072e2c9fd5f637e10
Pawel Czarnecki <pczarnecki@antmicro.com>
2023-10-05 16:16:48 +0200

    Add vizier proof of concept integration
    Internal-tag: [#50339]
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

---
+++ xls/build_rules/vizier.bzl
@@ -0,0 +1,175 @@
+load("//xls/build_rules:xls_build_defs.bzl", "xls_ir_verilog")
+load("@rules_hdl//place_and_route:build_defs.bzl", "place_and_route")
+load("@rules_hdl//synthesis:build_defs.bzl", "synthesize_rtl")
+load("@rules_hdl//verilog:providers.bzl", "verilog_library")
+load("@rules_python//python:defs.bzl", "py_binary", "py_test")
+
+def generate_compression_block_parameter_optimization(name, optimized_ir, cocotb_test, test_parser = "//xls/tools/dashboard:generic_parser", test_log_file = "/tmp/test_log.txt", opt_eng_iterations = 5, opt_eng_suggestions = 1, opt_eng_algorithm = "NSGA2"):
+    """Generate rules responsible for optimizing parameters of (de)compression
+    block written in DSLX. This is done through Vizier blackbox optimization
+    interface.
+
+    Arguments:
+
+    * name - string used for codegen 'module_name' attribute, used for
+             constructing names for the rules
+    * optimized_ir - DSLX (de)compression block passed in form of a label
+                     string pointing to the optimized IR of the design.
+    * cocotb_test - path to cocotb test used for evaluating the performance of
+                    (de)compression block
+    * test_parser - label for passing the parser for test output
+    * test_log_file - path to temporary log file with test output
+    * opt_eng_iterations - max iteration count for the optimization engine
+    * opt_eng_suggestions - max suggestion count per iteration
+    * opt_eng_algorithm - algorithm for the optimization engine
+
+    This macro instantiates the following rules:
+    # │ rule            │ name                                            │ visibility
+    ──┼─────────────────┼─────────────────────────────────────────────────┼────────────
+    1.│ xls_ir_verilog  │ <name>_compression_block_verilog                │ private
+    2.│ verilog_library │ <name>_compression_block_verilog_lib            │ private
+    3.│ synthesize_rtl  │ <name>_compression_block_synth                  │ private
+    4.│ place_and_route │ <name>_compression_block_place_and_route        │ default
+    5.│ py_test         │ <name>_compression_block_cocotb_test            │ default
+    5.│ vizier_optimize │ <name>_compression_block_parameter_optimization │ default
+
+    Most of the generated rules require setting the following environment variables
+    when building or running the generated rules:
+
+    * PIPELINE_STAGES - Number of pipeline stages to use in verilog generation.
+                        Decimal in string form.
+    * CLOCK_PERIOD - The amount of time a single clock period lasts in nanoseconds.
+                     Decimal in string form.
+    * PLACEMENT_DENSITY - How densely cells should be packaged on the die during
+                          global placement. Float in string form, between 0.0
+                          and 1.0.
+    * TARGET_DIE_UTILIZATION_PERCENTAGE - Sets the die area based on an estimated
+                                          die area target utilization. Decimal
+                                          in string form, between 0 and 100.
+
+    Environment variables should be set for building/running every rule generated
+    by this macro except `vizier_optimize`. This should be done in bazel command
+    line with argument '--action_env VAR=value', e.g.:
+
+        ```
+        bazel build --action_env PIPELINE_STAGES=1
+                    --action_env CLOCK_PERIOD=1
+                    --action_env PLACEMENT_DENSITY=0.8
+                    --action_env TARGET_DIE_UTILIZATION_PERCENTAGE=30
+                      //xls/modules/rle:rle_enc_compression_block_cocotb_test
+        ```
+
+    Rule `vizier_optimize` is a top-level rule used for executing vizier
+    optimization framework. The rule starts `vizier_runner` python script
+    responsible for running the optimization engine on given set of parameters,
+    performance metrics and DSLX design. It doesn't need specifying environment
+    viariables.
+    """
+
+    xls_ir_verilog(
+        name = name + "_compression_block_verilog",
+        src = optimized_ir,
+        codegen_args = {
+            "module_name": name,
+            "delay_model": "unit",
+            "pipeline_stages": "$PIPELINE_STAGES",
+            "reset": "rst",
+            "use_system_verilog": "false",
+            "streaming_channel_data_suffix": "_data",
+        },
+        verilog_file = name + "_compression_block.v",
+        visibility = ["//visibility:private"],
+    )
+
+    verilog_library(
+        name = name + "_compression_block_verilog_lib",
+        srcs = [
+            ":" + name + "_compression_block.v",
+        ],
+        visibility = ["//visibility:private"],
+    )
+
+    synthesize_rtl(
+        name = name + "_compression_block_synth",
+        standard_cells = "@org_theopenroadproject_asap7//:asap7_rvt_1x",
+        top_module = name,
+        deps = [
+            ":" + name + "_compression_block_verilog_lib",
+        ],
+        visibility = ["//visibility:private"],
+    )
+
+    place_and_route(
+        name = name + "_compression_block_place_and_route",
+        clock_period = "$env(CLOCK_PERIOD)",
+        core_padding_microns = 5,
+        placement_density = "$env(PLACEMENT_DENSITY)",
+        synthesized_rtl = ":" + name + "_compression_block_synth",
+        target_die_utilization_percentage = "$env(TARGET_DIE_UTILIZATION_PERCENTAGE)",
+    )
+
+    py_test(
+        name = name + "_compression_block_cocotb_test",
+        srcs = [cocotb_test],
+        main = cocotb_test,
+        data = [
+            ":" + name + "_compression_block_verilog",
+            "@com_icarus_iverilog//:iverilog",
+            "@com_icarus_iverilog//:vvp",
+        ],
+        deps = [
+            "//xls/common:runfiles",
+            "//xls/common:test_base",
+            "//xls/simulation/cocotb:cocotb_struct",
+            "//xls/simulation/cocotb:cocotb_xls",
+            "//xls/tools/dashboard:utils",
+        ],
+        imports = ["."],
+    )
+
+    test_label = native.package_name() + ":" + name + "_compression_block_cocotb_test"
+    vizier_optimize(
+        name = name + "_compression_block_parameter_optimization",
+        test = test_label,
+        parser = test_parser,
+        log_file = test_log_file,
+        iterations = opt_eng_iterations,
+        suggestions = opt_eng_suggestions,
+        algorithm = opt_eng_algorithm,
+    )
+
+def _vizier_optimize_impl(ctx):
+    executable = ctx.actions.declare_file("{}_run.sh".format(ctx.attr.name))
+    command = "python {} --root-dir {} --test-label \"{}\" --parser {} --log-file {} --iterations {} --suggestions {} --algorithm {}".format(ctx.executable._run.short_path, "$BUILD_WORKSPACE_DIRECTORY", ctx.attr.test, ctx.executable.parser.path, ctx.attr.log_file, ctx.attr.iterations, ctx.attr.suggestions, ctx.attr.algorithm)
+
+    ctx.actions.write(output = executable, content = command)
+
+    return [
+        DefaultInfo(
+            executable = executable,
+            runfiles = ctx.attr._run.default_runfiles.merge(ctx.attr.parser.default_runfiles),
+        ),
+    ]
+
+vizier_optimize = rule(
+    implementation = _vizier_optimize_impl,
+    executable = True,
+    attrs = {
+        "test": attr.string(doc = "Label string for python script with cocotb test"),
+        "parser": attr.label(cfg = "exec", executable = True, default = "//xls/tools/dashboard:generic_parser", doc = "Label for passing the parser for test output"),
+        "log_file": attr.string(default = "/tmp/test_log.txt", doc = "path to temporary log file with test output"),
+        "iterations": attr.int(default = 5, doc = "Max iteration count for the optimization engine"),
+        "suggestions": attr.int(default = 1, doc = "Max suggestion count per iteration"),
+        "algorithm": attr.string(default = "NSGA2", values = ["GAUSSIAN_PROCESS_BANDIT", "RANDOM_SEARCH", "QUASI_RANDOM_SEARCH", "GRID_SEARCH", "SHUFFLED_GRID_SEARCH", "EAGLE_STRATEGY", "NSGA2", "BOCS", "HARMONICA"], doc = "Algorithm for the optimization engine"),
+        "_run": attr.label(
+            cfg = "exec",
+            executable = True,
+            default = Label("//xls/tools:vizier_runner"),
+        ),
+    },
+)
+"""Runs the `vizier_runner` python script
+
+This rule runs `vizier_runner` script which executes the vizier parameter optimization
+for specified DSLX (de)compression block.
+"""
---
+++ xls/tools/vizier_runner.py
@@ -0,0 +1,247 @@
+from vizier import service
+from vizier.service import clients
+from vizier.service import pyvizier as vz
+from vizier.service.pyvizier import SearchSpaceSelector, MetricInformation, ObjectiveMetricGoal, MetricsConfig
+from vizier.service import servers
+from xls.tools.dashboard.run_and_parse import BazelLabel, SystemPath, parse_file
+from pathlib import Path
+import argparse
+import subprocess
+import json
+from dataclasses import dataclass
+from typing import List, Dict
+
+@dataclass
+class Parameter:
+    """
+    Data type used for wrapping XLS parameter into optimization parameter.
+
+    DSLX design parameters and implementation tool parameters can be specified
+    as instances of this class and then used in generic functions for integrating
+    those into optimization engine.
+
+    Attributes
+    ----------
+    type : type
+        Type of the parameter
+    name : str
+        Name of the parameter. Will be used by optimization engine and to construct
+        environment variables for passing the parameters to bazel rules
+    min_value: int or float
+    max_value: int or float
+        Constraints for the parameter. Will reduce the size of the search space.
+    """
+    type: type
+    name: str
+    min_value: int | float
+    max_value: int | float
+
+@dataclass
+class TestData:
+    """
+    Data type used for storing data required for running tests which evaluate
+    parameter values.
+
+    Attributes
+    ----------
+    label : BazelLabel
+        Bazel Label passed in string format which points to a python binary with
+        test capable of obtaining performance metrics from implemented DSLX design.
+    parser : SystemPath or str
+        Path to parser responsible for extracting the performance metrics from test output.
+    log : SystemPath or str
+        Path to temporary log file used for storing test output for parsing.
+    """
+    label: BazelLabel
+    parser: SystemPath | str
+    log: SystemPath | str
+
+def construct_parameters() -> List[Parameter]:
+    """Specify a list of parameters to optimize and constraints for the search space."""
+    parameters = []
+
+    parameters.append(Parameter(name="PIPELINE_STAGES", type=int, min_value=1, max_value=5))
+    parameters.append(Parameter(name="CLOCK_PERIOD", type=int, min_value=1, max_value=100))
+    parameters.append(Parameter(name="TARGET_DIE_UTILIZATION_PERCENTAGE", type=int, min_value=0, max_value=100))
+    parameters.append(Parameter(name="PLACEMENT_DENSITY", type=float, min_value=0.0, max_value=1.0))
+
+    return parameters
+
+def construct_performance_metrics() -> List[MetricInformation]:
+    """Specify a list of performance metrics used in parameter optmization process"""
+    metrics = []
+
+    metrics.append(MetricInformation(name='chip_area', goal=ObjectiveMetricGoal.MINIMIZE))
+    metrics.append(MetricInformation(name='delay', goal=ObjectiveMetricGoal.MINIMIZE))
+    metrics.append(MetricInformation(name='throughput', goal=ObjectiveMetricGoal.MAXIMIZE))
+
+    return metrics
+
+def add_performance_metrics(metrics: List[MetricInformation], metric_information: MetricsConfig) -> None:
+    """Configure the optimization engine with given performance metrics"""
+    metric_information.extend(metrics)
+
+def add_search_space_parameters(parameters: List[Parameter], search_space_root: SearchSpaceSelector) -> None:
+    """Configure the parameters to be optimized by the optimization engine"""
+    for param in parameters:
+        if (param.type == int):
+            search_space_root.add_int_param(name=param.name, min_value=param.min_value, max_value=param.max_value)
+        elif (param.type == float):
+            search_space_root.add_float_param(name=param.name, min_value=param.min_value, max_value=param.max_value)
+        else:
+            raise ValueError("Unsupported parameter type: {}".format(param.type))
+
+def prepare_env(env) -> None:
+    """Explicitly cast values of integer parameters to int.
+
+    This is required because the optimization engine suggest float values even
+    for parameters which were defined as int parameters in `construct_performance_metrics()`."""
+    for var, value in env.items():
+        if (var != "PLACEMENT_DENSITY"):
+            env[var] = int(value)
+
+def construct_bazel_command(test: BazelLabel, test_log: SystemPath, env) -> str:
+    """Specify command for implementing and testing DSLX design
+
+    Create two separate bazel commands, one for build, second for running tests
+    and chain those into single command.
+    Each bazel call should pass the parameter values as environmet variables
+    to bazel build system through `--action-env` argument.
+    Write test output to the `test_log`.
+    """
+    build_cmd = "bazel build"
+    test_cmd = "bazel test"
+    for var, value in env.items():
+        set_env_var = " --action_env " + var + "=" + str(value)
+        build_cmd += set_env_var
+        test_cmd += set_env_var
+    return f"{build_cmd} {test} && {test_cmd} {test} --test_output=all > {test_log}"
+
+def implement_and_test_design(cmd: str, cwd: SystemPath) -> None:
+    """Run command line for implementing and testing DSLX design"""
+    try:
+        subprocess.check_call(cmd, shell=True, cwd=cwd)
+    except subprocess.CalledProcessError:
+        raise Exception(
+           f"Error while executing Bazel test with the follwing command:\n{cmd}"
+        )
+
+def get_objective(test: TestData, root_dir) -> Dict:
+    """Fetch performance metrics into structure consumable for the optimization engine."""
+    results = parse_file(test.parser, test.log, root_dir)
+    json_obj = json.loads(results)
+
+    # Extract performance metrics from dashboard JSON
+    delay = json_obj[0]["value"][1][1]
+    throughput = json_obj[0]["value"][2][1]
+
+    objective = {
+        'chip_area': 0,
+        'delay': delay,
+        'throughput': throughput
+    }
+
+    return objective
+
+def evaluate(env, root_dir, test: TestData) -> Dict:
+    """Use suggested parameter values to obtain performance metrics
+    """
+    prepare_env(env) # Explicitly cast int parameters to integer type - this is probably Vizier bug, those parameters should be casted before being suggested
+    bazel_command = construct_bazel_command(test.label, test.log, env)
+    implement_and_test_design(bazel_command, root_dir)
+
+    return get_objective(test, root_dir)
+
+def setup_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("-r", "--root-directory", type=str, help="Root directory", required=True)
+    parser.add_argument("-t", "--test-label", type=str, help="Bazel label string for running tests", required=True)
+    parser.add_argument("-p", "--parser", type=str, help="Test output parser", required=True)
+    parser.add_argument("-l", "--log-file", type=str, help="Path to log file for the test output", default="/tmp/test_log.txt")
+    parser.add_argument("-i", "--iterations", type=int, help="Max iteration count for the optimization engine", default=5)
+    parser.add_argument("-s", "--suggestions", type=int, help="Suggestion count per iteration of the optimization engine", default=1)
+    parser.add_argument("-a", "--algorithm", type=str, choices=['GAUSSIAN_PROCESS_BANDIT', 'RANDOM_SEARCH', 'QUASI_RANDOM_SEARCH', 'GRID_SEARCH', 'SHUFFLED_GRID_SEARCH', 'EAGLE_STRATEGY', 'NSGA2', 'BOCS', 'HARMONICA'], help="Algorithm for the optimization engine", default='NSGA2')
+
+    return parser.parse_args()
+
+def check_args(args: argparse.Namespace) -> None:
+    """Validate arguments passed to the parameter optimization script."""
+    root_dir = Path(args.root_directory)
+    if not root_dir.is_dir():
+        raise FileNotFoundError(f"Root directory {str(root_dir)} does not exist")
+
+    parser_path = root_dir / args.parser
+    if not parser_path.exists():
+        raise FileNotFoundError(f"Output parser {str(parser_path)} does not exist")
+
+    if (args.iterations < 1):
+        raise ValueError(f"Iteration count must be above 0 (got {str(args.iterations)})")
+
+    if (args.suggestions < 1):
+        raise ValueError(f"Suggestions count must be above 0 (got {str(args.suggestions)})")
+
+def client_runner(client: 'Study', test_data: TestData, root_dir, iteration_cnt: int, suggestion_cnt: int) -> None:
+    """
+    Vizier Study Client performs parameter optimization.
+
+    Run `iteration_cnt` iterations in which `suggestion_cnt` suggestions are generated.
+    Each suggestion is a set of parameter values which are then evaluated for
+    the performance metrics.
+    """
+    for i in range(iteration_cnt):
+        suggestions = client.suggest(count=suggestion_cnt)
+        for s, suggestion in enumerate(suggestions):
+            objective = evaluate(suggestion.parameters, root_dir, test_data)
+            print(f'Iteration {i}, suggestion {s} with parameters: {suggestion.parameters} led to objective value: {objective}.')
+            final_measurement = vz.Measurement(objective)
+            suggestion.complete(final_measurement)
+    client.set_state(vz.StudyState.COMPLETED)
+
+def print_client_optimal_trials(client: 'Study'):
+    """Print information gathered by the client in optimization process."""
+
+    if (client.materialize_state() != vz.StudyState.COMPLETED):
+        raise RuntimeError(f"Client {client.resource_name} status is not COMPLETED yet")
+
+    for optimal_trial in client.optimal_trials():
+        optimal_trial = optimal_trial.materialize()
+        print(f"Client {client.resource_name} Optimal Trial Suggestion: {optimal_trial.parameters} and Objective: {optimal_trial.final_measurement}")
+
+def main() -> None:
+    args = setup_args()
+    check_args(args)
+
+    root_dir = Path(args.root_directory)
+    parser = root_dir / args.parser
+    test_data = TestData(args.test_label, parser, args.log_file)
+
+    print(f"Setting Optimization engine with parameters: {args}")
+    problem = vz.ProblemStatement()
+
+    parameters = construct_parameters()
+    add_search_space_parameters(parameters, problem.search_space.root)
+
+    performance_metrics = construct_performance_metrics()
+    add_performance_metrics(performance_metrics, problem.metric_information)
+
+    study_config = vz.StudyConfig.from_problem(problem)
+    study_config.algorithm = args.algorithm
+
+    # Vizier Server setup
+    server_host = "localhost"
+    server_database = service.SQL_LOCAL_URL
+    server = servers.DefaultVizierServer(host=server_host, database_url=server_database)
+    print(f"Started Vizier Server at: {server.endpoint}")
+    print(f"SQL database file located at: {server._database_url}")
+
+    # Vizier Client setup
+    clients.environment_variables.server_endpoint = server.endpoint
+    study_client = clients.Study.from_study_config(study_config, owner='owner', study_id='example_study_id')
+
+    client_runner(study_client, test_data, root_dir, args.iterations, args.suggestions)
+    print_client_optimal_trials(study_client)
+
+if __name__ == "__main__":
+    main()
+
+
