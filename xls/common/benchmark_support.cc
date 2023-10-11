1e669d448d9c2be8756f0bb549b756ba36275aae
Alex Light <allight@google.com>
2023-10-11 14:11:40 -0700

    Automated rollback of commit 85879778695f1eaf03313f3407688fe17ee5b995.
    PiperOrigin-RevId: 572682815

---
+++ xls/common/benchmark_support.cc
@@ -0,0 +1,49 @@
+// Copyright 2023 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/common/benchmark_support.h"
+
+#include <optional>
+#include <string>
+
+#include "benchmark/benchmark.h"
+#include "absl/flags/flag.h"
+
+// Workaround absl and benchmark:: fighting for flags.
+// TODO(https://github.com/google/xls/issues/1150): 2023-11-10 We should try to
+// remove this workaround at some point.
+ABSL_FLAG(std::optional<std::string>, benchmark_filter, std::nullopt,
+          "regex to select benchmarks to run");
+
+namespace xls {
+
+void RunSpecifiedBenchmarks() {
+  // Match BENCHMARK_MAIN macro definition of the benchmark MAIN function.
+  // We don't want to run benchmarks unless some are actually requested and we
+  // want to always run gtests so we can't use the standard BENCHMARK_MAIN
+  // macro. This replicates the parts that configure benchmarks and run them
+  // with our filters.
+  int fake_argc = 1;
+  char fake_argv[] = "benchmark";
+  char* fake_argv_ptr = &fake_argv[0];
+  benchmark::Initialize(&fake_argc, &fake_argv_ptr);
+
+  // Only run benchmarks if requested.
+  if (absl::GetFlag(FLAGS_benchmark_filter)) {
+    benchmark::SetBenchmarkFilter(*absl::GetFlag(FLAGS_benchmark_filter));
+    benchmark::RunSpecifiedBenchmarks();
+  }
+  benchmark::Shutdown();
+}
+}  // namespace xls
--- xls/common/gunit_init_xls.h
+++ xls/common/gunit_init_xls.h
@@ -1,4 +1,4 @@
-// Copyright 2020 The XLS Authors
+// Copyright 2023 The XLS Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,22 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef XLS_COMMON_INIT_XLS_H_
-#define XLS_COMMON_INIT_XLS_H_
+#ifndef XLS_COMMON_GUNIT_INIT_XLS_H_
+#define XLS_COMMON_GUNIT_INIT_XLS_H_
 
 #include <string_view>
 #include <vector>
 
 namespace xls {
 
-// Initializes global state in the binary, including things like command line
+// Initializes global state in the test, including things like command line
 // flags, symbolizer etc. This function might exit the program, for example if
 // the command line flags are invalid or if a '--help' command line argument was
 // provided.
 //
 // Is typically called early on in main().
 //
-// `usage` provides a short usage message passed to
+// It must be called after InitGoogleTest
+//
+// `usage` provides a short usage message usable by
 // absl::SetProgramUsageMessage().
 //
 // `argc` and `argv` are the command line flags to parse. This function does not
@@ -37,15 +39,9 @@ namespace xls {
 // command-line flag (or arguments to a flag), not including the program
 // invocation name. (This includes positional arguments after the
 // flag-terminating delimiter '--'.)
-std::vector<std::string_view> InitXls(std::string_view usage,
-                                      int argc, char* argv[]);
+std::vector<std::string_view> InitXlsForTest(std::string_view usage, int argc,
+                                             char* argv[]);
 
-namespace internal {
-// Internal function which sets up post-absl common components shared by both
-// testing and non-testing inits. Should only be called by InitXls or
-// InitXlsForTest.
-void InitXlsPostAbslFlagParse();
-}  // namespace internal
 }  // namespace xls
 
-#endif  // XLS_COMMON_INIT_XLS_H_
+#endif  // XLS_COMMON_GUNIT_INIT_XLS_H_
