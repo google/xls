8fd8795fd3ded05842865a570b78adb02d003aef
Alex Light <allight@google.com>
2023-10-13 17:15:32 -0700

    Make a switchable function jit
    Sometimes due to LLVM updates the jit might temporarially be non-functional. Add a target tests and external users can use which can be manually made to fallback to an interpreter in cases where the jit is non-functional.
    
    PiperOrigin-RevId: 573354735

---
+++ xls/jit/switchable_function_jit.cc
@@ -0,0 +1,148 @@
+// Copyright 2023 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/jit/switchable_function_jit.h"
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/types/span.h"
+#include "xls/common/logging/logging.h"
+#include "xls/common/status/ret_check.h"
+#include "xls/common/status/status_macros.h"
+#include "xls/interpreter/ir_interpreter.h"
+#include "xls/ir/events.h"
+#include "xls/ir/function.h"
+#include "xls/ir/node.h"
+#include "xls/ir/nodes.h"
+#include "xls/ir/value.h"
+#include "xls/jit/function_jit.h"
+#include "xls/jit/observer.h"
+
+namespace xls {
+
+#if EMERGENCY_FORCE_INTERPRETER == 1
+#pragma clang diagnostic_push
+#pragma clang diagnostic warning "-W#warnings"
+#warning XLS JIT is currently disabled via EMERGENCY_FORCE_INTERPRETER and by \
+default won't be used.
+constexpr ExecutionType kRealDefaultExecutionType = ExecutionType::kInterpreter;
+#pragma clang diagnostic_pop
+#else
+constexpr ExecutionType kRealDefaultExecutionType = ExecutionType::kJit;
+#endif
+
+absl::StatusOr<std::unique_ptr<SwitchableFunctionJit>>
+SwitchableFunctionJit::CreateJit(Function* xls_function, int64_t opt_level,
+                                 JitObserver* observer) {
+  XLS_ASSIGN_OR_RETURN(auto jit,
+                       FunctionJit::Create(xls_function, opt_level, observer));
+  return std::unique_ptr<SwitchableFunctionJit>(new SwitchableFunctionJit(
+      xls_function, /*use_jit=*/true, std::move(jit)));
+}
+
+absl::StatusOr<std::unique_ptr<SwitchableFunctionJit>>
+SwitchableFunctionJit::CreateInterpreter(Function* xls_function) {
+  return std::unique_ptr<SwitchableFunctionJit>(
+      new SwitchableFunctionJit(xls_function, /*use_jit=*/false, nullptr));
+}
+
+absl::StatusOr<std::unique_ptr<SwitchableFunctionJit>>
+SwitchableFunctionJit::Create(Function* xls_function, ExecutionType execution,
+                              int64_t opt_level, JitObserver* observer) {
+  if (execution == ExecutionType::kDefault) {
+    execution = kRealDefaultExecutionType;
+    XLS_LOG_IF(WARNING, execution == ExecutionType::kInterpreter)
+        << "Interpreter is being used in place of JIT.";
+  }
+  switch (execution) {
+    case ExecutionType::kInterpreter:
+      return SwitchableFunctionJit::CreateInterpreter(xls_function);
+    case ExecutionType::kJit:
+      return SwitchableFunctionJit::CreateJit(xls_function, opt_level,
+                                              observer);
+    case ExecutionType::kDefault:
+      XLS_LOG(FATAL) << "Unreachable";
+  }
+}
+
+namespace {
+absl::StatusOr<absl::flat_hash_map<Node*, Value>> ToValueMap(
+    absl::Span<const Value> args, Function* f) {
+  absl::flat_hash_map<Node*, Value> res;
+  XLS_RET_CHECK_EQ(args.size(), f->params().size())
+      << "Wrong number of parameters";
+  int64_t i = 0;
+  for (Param* p : f->params()) {
+    res.emplace(p, args.at(i++));
+  }
+  return res;
+}
+
+absl::StatusOr<absl::flat_hash_map<Node*, Value>> ToValueMap(
+    const absl::flat_hash_map<std::string, Value>& args, Function* f) {
+  absl::flat_hash_map<Node*, Value> res;
+  for (Param* p : f->params()) {
+    XLS_RET_CHECK(args.contains(p->name()))
+        << "No value for param called '" << p->name() << "' given!";
+    res.emplace(p, args.at(p->name()));
+  }
+  return res;
+}
+
+class MemoizedIrInterpreter : public IrInterpreter {
+ public:
+  using IrInterpreter::IrInterpreter;
+  absl::Status HandleParam(Param* param) override {
+    XLS_RET_CHECK(NodeValuesMap().contains(param));
+    return absl::OkStatus();
+  }
+};
+
+absl::StatusOr<InterpreterResult<Value>> Interpret(
+    absl::flat_hash_map<Node*, Value> values, Function* function) {
+  InterpreterEvents e;
+  Node* return_val = function->return_value();
+  InterpreterResult<Value> res;
+  MemoizedIrInterpreter interp(&values, &res.events);
+  XLS_RETURN_IF_ERROR(function->Accept(&interp));
+  res.value = values.at(return_val);
+  return res;
+}
+}  // namespace
+
+absl::StatusOr<InterpreterResult<Value>> SwitchableFunctionJit::Run(
+    absl::Span<const Value> args) {
+  if (use_jit_) {
+    return function_jit_->Run(args);
+  }
+  XLS_ASSIGN_OR_RETURN(auto node_args, ToValueMap(args, function()));
+  return Interpret(std::move(node_args), function());
+}
+
+absl::StatusOr<InterpreterResult<Value>> SwitchableFunctionJit::Run(
+    const absl::flat_hash_map<std::string, Value>& kwargs) {
+  if (use_jit_) {
+    return function_jit_->Run(kwargs);
+  }
+  XLS_ASSIGN_OR_RETURN(auto node_args, ToValueMap(kwargs, function()));
+  return Interpret(std::move(node_args), function());
+}
+
+}  // namespace xls
---
+++ xls/jit/switchable_function_jit.h
@@ -0,0 +1,92 @@
+// Copyright 2023 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef XLS_JIT_SWITCHABLE_FUNCTION_JIT_H_
+#define XLS_JIT_SWITCHABLE_FUNCTION_JIT_H_
+
+#include <cstdint>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/types/span.h"
+#include "xls/common/status/ret_check.h"
+#include "xls/ir/events.h"
+#include "xls/ir/function.h"
+#include "xls/ir/value.h"
+#include "xls/jit/function_jit.h"
+#include "xls/jit/observer.h"
+
+namespace xls {
+
+enum class ExecutionType {
+  kDefault,
+  kJit,
+  kInterpreter,
+};
+
+// A wrapper for the jit structures that can be turned off at build time if
+// issues arise with the LLVM jit. This class tries to match FunctionJit
+// interface as much as possible.
+// TODO(google/xls#1151): 2023-10-13 Implement the rest of the FunctionJit API.
+class SwitchableFunctionJit {
+ public:
+  // Returns an object containing a host-compiled version of the specified XLS
+  // function.
+  static absl::StatusOr<std::unique_ptr<SwitchableFunctionJit>> CreateJit(
+      Function* xls_function, int64_t opt_level = 3,
+      JitObserver* observer = nullptr);
+  static absl::StatusOr<std::unique_ptr<SwitchableFunctionJit>>
+  CreateInterpreter(Function* xls_function);
+  static absl::StatusOr<std::unique_ptr<SwitchableFunctionJit>> Create(
+      Function* xls_function, ExecutionType execution = ExecutionType::kDefault,
+      int64_t opt_level = 3, JitObserver* observer = nullptr);
+
+  // Executes the compiled function with the specified arguments.
+  absl::StatusOr<InterpreterResult<Value>> Run(absl::Span<const Value> args);
+
+  // As above, buth with arguments as key-value pairs.
+  absl::StatusOr<InterpreterResult<Value>> Run(
+      const absl::flat_hash_map<std::string, Value>& kwargs);
+
+  // TODO(google/xls#1151): 2023-10-13 Match more of the function-jit api.
+
+  // Returns the function that the JIT executes.
+  Function* function() { return xls_function_; }
+
+  std::optional<FunctionJit*> function_jit() {
+    if (use_jit_) {
+      return function_jit_.get();
+    }
+    return std::nullopt;
+  }
+
+ private:
+  explicit SwitchableFunctionJit(Function* xls_function, bool use_jit,
+                                 std::unique_ptr<FunctionJit>&& jit)
+      : xls_function_(xls_function),
+        use_jit_(use_jit),
+        function_jit_(std::move(jit)) {}
+
+  Function* xls_function_;
+  bool use_jit_;
+  std::unique_ptr<FunctionJit> function_jit_;
+};
+}  // namespace xls
+
+#endif  // XLS_JIT_SWITCHABLE_FUNCTION_JIT_H_
