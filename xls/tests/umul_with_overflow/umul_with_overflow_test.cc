88f9931bf27a6c62a47fedce14baaec53a95f676
Eric Astor <epastor@google.com>
2023-10-20 06:19:24 -0700

    Migrate `umul_with_overflow_test` to GoogleFuzzTest
    PiperOrigin-RevId: 575199214

--- xls/tests/umul_with_overflow/umul_with_overflow_test.cc
+++ xls/tests/umul_with_overflow/umul_with_overflow_test.cc
@@ -16,8 +16,7 @@
 #include <memory>
 
 #include "gtest/gtest.h"
-#include "rapidcheck/gtest.h"
-#include "rapidcheck.h"
+#include "fuzztest/fuzztest.h"
 #include "xls/common/status/matchers.h"
 #include "xls/ir/value_view.h"
 #include "xls/tests/umul_with_overflow/umul_with_overflow_21_21_18_jit_wrapper.h"
@@ -68,17 +67,17 @@ TEST(UmulOverflowTest, Smoke35Test) {
   EXPECT_EQ(product, 72);
 }
 
-RC_GTEST_PROP(UmulOverflowRapidcheck, UmulOverflow21Test, ()) {
+constexpr int64_t kMaxUint21 = (int64_t{1} << 21) - 1;
+constexpr int64_t kMaxUint18 = (int64_t{1} << 18) - 1;
+
+void UmulOverflow212118WorksAsExpected(int64_t x, int64_t y) {
   XLS_ASSERT_OK_AND_ASSIGN(std::unique_ptr<xls::test::UmulWithOverflow212118> f,
                            xls::test::UmulWithOverflow212118::Create());
-
-  int64_t x = *rc::gen::inRange(0, 0x20'0000).as("x");
-  int64_t y = *rc::gen::inRange(0, 0x4'0000).as("y");
-  uint8_t result[8] = {0};
 
   xls::BitsView<21> x_view(reinterpret_cast<uint8_t*>(&x));
   xls::BitsView<18> y_view(reinterpret_cast<uint8_t*>(&y));
 
+  uint8_t result[8] = {0};
   xls::MutableTupleView<xls::MutableBitsView<1>, xls::MutableBitsView<21>>
       result_view(result);
   XLS_ASSERT_OK(f->Run(x_view, y_view, result_view));
@@ -87,24 +86,27 @@ RC_GTEST_PROP(UmulOverflowRapidcheck, UmulOverflow21Test, ()) {
   int64_t product = result_view.Get<1>().GetValue();
 
   int64_t actual_product = x * y;
-  int64_t expected_product = actual_product & 0x1fffff;
+  int64_t expected_product = actual_product & kMaxUint21;
   bool expected_overflow = actual_product != expected_product;
 
-  RC_ASSERT(overflow == expected_overflow);
-  RC_ASSERT(product == expected_product);
+  EXPECT_EQ(overflow, expected_overflow);
+  EXPECT_EQ(product, expected_product);
 }
+FUZZ_TEST(UmulOverflowFuzzTest, UmulOverflow212118WorksAsExpected)
+    .WithDomains(fuzztest::InRange<int64_t>(0, kMaxUint21),
+                 fuzztest::InRange<int64_t>(0, kMaxUint18));
 
-RC_GTEST_PROP(UmulOverflowRapidcheck, UmulOverflow35Test, ()) {
+constexpr int64_t kMaxUint35 = (int64_t{1} << 35) - 1;
+constexpr int64_t kMaxUint32 = (int64_t{1} << 32) - 1;
+
+void UmulOverflow353218WorksAsExpected(int64_t x, int64_t y) {
   XLS_ASSERT_OK_AND_ASSIGN(std::unique_ptr<xls::test::UmulWithOverflow353218> f,
                            xls::test::UmulWithOverflow353218::Create());
-
-  int64_t x = *rc::gen::inRange(0l, 0x1'0000'0000l).as("x");
-  int64_t y = *rc::gen::inRange(0l, 0x4'0000l).as("y");
-  uint8_t result[16] = {0};
 
   xls::BitsView<32> x_view(reinterpret_cast<uint8_t*>(&x));
   xls::BitsView<18> y_view(reinterpret_cast<uint8_t*>(&y));
 
+  uint8_t result[16] = {0};
   xls::MutableTupleView<xls::MutableBitsView<1>, xls::MutableBitsView<35>>
       result_view(result);
   XLS_ASSERT_OK(f->Run(x_view, y_view, result_view));
@@ -113,12 +115,15 @@ RC_GTEST_PROP(UmulOverflowRapidcheck, UmulOverflow35Test, ()) {
   int64_t product = result_view.Get<1>().GetValue();
 
   int64_t actual_product = x * y;
-  int64_t expected_product = actual_product & 0x7'ffff'ffff;
+  int64_t expected_product = actual_product & kMaxUint35;
   bool expected_overflow = actual_product != expected_product;
 
-  RC_ASSERT(overflow == expected_overflow);
-  RC_ASSERT(product == expected_product);
+  EXPECT_EQ(overflow, expected_overflow);
+  EXPECT_EQ(product, expected_product);
 }
+FUZZ_TEST(UmulOverflowFuzzTest, UmulOverflow353218WorksAsExpected)
+    .WithDomains(fuzztest::InRange<int64_t>(0, kMaxUint32),
+                 fuzztest::InRange<int64_t>(0, kMaxUint18));
 
 }  // namespace
 }  // namespace xls
