google/xls

commit 4c166ed6f009677afd85b2f0b5582594003409c3

Simplify shift-right of constant 1; reduce to comparison to zero
PiperOrigin-RevId: 579884648

xls/passes/arith_simplification_pass.cc
@@ -15,6 +15,7 @@
 #include "xls/passes/arith_simplification_pass.h"
 
 #include <algorithm>
+#include <cstdint>
 #include <functional>
 #include <optional>
 #include <tuple>
@@ -25,10 +26,13 @@
 #include "xls/common/status/ret_check.h"
 #include "xls/common/status/status_macros.h"
 #include "xls/ir/big_int.h"
+#include "xls/ir/bits.h"
 #include "xls/ir/bits_ops.h"
 #include "xls/ir/node_iterator.h"
 #include "xls/ir/node_util.h"
 #include "xls/ir/nodes.h"
+#include "xls/ir/op.h"
+#include "xls/ir/value.h"
 #include "xls/ir/value_helpers.h"
 #include "xls/passes/optimization_pass.h"
 #include "xls/passes/pass_base.h"
@@ -927,6 +931,25 @@ absl::StatusOr<bool> MatchArithPatterns(int64_t opt_level, Node* n) {
     XLS_VLOG(2) << "FOUND: replace ashr by constant with signext(slice(x))";
     XLS_RETURN_IF_ERROR(
         n->ReplaceUsesWithNew<ExtendOp>(slice, bit_count, Op::kSignExt)
+            .status());
+    return true;
+  }
+
+  // Any shift right of a constant 1 can be replaced by a test for equality with
+  // zero (followed by a zero extension), since
+  //   (1 >> x) = 1 if x == 0,
+  //              0 otherwise.
+  if ((n->op() == Op::kShra || n->op() == Op::kShrl) &&
+      IsLiteralUnsignedOne(n->operand(0))) {
+    XLS_VLOG(2) << "FOUND: shift right of constant 1";
+    Node* x = n->operand(1);
+    XLS_ASSIGN_OR_RETURN(Literal * zero,
+                         n->function_base()->MakeNode<Literal>(
+                             n->loc(), Value(UBits(0, x->BitCountOrDie()))));
+    XLS_ASSIGN_OR_RETURN(Node * test, n->function_base()->MakeNode<CompareOp>(
+                                          n->loc(), x, zero, Op::kEq));
+    XLS_RETURN_IF_ERROR(
+        n->ReplaceUsesWithNew<ExtendOp>(test, n->BitCountOrDie(), Op::kZeroExt)
             .status());
     return true;
   }

xls/passes/arith_simplification_pass_test.cc
@@ -793,6 +793,34 @@ TEST_F(ArithSimplificationPassTest, CompareBoolAgainstZero) {
   EXPECT_THAT(f->return_value(), m::Not(m::Param()));
 }
 
+TEST_F(ArithSimplificationPassTest, ArithmeticShiftRightOfConstantOne) {
+  auto p = CreatePackage();
+  XLS_ASSERT_OK_AND_ASSIGN(Function * f, ParseFunction(R"(
+     fn f(x:bits[10]) -> bits[42] {
+        literal.1: bits[42] = literal(value=1)
+        ret result: bits[42] = shra(literal.1, x)
+     }
+  )",
+                                                       p.get()));
+  EXPECT_THAT(f->return_value(), m::Shra());
+  ASSERT_THAT(Run(p.get()), IsOkAndHolds(true));
+  EXPECT_THAT(f->return_value(), m::ZeroExt(m::Eq(m::Param(), m::Literal(0))));
+}
+
+TEST_F(ArithSimplificationPassTest, LogicalShiftRightOfConstantOne) {
+  auto p = CreatePackage();
+  XLS_ASSERT_OK_AND_ASSIGN(Function * f, ParseFunction(R"(
+     fn f(x:bits[10]) -> bits[42] {
+        literal.1: bits[42] = literal(value=1)
+        ret result: bits[42] = shrl(literal.1, x)
+     }
+  )",
+                                                       p.get()));
+  EXPECT_THAT(f->return_value(), m::Shrl());
+  ASSERT_THAT(Run(p.get()), IsOkAndHolds(true));
+  EXPECT_THAT(f->return_value(), m::ZeroExt(m::Eq(m::Param(), m::Literal(0))));
+}
+
 TEST_F(ArithSimplificationPassTest, DoubleNegation) {
   auto p = CreatePackage();
   XLS_ASSERT_OK_AND_ASSIGN(Function * f, ParseFunction(R"(

xls/passes/BUILD
@@ -372,8 +372,11 @@ cc_library(
         "//xls/common/status:status_macros",
         "//xls/ir",
         "//xls/ir:big_int",
+        "//xls/ir:bits",
         "//xls/ir:bits_ops",
         "//xls/ir:node_util",
+        "//xls/ir:op",
+        "//xls/ir:value",
         "//xls/ir:value_helpers",
     ],
 )

commit a636436760c24e33faa8c8a1fd976712d22187f2

Make sure our optimizer handles arithmetic shift-right of one-bit operands
This is a no-op operation - but if we try to pattern-match arithmetic shift-right too generally (as we did before), we can get into trouble unless this is already removed.

Fixes a bug introduced in https://github.com/google/xls/commit/4c166ed6f009677afd85b2f0b5582594003409c3

PiperOrigin-RevId: 580299232

xls/passes/arith_simplification_pass.cc
@@ -935,12 +935,26 @@ absl::StatusOr<bool> MatchArithPatterns(int64_t opt_level, Node* n) {
     return true;
   }
 
+  // An arithmetic shift-right of a 1-bit value is a no-op.
+  if (n->op() == Op::kShra && n->operand(0)->BitCountOrDie() == 1) {
+    XLS_VLOG(2) << "FOUND: arithmetic shift-right of 1-bit value";
+    XLS_RETURN_IF_ERROR(n->ReplaceUsesWith(n->operand(0)));
+    return true;
+  }
+
   // Any shift right of a constant 1 can be replaced by a test for equality with
   // zero (followed by a zero extension), since
+  //
   //   (1 >> x) = 1 if x == 0,
   //              0 otherwise.
+  //
+  // ... as long as it's not an arithmetic shift-right of bits[1]:1.
   if ((n->op() == Op::kShra || n->op() == Op::kShrl) &&
       IsLiteralUnsignedOne(n->operand(0))) {
+    // Make absolutely sure we're not dealing with an arithmetic shift-right of
+    // bits[1]:1. (For correctness, that case must be handled first.)
+    XLS_RET_CHECK(n->op() != Op::kShra || n->operand(0)->BitCountOrDie() > 1);
+
     XLS_VLOG(2) << "FOUND: shift right of constant 1";
     Node* x = n->operand(1);
     XLS_ASSIGN_OR_RETURN(Literal * zero,
@@ -948,9 +962,13 @@ absl::StatusOr<bool> MatchArithPatterns(int64_t opt_level, Node* n) {
                              n->loc(), Value(UBits(0, x->BitCountOrDie()))));
     XLS_ASSIGN_OR_RETURN(Node * test, n->function_base()->MakeNode<CompareOp>(
                                           n->loc(), x, zero, Op::kEq));
-    XLS_RETURN_IF_ERROR(
-        n->ReplaceUsesWithNew<ExtendOp>(test, n->BitCountOrDie(), Op::kZeroExt)
-            .status());
+    if (n->BitCountOrDie() == 1) {
+      XLS_RETURN_IF_ERROR(n->ReplaceUsesWith(test));
+    } else {
+      XLS_RETURN_IF_ERROR(n->ReplaceUsesWithNew<ExtendOp>(
+                               test, n->BitCountOrDie(), Op::kZeroExt)
+                              .status());
+    }
     return true;
   }
 
@@ -1017,8 +1035,8 @@ absl::StatusOr<bool> MatchArithPatterns(int64_t opt_level, Node* n) {
   //
   //   shift(x, K) = shift(x, width(x)) for all K >= width(x).
   //
-  // This transformation can be performed for any value of LIMIT greater than or
-  // equal to width(x).
+  // This transformation can be performed for any value of LIMIT greater than
+  // or equal to width(x).
   if (n->op() == Op::kShll || n->op() == Op::kShrl || n->op() == Op::kShra) {
     std::optional<ClampExpr> clamp_expr = MatchClampUpperLimit(n->operand(1));
     if (clamp_expr.has_value() &&
@@ -1114,11 +1132,11 @@ absl::StatusOr<bool> MatchArithPatterns(int64_t opt_level, Node* n) {
     return true;
   }
 
-  // An unsigned comparison against a literal mask of LSBs (e.g., 0b0001111) can
-  // be simplified:
+  // An unsigned comparison against a literal mask of LSBs (e.g., 0b0001111)
+  // can be simplified:
   //
-  //    x < 0b0001111  =>  or_reduce(msb_slice(x)) NOR and_reduce(lsb_slice(x))
-  //    x > 0b0001111  =>  or_reduce(msb_slice(x))
+  //    x < 0b0001111  =>  or_reduce(msb_slice(x)) NOR
+  //    and_reduce(lsb_slice(x)) x > 0b0001111  =>  or_reduce(msb_slice(x))
   //
   //   x <= 0b0001111  =>  nor_reduce(msb_slice(x))
   //   x >= 0b0001111  =>  or_reduce(msb_slice(x)) OR and_reduce(lsb_slice(x))

xls/passes/arith_simplification_pass_test.cc
@@ -821,6 +821,33 @@ TEST_F(ArithSimplificationPassTest, LogicalShiftRightOfConstantOne) {
   EXPECT_THAT(f->return_value(), m::ZeroExt(m::Eq(m::Param(), m::Literal(0))));
 }
 
+TEST_F(ArithSimplificationPassTest, ArithmeticShiftRightOfOneBitUnknown) {
+  auto p = CreatePackage();
+  XLS_ASSERT_OK_AND_ASSIGN(Function * f, ParseFunction(R"(
+     fn f(x:bits[10], y:bits[1]) -> bits[1] {
+        ret result: bits[1] = shra(y, x)
+     }
+  )",
+                                                       p.get()));
+  EXPECT_THAT(f->return_value(), m::Shra());
+  ASSERT_THAT(Run(p.get()), IsOkAndHolds(true));
+  EXPECT_THAT(f->return_value(), m::Param("y"));
+}
+
+TEST_F(ArithSimplificationPassTest, ArithmeticShiftRightOfOneBitOne) {
+  auto p = CreatePackage();
+  XLS_ASSERT_OK_AND_ASSIGN(Function * f, ParseFunction(R"(
+     fn f(x:bits[10]) -> bits[1] {
+        literal.1: bits[1] = literal(value=1)
+        ret result: bits[1] = shra(literal.1, x)
+     }
+  )",
+                                                       p.get()));
+  EXPECT_THAT(f->return_value(), m::Shra());
+  ASSERT_THAT(Run(p.get()), IsOkAndHolds(true));
+  EXPECT_THAT(f->return_value(), m::Literal(1));
+}
+
 TEST_F(ArithSimplificationPassTest, DoubleNegation) {
   auto p = CreatePackage();
   XLS_ASSERT_OK_AND_ASSIGN(Function * f, ParseFunction(R"(