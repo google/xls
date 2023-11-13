3e4f17e7708e69e21fc0fdc922292a78a9e75a70
Mark Heffernan <meheff@google.com>
2023-11-13 13:37:11 -0800

    Minor tweaks to LLVM fuzzer debug script. Builds and runs sample with inlining only. This is useful for filing LLVM bugs as it results in a more concise IR sample.
    PiperOrigin-RevId: 582068427

--- xls/fuzzer/debug/run.sh
+++ xls/fuzzer/debug/run.sh
@@ -13,9 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Script for helping isolate LLVM JIT failures and suspected LLVM bugs. Takes
+# XLS IR input and uses the JIT to generate LLVM IR which is invoked in several
+# ways (with/without optimizations).
+#
+# Usage:
+#  (1) Set SRC_TOP to top of source tree.
+#  (2) Point XLS_IR to the XLS IR sample and set XLS_INPUT to the
+#      value which triggers the issue.
+#  (3) Tweak main.c to pass-in and return appropriate arguments/return value.
+#  (4) Run script. Artifacts will be dumped in a newly created temp directory.
+
 XLS_IR=test.ir
 XLS_INPUT="bits[8]:0xef;bits[16]:0x1234"
 MAIN_C=main.c
+LLVM_OPT_LEVEL=2
 
 SRC_TOP=
 if [[ -z ${SRC_TOP} ]]; then
@@ -24,48 +36,65 @@ if [[ -z ${SRC_TOP} ]]; then
 fi
 BIN=${SRC_TOP}/bazel-bin
 
-LLVM_BIN_DIR=
+LLVM_BIN_DIR=${SRC_TOP}/bazel-bin/llvm/llvm-project/llvm
 if [[ -z ${LLVM_BIN_DIR} ]]; then
   echo "LLVM_BIN_DIR must be set."
   echo "When built from source this is the bin subdirectory in the LLVM "
   echo "build directory. Internal to Google, this the directory "
-  echo "containing the artifacts from building llvm/llvm-project/llvm:all."
+  echo "containing the artifacts from "
+  echo "building llvm/llvm-project/llvm:all."
   exit 1
 fi
 
-LLVM_OPTS=-O0
-
 CLANG=/usr/bin/clang
 
-echo "=== Building XLS tools..."
+echo "=== Building tools..."
 cd ${SRC_TOP}
-bazel build -c opt xls/tools:all
+bazel build -c opt xls/tools:all llvm/llvm-project/llvm:all
 cd -
 
-OUTDIR=`mktemp -d`
+OUTDIR=`mktemp -d xls_fuzzer_debug.XXXXXX --tmpdir`
 echo "=== Output directory: ${OUTDIR}"
 
 echo "=== Evaluating XLS IR and dumping LLVM IR..."
-LLVM_IR=${OUTDIR}/test.ll
-LLVM_OPT_IR=${OUTDIR}/test.opt.ll
+DUMPED_LLVM_IR=${OUTDIR}/test.ll
+DUMPED_LLVM_OPT_IR=${OUTDIR}/jitopt.test.ll
 ${BIN}/xls/tools/eval_ir_main \
-  --input=${XLS_INPUT} \
+  --input="${XLS_INPUT}" \
   --use_llvm_jit \
-  --llvm_jit_ir_output=${LLVM_IR} \
-  --llvm_jit_opt_ir_output=${LLVM_OPT_IR} \
+  --llvm_jit_ir_output=${DUMPED_LLVM_IR} \
+  --llvm_jit_opt_ir_output=${DUMPED_LLVM_OPT_IR} \
+  --llvm_opt_level=${LLVM_OPT_LEVEL} \
   ${XLS_IR}
 
-echo "=== Building assembly and object file from LLVM IR..."
-TEST_S=${OUTDIR}/test.s
-TEST_O=${OUTDIR}/test.o
-${LLVM_BIN_DIR}/llc ${LLVM_IR} ${LLVM_OPTS} -filetype=asm -o ${TEST_S}
-${LLVM_BIN_DIR}/llc ${LLVM_IR} ${LLVM_OPTS} -filetype=obj -o ${TEST_O}
+echo "=== Optimizing LLVM IR"
+OPT_LLVM_IR=${OUTDIR}/opt.test.ll
+INLINE_LLVM_IR=${OUTDIR}/inlined.test.ll
+OPT_INLINE_LLVM_IR=${OUTDIR}/opt.inlined.test.ll
+${LLVM_BIN_DIR}/opt ${DUMPED_LLVM_IR} -O${LLVM_OPT_LEVEL} -S > ${OPT_LLVM_IR}
+${LLVM_BIN_DIR}/opt ${DUMPED_LLVM_IR} -passes=inline,dce -S > ${INLINE_LLVM_IR}
+${LLVM_BIN_DIR}/opt ${INLINE_LLVM_IR} -O${LLVM_OPT_LEVEL} -S > ${OPT_INLINE_LLVM_IR}
 
-echo "== Building binary..."
-${CLANG} ${TEST_O} ${MAIN_C} -o ${OUTDIR}/main
+function build_and_run() {
+  echo "=== $2 [$1]: Building assembly and object file from LLVM IR..."
+  LLVM_IR=${OUTDIR}/$1.ll
+  TEST_S=${OUTDIR}/$1.s
+  TEST_O=${OUTDIR}/$1.o
 
-echo "== Running binary..."
-${OUTDIR}/main
+  ${LLVM_BIN_DIR}/llc ${LLVM_IR} -O${LLVM_OPT_LEVEL} -filetype=asm -o ${TEST_S}
+  ${LLVM_BIN_DIR}/llc ${LLVM_IR} -O${LLVM_OPT_LEVEL} -filetype=obj -o ${TEST_O}
+
+  echo "=== $2 [$1]: Building binary..."
+  ${CLANG} ${TEST_O} ${MAIN_C} -o ${OUTDIR}/$1.main
+
+  echo "=== $2 [$1]: Running binary..."
+  ${OUTDIR}/$1.main
+}
+
+build_and_run "test" "Unoptimized"
+build_and_run "opt.test" "Optimized at -O${LLVM_OPT_LEVEL}"
+build_and_run "inlined.test" "Only inlined"
+build_and_run "opt.inlined.test" "Inlined then optimized at -O${LLVM_OPT_LEVEL}"
 
 # Commands for generating a LLVM-IR-only implementation and evaluating with lli.
 # echo "== Compiling main to LLVM IR..."
