8d8d75a3d622eec0999badbfe4161c9a8f92a9a9
Chris Leary <cdleary@gmail.com>
2024-04-20 13:35:50 -0700

    Start of C API, amenable to FFI binding.

---
+++ xls/public/c_api.cc
@@ -0,0 +1,85 @@
+// Copyright 2024 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/public/c_api.h"
+
+#include "xls/public/runtime_build_actions.h"
+
+namespace {
+
+std::vector<std::filesystem::path> ToCpp(const char* additional_search_paths[],
+                                         size_t additional_search_paths_count) {
+  std::vector<std::filesystem::path> additional_search_paths_cpp;
+  additional_search_paths_cpp.reserve(additional_search_paths_count);
+  for (size_t i = 0; i < additional_search_paths_count; ++i) {
+    const char* additional_search_path = additional_search_paths[i];
+    CHECK(additional_search_path != nullptr);
+    additional_search_paths_cpp.push_back(additional_search_path);
+  }
+  return additional_search_paths_cpp;
+}
+
+char* ToOwnedCString(const std::string& s) { return strdup(s.c_str()); }
+
+}  // namespace
+
+extern "C" {
+
+bool ConvertDslxToIr(const char* dslx, const char* path,
+                     const char* module_name, const char* dslx_stdlib_path,
+                     const char* additional_search_paths[],
+                     size_t additional_search_paths_count, char** error_out,
+                     char** ir_out) {
+  CHECK(dslx != nullptr);
+  CHECK(path != nullptr);
+  CHECK(dslx_stdlib_path != nullptr);
+
+  std::vector<std::filesystem::path> additional_search_paths_cpp =
+      ToCpp(additional_search_paths, additional_search_paths_count);
+
+  absl::StatusOr<std::string> result = xls::ConvertDslxToIr(
+      dslx, path, module_name, dslx_stdlib_path, additional_search_paths_cpp);
+  if (result.ok()) {
+    *ir_out = ToOwnedCString(result.value());
+    return true;
+  }
+
+  *ir_out = nullptr;
+  *error_out = ToOwnedCString(result.status().ToString());
+  return false;
+}
+
+bool ConvertDslxPathToIr(const char* path, const char* dslx_stdlib_path,
+                         const char* additional_search_paths[],
+                         size_t additional_search_paths_count, char** error_out,
+                         char** ir_out) {
+  CHECK(path != nullptr);
+  CHECK(dslx_stdlib_path != nullptr);
+
+  std::vector<std::filesystem::path> additional_search_paths_cpp =
+      ToCpp(additional_search_paths, additional_search_paths_count);
+
+  absl::StatusOr<std::string> result = xls::ConvertDslxPathToIr(
+      path, dslx_stdlib_path, additional_search_paths_cpp);
+  if (result.ok()) {
+    *ir_out = ToOwnedCString(result.value());
+    return true;
+  }
+
+  *ir_out = nullptr;
+  *error_out = ToOwnedCString(result.status().ToString());
+  return false;
+}
+
+}  // extern "C"
---
+++ xls/public/c_api_test.cc
@@ -0,0 +1,106 @@
+// Copyright 2024 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/public/c_api.h"
+
+#include <string>
+
+#include "absl/cleanup/cleanup.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "xls/common/file/filesystem.h"
+#include "xls/common/file/temp_directory.h"
+#include "xls/common/status/matchers.h"
+#include "xls/dslx/default_dslx_stdlib_path.h"
+
+namespace {
+
+using testing::HasSubstr;
+
+// Smoke test for ConvertDslxToIr C API.
+TEST(XlsCApiTest, ConvertDslxToIrSimple) {
+  const std::string kProgram = "fn id(x: u32) -> u32 { x }";
+  const char* additional_search_paths[] = {};
+  char* error_out = nullptr;
+  char* ir_out = nullptr;
+  bool ok = ConvertDslxToIr(kProgram.c_str(), "my_module.x", "my_module",
+                            /*dslx_stdlib_path=*/xls::kDefaultDslxStdlibPath,
+                            additional_search_paths, 0, &error_out, &ir_out);
+
+  absl::Cleanup free_cstrs([&] {
+    free(error_out);
+    free(ir_out);
+  });
+
+  // We should get IR and no error.
+  ASSERT_TRUE(ok);
+  ASSERT_EQ(error_out, nullptr);
+  ASSERT_NE(ir_out, nullptr);
+
+  EXPECT_THAT(ir_out, HasSubstr("fn __my_module__id"));
+}
+
+TEST(XlsCApiTest, ConvertDslxToIrError) {
+  const std::string kInvalidProgram = "@!";
+  const char* additional_search_paths[] = {};
+  char* error_out = nullptr;
+  char* ir_out = nullptr;
+
+  absl::Cleanup free_cstrs([&] {
+    free(error_out);
+    free(ir_out);
+  });
+
+  bool ok = ConvertDslxToIr(kInvalidProgram.c_str(), "my_module.x", "my_module",
+                            /*dslx_stdlib_path=*/xls::kDefaultDslxStdlibPath,
+                            additional_search_paths, 0, &error_out, &ir_out);
+  ASSERT_FALSE(ok);
+
+  // We should get an error and not get IR.
+  ASSERT_NE(error_out, nullptr);
+  ASSERT_EQ(ir_out, nullptr);
+
+  EXPECT_THAT(error_out, HasSubstr("Unrecognized character: '@'"));
+}
+
+// Smoke test for ConvertDslxPathToIr C API.
+TEST(XlsCApiTest, ConvertDslxPathToIr) {
+  const std::string kProgram = "fn id(x: u32) -> u32 { x }";
+
+  XLS_ASSERT_OK_AND_ASSIGN(xls::TempDirectory tempdir,
+                           xls::TempDirectory::Create());
+  const std::filesystem::path module_path = tempdir.path() / "my_module.x";
+  XLS_ASSERT_OK(xls::SetFileContents(module_path, kProgram));
+
+  const char* additional_search_paths[] = {};
+  char* error_out = nullptr;
+  char* ir_out = nullptr;
+  bool ok = ConvertDslxPathToIr(
+      module_path.c_str(), /*dslx_stdlib_path=*/xls::kDefaultDslxStdlibPath,
+      additional_search_paths, 0, &error_out, &ir_out);
+
+  absl::Cleanup free_cstrs([&] {
+    free(error_out);
+    free(ir_out);
+  });
+
+  // We should get IR and no error.
+  ASSERT_TRUE(ok);
+  ASSERT_EQ(error_out, nullptr);
+  ASSERT_NE(ir_out, nullptr);
+
+  EXPECT_THAT(ir_out, HasSubstr("fn __my_module__id"));
+}
+
+}  // namespace
