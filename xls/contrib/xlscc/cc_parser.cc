6ceee2971580e19afca15a124e8891e06ddcdd06
Benjamin Kramer <kramerb@google.com>
2024-05-10 06:12:42 -0700

    Integrate LLVM at llvm/llvm-project@fc57f88f0074
    Updates LLVM usage to match
    [fc57f88f0074](https://github.com/llvm/llvm-project/commit/fc57f88f0074)
    
    PiperOrigin-RevId: 632473772

--- xls/contrib/xlscc/cc_parser.cc
+++ xls/contrib/xlscc/cc_parser.cc
@@ -616,6 +616,7 @@ void LibToolThread::Run() {
   argv.emplace_back("-Wno-constant-logical-operand");
   argv.emplace_back("-Wno-unused-but-set-variable");
   argv.emplace_back("-Wno-c++11-narrowing");
+  argv.emplace_back("-Wno-conversion");
 
   llvm::IntrusiveRefCntPtr<clang::FileManager> libtool_files;
 
