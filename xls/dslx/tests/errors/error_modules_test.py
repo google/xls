f71b2e742817fabd1abaa6e8ac4ca452819e1b0c
Christopher D. Leary <cdleary@openai.com>
2024-06-19 10:10:05 -0700

    Tweak Python columns in case g3 Python formatter is going to get mad.

--- xls/dslx/tests/errors/error_modules_test.py
+++ xls/dslx/tests/errors/error_modules_test.py
@@ -1184,7 +1184,8 @@ class ImportModuleWithTypeErrorTest(test_base.TestCase):
   def test_gh_1473(self):
     stderr = self._run('xls/dslx/tests/errors/gh_1473.x')
     self.assertIn(
-        'Parametric expression `umax(MAX_N_M, V)` refered to `V` which is not present in the parametric environment', stderr
+        'Parametric expression `umax(MAX_N_M, V)` refered to `V`'
+        ' which is not present in the parametric environment', stderr
     )
 
 
