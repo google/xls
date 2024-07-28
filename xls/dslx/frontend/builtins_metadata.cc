759e90e1ed827dd84714e1548012825be8c8c08f
David Plass <davidplass@google.com>
2024-07-27 18:53:59 -0700

    Use designated initializers when creating `BuiltinsData` structs in the builtins metadata map.
    PiperOrigin-RevId: 656826315

--- xls/dslx/frontend/builtins_metadata.cc
+++ xls/dslx/frontend/builtins_metadata.cc
@@ -27,44 +27,61 @@ const absl::flat_hash_map<std::string, BuiltinsData>& GetParametricBuiltins() {
   static const absl::NoDestructor<
       absl::flat_hash_map<std::string, BuiltinsData>>
       map({
-          {"add_with_carry", {"(uN[T], uN[T]) -> (u1, uN[T])", false}},
-          {"assert_eq", {"(T, T) -> ()", false}},
-          {"assert_lt", {"(xN[N], xN[N]) -> ()", false}},
-          {"bit_slice_update", {"(uN[N], uN[U], uN[V]) -> uN[N]", false}},
-          {"clz", {"(uN[N]) -> uN[N]", false}},
-          {"ctz", {"(uN[N]) -> uN[N]", false}},
-          {"cover!", {"(u8[N], u1) -> ()", false}},
-          {"fail!", {"(u8[N], T) -> T", false}},
+          {"add_with_carry",
+           {.signature = "(uN[T], uN[T]) -> (u1, uN[T])",
+            .is_ast_node = false}},
+          {"assert_eq", {.signature = "(T, T) -> ()", .is_ast_node = false}},
+          {"assert_lt",
+           {.signature = "(xN[N], xN[N]) -> ()", .is_ast_node = false}},
+          {"bit_slice_update",
+           {.signature = "(uN[N], uN[U], uN[V]) -> uN[N]",
+            .is_ast_node = false}},
+          {"clz", {.signature = "(uN[N]) -> uN[N]", .is_ast_node = false}},
+          {"ctz", {.signature = "(uN[N]) -> uN[N]", .is_ast_node = false}},
+          {"cover!", {.signature = "(u8[N], u1) -> ()", .is_ast_node = false}},
+          {"fail!", {.signature = "(u8[N], T) -> T", .is_ast_node = false}},
           {"assert!",
            {.signature = "(bool, u8[N]) -> ()", .is_ast_node = false}},
-          {"gate!", {"(u1, T) -> T", false}},
-          {"map", {"(T[N], (T) -> U) -> U[N]", false}},
-          {"decode", {"<uN[M]>(uN[N]) -> uN[M]", false}},
-          {"encode", {"(uN[N]) -> uN[ceil(log2(N))]", false}},
-          {"one_hot", {"(uN[N], u1) -> uN[N+1]", false}},
-          {"one_hot_sel", {"(uN[N], xN[M][N]) -> xN[M]", false}},
-          {"priority_sel", {"(uN[N], xN[M][N]) -> xN[M]", false}},
-          {"rev", {"(uN[N]) -> uN[N]", false}},
-          {"umulp", {"(uN[N], uN[N]) -> (uN[N], uN[N])", false}},
+          {"gate!", {.signature = "(u1, T) -> T", .is_ast_node = false}},
+          {"map",
+           {.signature = "(T[N], (T) -> U) -> U[N]", .is_ast_node = false}},
+          {"decode",
+           {.signature = "<uN[M]>(uN[N]) -> uN[M]", .is_ast_node = false}},
+          {"encode",
+           {.signature = "(uN[N]) -> uN[ceil(log2(N))]", .is_ast_node = false}},
+          {"one_hot",
+           {.signature = "(uN[N], u1) -> uN[N+1]", .is_ast_node = false}},
+          {"one_hot_sel",
+           {.signature = "(uN[N], xN[M][N]) -> xN[M]", .is_ast_node = false}},
+          {"priority_sel",
+           {.signature = "(uN[N], xN[M][N]) -> xN[M]", .is_ast_node = false}},
+          {"rev", {.signature = "(uN[N]) -> uN[N]", .is_ast_node = false}},
+          {"umulp",
+           {.signature = "(uN[N], uN[N]) -> (uN[N], uN[N])",
+            .is_ast_node = false}},
 
           // Note: the result tuple from `smulp` are two "bags of bits" that
           // must be added together in order to arrive at the signed product. So
           // we give them back as unsigned and users should cast the sum of
           // these elements to a signed number.
-          {"smulp", {"(sN[N], sN[N]) -> (uN[N], uN[N])", false}},
+          {"smulp",
+           {.signature = "(sN[N], sN[N]) -> (uN[N], uN[N])",
+            .is_ast_node = false}},
 
-          {"array_rev", {"(T[N]) -> T[N]", false}},
-          {"array_size", {"(T[N]) -> u32", false}},
+          {"array_rev", {.signature = "(T[N]) -> T[N]", .is_ast_node = false}},
+          {"array_size", {.signature = "(T[N]) -> u32", .is_ast_node = false}},
 
           // Bitwise reduction ops.
-          {"and_reduce", {"(uN[N]) -> u1", false}},
-          {"or_reduce", {"(uN[N]) -> u1", false}},
-          {"xor_reduce", {"(uN[N]) -> u1", false}},
+          {"and_reduce", {.signature = "(uN[N]) -> u1", .is_ast_node = false}},
+          {"or_reduce", {.signature = "(uN[N]) -> u1", .is_ast_node = false}},
+          {"xor_reduce", {.signature = "(uN[N]) -> u1", .is_ast_node = false}},
 
           // Use a dummy value to determine size.
-          {"signex", {"(xN[M], xN[N]) -> xN[N]", false}},
-          {"slice", {"(T[M], uN[N], T[P]) -> T[P]", false}},
-          {"trace!", {"(T) -> T", false}},
+          {"signex",
+           {.signature = "(xN[M], xN[N]) -> xN[N]", .is_ast_node = false}},
+          {"slice",
+           {.signature = "(T[M], uN[N], T[P]) -> T[P]", .is_ast_node = false}},
+          {"trace!", {.signature = "(T) -> T", .is_ast_node = false}},
 
           // Note: the macros we have AST nodes for.
           //
@@ -77,16 +94,19 @@ const absl::flat_hash_map<std::string, BuiltinsData>& GetParametricBuiltins() {
           {"update",
            {.signature = "(T[...], uN[M]|(uN[M], ...), T) -> T[...]",
             .is_ast_node = false}},
-          {"enumerate", {"(T[N]) -> (u32, T)[N]", false}},
+          {"enumerate",
+           {.signature = "(T[N]) -> (u32, T)[N]", .is_ast_node = false}},
 
-          {"widening_cast", {"<U>(T) -> U", false}},
-          {"checked_cast", {"<U>(T) -> U", false}},
+          {"widening_cast", {.signature = "<U>(T) -> U", .is_ast_node = false}},
+          {"checked_cast", {.signature = "<U>(T) -> U", .is_ast_node = false}},
 
           // Require-const-argument.
           //
           // Note this is a messed up type signature to need to support and
           // should really be replaced with known-statically-sized iota syntax.
-          {"range", {"(const uN[N], const uN[N]) -> uN[N][R]", false}},
+          {"range",
+           {.signature = "(const uN[N], const uN[N]) -> uN[N][R]",
+            .is_ast_node = false}},
 
           {"zip",
            {.signature = "(T[N], U[N]) -> (T, U)[N]", .is_ast_node = false}},
