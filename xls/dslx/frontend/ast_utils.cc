796f0f491a3159aee7a570012c58f157fa6b6008
David Plass <davidplass@google.com>
2024-07-31 10:25:07 -0700

    Remove yet another unused include from ast_utils.cc
    PiperOrigin-RevId: 658055329

--- xls/dslx/frontend/ast_utils.cc
+++ xls/dslx/frontend/ast_utils.cc
@@ -30,7 +30,6 @@
 #include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
 #include "absl/types/variant.h"
-#include "xls/common/casts.h"
 #include "xls/common/status/ret_check.h"
 #include "xls/common/status/status_macros.h"
 #include "xls/common/visitor.h"
