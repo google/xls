ea01d7cecb8dc0673d8569489995f28e1103964e
Christopher D. Leary <cdleary@openai.com>
2024-08-07 22:37:47 -0700

    Comments for one file I missed.

--- xls/ir/render_nodes_header.h
+++ xls/ir/render_nodes_header.h
@@ -7,8 +7,12 @@
 
 namespace xls {
 
+// Renders the class declaration for the given `op_class`.
+//
+// Exposed for unit testing.
 std::string RenderNodeSubclass(const OpClass& op_class);
 
+// Renders the entirety of the `nodes.h` header file.
 std::string RenderNodesHeader();
 
 }  // namespace xls
