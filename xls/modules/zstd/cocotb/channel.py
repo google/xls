af3b51aaa83f00a00317a38602d5b60c7287fed1
Krzysztof Obłonczek <koblonczek@antmicro.com>
2024-08-16 14:19:45 +0200

    Add cocotb tests
    Add cocotb testing utilities:
    - XLSStruct for easier handling and serializing/deserializing XLS structs
    - XLSChannel that serves as a dummy receiving channel
    - XLSMonitor that monitors transactions on an XLS channel
    - XLSDriver that can send data on an XLS channel
    - LatencyScoreboard that can measure latency between corresponding transactions on input and output buses
    - File-backed AXI memory python model
    
    Add cocotb tests for:
    - modules/zstd/memory/MemReader
    - modules/zstd/memory/AxiWriter
    - modules/zstd/memory/MemWriter
    
    Co-authred-by: Pawel Czarnecki <pczarnecki@antmicro.com>
    Co-authred-by: Robert Winkler <rwinkler@antmicro.com>
    Co-authred-by: Michal Czyz <mczyz@antmicro.com>
    Signed-off-by: Michal Czyz <mczyz@antmicro.com>
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>
    Signed-off-by: Krzysztof Obłonczek <koblonczek@antmicro.com>

---
+++ xls/modules/zstd/cocotb/channel.py
@@ -0,0 +1,95 @@
+# Copyright 2024 The XLS Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any, Sequence, Type, Union
+
+import cocotb
+from cocotb.handle import SimHandleBase
+from cocotb.triggers import RisingEdge
+from cocotb_bus.bus import Bus
+from cocotb_bus.drivers import BusDriver
+from cocotb_bus.monitors import BusMonitor
+
+from xls.modules.zstd.cocotb.xlsstruct import XLSStruct
+
+Transaction = Union[XLSStruct, Sequence[XLSStruct]]
+
+XLS_CHANNEL_SIGNALS = ["data", "rdy", "vld"]
+XLS_CHANNEL_OPTIONAL_SIGNALS = []
+
+
+class XLSChannel(Bus):
+  _signals = XLS_CHANNEL_SIGNALS
+  _optional_signals = XLS_CHANNEL_OPTIONAL_SIGNALS
+
+  def __init__(self, entity, name, clk, *, start_now=False, **kwargs: Any):
+    super().__init__(entity, name, self._signals, self._optional_signals, **kwargs)
+    self.clk = clk
+    if start_now:
+        self.start_recv_loop()
+
+  @cocotb.coroutine
+  async def recv_channel(self):
+    """Cocotb coroutine that acts as a proc receiving data from a channel"""
+    self.rdy.setimmediatevalue(1)
+    while True:
+      await RisingEdge(self.clk)
+
+  def start_recv_loop(self):
+    cocotb.start_soon(self.recv_channel())
+
+
+class XLSChannelDriver(BusDriver):
+  _signals = XLS_CHANNEL_SIGNALS
+  _optional_signals = XLS_CHANNEL_OPTIONAL_SIGNALS
+
+  def __init__(self, entity: SimHandleBase, name: str, clock: SimHandleBase, **kwargs: Any):
+    BusDriver.__init__(self, entity, name, clock, **kwargs)
+
+    self.bus.data.setimmediatevalue(0)
+    self.bus.vld.setimmediatevalue(0)
+
+  async def _driver_send(self, transaction: Transaction, sync: bool = True, **kwargs: Any) -> None:
+    if sync:
+      await RisingEdge(self.clock)
+
+    data_to_send = (transaction if isinstance(transaction, Sequence) else [transaction])
+
+    for word in data_to_send:
+      self.bus.vld.value = 1
+      self.bus.data.value = word.binaryvalue
+
+      while True:
+        await RisingEdge(self.clock)
+        if self.bus.rdy.value:
+          break
+
+      self.bus.vld.value = 0
+
+
+class XLSChannelMonitor(BusMonitor):
+  _signals = XLS_CHANNEL_SIGNALS
+  _optional_signals = XLS_CHANNEL_OPTIONAL_SIGNALS
+
+  def __init__(self, entity: SimHandleBase, name: str, clock: SimHandleBase, struct: Type[XLSStruct], **kwargs: Any):
+    BusMonitor.__init__(self, entity, name, clock, **kwargs)
+    self.struct = struct
+
+  @cocotb.coroutine
+  async def _monitor_recv(self) -> None:
+    while True:
+      await RisingEdge(self.clock)
+      if self.bus.rdy.value and self.bus.vld.value:
+        vec = self.struct.from_int(self.bus.data.value.integer)
+        self._recv(vec)
---
+++ xls/modules/zstd/memory/mem_reader_wrapper.v
@@ -0,0 +1,111 @@
+// Copyright 2024 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+`default_nettype none
+
+module mem_reader_wrapper #(
+    parameter DSLX_DATA_W = 64,
+    parameter DSLX_ADDR_W = 16,
+    parameter AXI_DATA_W  = 128,
+    parameter AXI_ADDR_W  = 16,
+    parameter AXI_DEST_W  = 8,
+    parameter AXI_ID_W    = 8,
+
+    parameter CTRL_W   = (DSLX_ADDR_W),
+    parameter REQ_W    = (2 * DSLX_ADDR_W),
+    parameter RESP_W   = (1 + DSLX_DATA_W + DSLX_ADDR_W + 1),
+    parameter AXI_AR_W = (AXI_ID_W + AXI_ADDR_W + 28),
+    parameter AXI_R_W  = (AXI_ID_W + AXI_DATA_W + 4)
+) (
+    input wire clk,
+    input wire rst,
+
+    output wire             req_rdy,
+    input  wire             req_vld,
+    input  wire [REQ_W-1:0] req_data,
+
+    output wire              resp_vld,
+    input  wire              resp_rdy,
+    output wire [RESP_W-1:0] resp_data,
+
+    output wire                  axi_ar_arvalid,
+    input  wire                  axi_ar_arready,
+    output wire [  AXI_ID_W-1:0] axi_ar_arid,
+    output wire [AXI_ADDR_W-1:0] axi_ar_araddr,
+    output wire [           3:0] axi_ar_arregion,
+    output wire [           7:0] axi_ar_arlen,
+    output wire [           2:0] axi_ar_arsize,
+    output wire [           1:0] axi_ar_arburst,
+    output wire [           3:0] axi_ar_arcache,
+    output wire [           2:0] axi_ar_arprot,
+    output wire [           3:0] axi_ar_arqos,
+
+    input  wire                  axi_r_rvalid,
+    output wire                  axi_r_rready,
+    input  wire [  AXI_ID_W-1:0] axi_r_rid,
+    input  wire [AXI_DATA_W-1:0] axi_r_rdata,
+    input  wire [           2:0] axi_r_rresp,
+    input  wire                  axi_r_rlast
+);
+
+  wire [AXI_AR_W-1:0] axi_ar_data;
+  wire                axi_ar_rdy;
+  wire                axi_ar_vld;
+
+  assign axi_ar_rdy = axi_ar_arready;
+
+  assign axi_ar_arvalid = axi_ar_vld;
+  assign {
+    axi_ar_arid,
+    axi_ar_araddr,
+    axi_ar_arregion,
+    axi_ar_arlen,
+    axi_ar_arsize,
+    axi_ar_arburst,
+    axi_ar_arcache,
+    axi_ar_arprot,
+    axi_ar_arqos
+} = axi_ar_data;
+
+  wire [AXI_R_W-1:0] axi_r_data;
+  wire               axi_r_vld;
+  wire               axi_r_rdy;
+
+  assign axi_r_data = {axi_r_rid, axi_r_rdata, axi_r_rresp, axi_r_rlast};
+  assign axi_r_vld = axi_r_rvalid;
+
+  assign axi_r_rready = axi_r_rdy;
+
+  mem_reader_adv mem_reader_adv (
+      .clk(clk),
+      .rst(rst),
+
+      .mem_reader__req_r_data(req_data),
+      .mem_reader__req_r_rdy (req_rdy),
+      .mem_reader__req_r_vld (req_vld),
+
+      .mem_reader__resp_s_data(resp_data),
+      .mem_reader__resp_s_rdy (resp_rdy),
+      .mem_reader__resp_s_vld (resp_vld),
+
+      .mem_reader__axi_ar_s_data(axi_ar_data),
+      .mem_reader__axi_ar_s_rdy (axi_ar_rdy),
+      .mem_reader__axi_ar_s_vld (axi_ar_vld),
+
+      .mem_reader__axi_r_r_data(axi_r_data),
+      .mem_reader__axi_r_r_vld (axi_r_vld),
+      .mem_reader__axi_r_r_rdy (axi_r_rdy)
+  );
+
+endmodule
