c5839315de04b4d0266ea2e417dfafe3165f120d
Alex Light <allight@google.com>
2024-08-22 13:39:47 -0700

    Add helper for generating LLVM Artifacts
    We have at various points found several bugs in the LLVM code-generation/optimization pipeline. These are typically revealed by some XLS code which generates odd LLVM ir when jitted/aot'd. Previously it has always been a laborious and manual process to collect everything together in order to do validation that the issue is in LLVM and to collect the artifacts needed to submit an LLVM bug report.
    
    To simplify this process a new xls/jit:dump_llvm_artifacts binary is now available which can be given an XLS function IR file, an input set and an expected output and dump a collection of LLVM ir files which implement a basic tester. These files include a 'linked.ll' and 'linked.opt.ll' file which is a single llvm ir file with a 'main' method that exits '0' if the XLS code results in the expected outcome and '1' otherwise. This can easily be used with --bisect_passes_limit in llvm-opt to determine where a miscompile is ocuring.
    
    PiperOrigin-RevId: 666472428

---
+++ xls/jit/dump_llvm_artifacts_main.py
@@ -0,0 +1,203 @@
+#
+# Copyright 2024 The XLS Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Generate a set of files which are useful for diagnosing llvm issues on an xls function."""
+
+from collections.abc import Sequence
+import itertools
+import pathlib
+import subprocess
+
+from absl import app
+from absl import flags
+
+from xls.common import runfiles
+
+_OUT_DIR = flags.DEFINE_string(
+    "out_dir",
+    default=None,
+    required=True,
+    help="Directory to put all generated files in.",
+)
+
+_IR = flags.DEFINE_string(
+    "ir",
+    default=None,
+    required=True,
+    help="IR to generate a wrapper for",
+)
+
+_TOP = flags.DEFINE_string(
+    "top",
+    default=None,
+    required=False,
+    help="Top function to generate test for",
+)
+
+_INPUT = flags.DEFINE_multi_string(
+    "input",
+    default=[],
+    help=(
+        'Each input to have the main function For example:  "(bits[7]:0,'
+        ' bits[20]:4)"'
+    ),
+)
+_RESULT = flags.DEFINE_string(
+    "result",
+    default=None,
+    required=True,
+    help=(
+        "Value to expect the result to be. Formatted the same as 'input' though"
+        " only a single value."
+    ),
+)
+_MASK = flags.DEFINE_string(
+    "mask", default=None, required=False, help="Mask to apply to result."
+)
+_WRITE_RESULT = flags.DEFINE_bool(
+    "write_result",
+    default=False,
+    required=False,
+    help=(
+        "Whether to have the main function write the result buffer to stdout,"
+        " requires that a posix 'write' is linked."
+    ),
+)
+
+_DEFAULT_CLANG = "clang/clang"
+_DEFAULT_LLVM_LINK = "llvm/llvm-link"
+
+_CLANG = flags.DEFINE_string(
+    "clang",
+    default=runfiles.get_path(_DEFAULT_CLANG, repository="llvm-project"),
+    required=False,
+    help="If present the clang to use to turn the main.cc into an llvm ir",
+)
+_LLVM_LINK = flags.DEFINE_string(
+    "llvm_link",
+    default=runfiles.get_path(_DEFAULT_LLVM_LINK, repository="llvm-project"),
+    required=False,
+    help="If present the llvm-link to use to link the main.cc",
+)
+
+_DUMP_MAIN = "xls/jit/aot_main_wrapper_main"
+_AOT_COMPILER = "xls/jit/aot_compiler_main"
+
+
+def main(argv: Sequence[str]) -> None:
+  if len(argv) > 1:
+    raise app.UsageError("Too many command-line arguments.")
+  out_base = pathlib.Path(_OUT_DIR.value)
+  if not out_base.exists():
+    out_base.mkdir()
+  if not out_base.is_dir():
+    raise app.UsageError(
+        f"{_OUT_DIR.value} already exists and is not a directory"
+    )
+  llvm_ir = str(out_base / "result.ll")
+  opt_ir = str(out_base / "result.opt.ll")
+  asm = str(out_base / "result.asm")
+  obj = str(out_base / "result.o")
+  text_proto = str(out_base / "result.entrypoints.txtpb")
+  proto = str(out_base / "result.entrypoints.pb")
+  main_file = str(out_base / "main.cc")
+  main_ll = str(out_base / "main.ll")
+  linked_ll = str(out_base / "linked.ll")
+  linked_opt_ll = str(out_base / "linked.opt.ll")
+  top_arg = ["-top", _TOP.value] if _TOP.value else []
+  mask_arg = ["-mask", _MASK.value] if _MASK.value else []
+  print("Generating XLS artifacts")
+  subprocess.run(
+      [
+          runfiles.get_path(_AOT_COMPILER),
+          "-input",
+          _IR.value,
+          "-output_object",
+          obj,
+          "-output_proto",
+          proto,
+          "-output_textproto",
+          text_proto,
+          "-output_llvm_ir",
+          llvm_ir,
+          "-output_llvm_opt_ir",
+          opt_ir,
+          "-output_asm",
+          asm,
+      ]
+      + top_arg,
+      check=True,
+  )
+  print("Generating main.cc")
+  subprocess.run(
+      [
+          runfiles.get_path(_DUMP_MAIN),
+          "-output",
+          main_file,
+          "-package",
+          proto,
+          "-result",
+          _RESULT.value,
+          "--write_result" if _WRITE_RESULT.value else "--nowrite_result",
+      ]
+      + mask_arg
+      + top_arg
+      + list(
+          itertools.chain.from_iterable(["-input", i] for i in _INPUT.value)
+      ),
+      check=True,
+  )
+  if _CLANG.value:
+    print("Compiling main.cc")
+    subprocess.run(
+        [
+            _CLANG.value,
+            "-S",
+            "-emit-llvm",
+            main_file,
+            "-o",
+            main_ll,
+        ],
+        check=True,
+    )
+    if _LLVM_LINK.value:
+      print("Linking unopt main.cc")
+      subprocess.run(
+          [
+              _LLVM_LINK.value,
+              main_ll,
+              llvm_ir,
+              "-S",
+              "-o",
+              linked_ll,
+          ],
+          check=True,
+      )
+      print("Linking opt main.cc")
+      subprocess.run(
+          [
+              _LLVM_LINK.value,
+              main_ll,
+              opt_ir,
+              "-S",
+              "-o",
+              linked_opt_ll,
+          ],
+          check=True,
+      )
+
+
+if __name__ == "__main__":
+  app.run(main)
---
+++ xls/jit/dump_llvm_artifacts_main_test.py
@@ -0,0 +1,127 @@
+#
+# Copyright 2024 The XLS Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Test for xls.jit.dump_llvm_artifacts_main."""
+
+import pathlib
+import struct
+import subprocess
+
+from absl.testing import absltest
+from xls.common import runfiles
+
+_DUMP_ARTIFACTS = runfiles.get_path('xls/jit/dump_llvm_artifacts')
+
+ADD_IR = """package foo
+
+top fn muladd(a: bits[8], b: bits[8], c: bits[8]) -> bits[8] {
+  umul.4: bits[8] = umul(a, b, id=4, pos=[(0,3,6)])
+  ret add.5: bits[8] = add(umul.4, c, id=5, pos=[(0,3,10)])
+}
+"""
+
+
+class DumpLlvmArtifactsMainTest(absltest.TestCase):
+
+  def test_expected_files_exist(self):
+    tmp = self.create_tempdir()
+    tmp_path = pathlib.Path(tmp.full_path)
+    ir = self.create_tempfile(content=ADD_IR)
+    subprocess.run(
+        [
+            _DUMP_ARTIFACTS,
+            '-out_dir',
+            tmp.full_path,
+            '-ir',
+            ir.full_path,
+            '--input=bits[8]:3',
+            '--input=bits[8]:4',
+            '--input=bits[8]:5',
+            '--result=bits[8]:17',
+        ],
+        check=True,
+    )
+    self.assertTrue((tmp_path / 'linked.ll').exists())
+    self.assertTrue((tmp_path / 'linked.opt.ll').exists())
+    self.assertTrue((tmp_path / 'main.ll').exists())
+    self.assertTrue((tmp_path / 'main.cc').exists())
+    self.assertTrue((tmp_path / 'result.asm').exists())
+    self.assertTrue((tmp_path / 'result.o').exists())
+    self.assertTrue((tmp_path / 'result.ll').exists())
+    self.assertTrue((tmp_path / 'result.opt.ll').exists())
+    self.assertTrue((tmp_path / 'result.entrypoints.pb').exists())
+    self.assertTrue((tmp_path / 'result.entrypoints.txtpb').exists())
+
+  def test_can_detect_mismatch(self):
+    tmp = self.create_tempdir()
+    tmp_path = pathlib.Path(tmp.full_path)
+    ir = self.create_tempfile(content=ADD_IR)
+    subprocess.run(
+        [
+            _DUMP_ARTIFACTS,
+            '-out_dir',
+            tmp.full_path,
+            '-ir',
+            ir.full_path,
+            '--input=bits[8]:3',
+            '--input=bits[8]:4',
+            '--input=bits[8]:5',
+            '--result=bits[8]:7',  # Whoops not the right value
+            '--write_result',
+        ],
+        check=True,
+    )
+    lli_res = subprocess.run(
+        [
+            runfiles.get_path('llvm/lli', repository='llvm-project'),
+            str(tmp_path / 'linked.ll'),
+        ],
+        check=False,
+        stdout=subprocess.PIPE,
+    )
+    self.assertEqual(17, struct.unpack('=B', lli_res.stdout)[0])
+    self.assertEqual(lli_res.returncode, 1)
+
+  def test_can_interpret(self):
+    tmp = self.create_tempdir()
+    tmp_path = pathlib.Path(tmp.full_path)
+    ir = self.create_tempfile(content=ADD_IR)
+    subprocess.run(
+        [
+            _DUMP_ARTIFACTS,
+            '-out_dir',
+            tmp.full_path,
+            '-ir',
+            ir.full_path,
+            '--input=bits[8]:3',
+            '--input=bits[8]:4',
+            '--input=bits[8]:5',
+            '--result=bits[8]:17',
+            '--write_result',
+        ],
+        check=True,
+    )
+    lli_res = subprocess.run(
+        [
+            runfiles.get_path('llvm/lli', repository='llvm-project'),
+            str(tmp_path / 'linked.ll'),
+        ],
+        check=True,
+        stdout=subprocess.PIPE,
+    ).stdout
+    self.assertEqual(17, struct.unpack('=B', lli_res)[0])
+
+
+if __name__ == '__main__':
+  absltest.main()
