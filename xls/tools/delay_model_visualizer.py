ee84437dca45d3cd962b7066018a0d45c5df194c
Hoa Nguyen <hnpl@google.com>
2024-08-29 16:02:39 -0700

    Add R^2 score and estimator equation to graphs generated by delay_model_visualizer
    PiperOrigin-RevId: 669094049

--- xls/tools/delay_model_visualizer.py
+++ xls/tools/delay_model_visualizer.py
@@ -44,6 +44,18 @@ flags.mark_flag_as_required('output_dir')
 
 FLAGS = flags.FLAGS
 
+# Choosing figsizes that do not cutoff graph details
+_2D_GRAPH_FIGSIZE = (8.0, 5.0)
+_3D_GRAPH_FIGSIZE = (8.0, 7.0)
+
+
+def get_r2_score(y_pred: np.ndarray, y_true: np.ndarray) -> np.float64:
+  residual_sum_of_squares = np.sum(np.square(y_pred - y_true))
+  total_sum_of_squares = np.sum(np.square(y_true - np.mean(y_true)))
+  if total_sum_of_squares == 0:
+    return np.float64(1.0)
+  return 1.0 - residual_sum_of_squares / total_sum_of_squares
+
 
 def maybe_plot_estimator_and_data_points(
     estimator: delay_model.Estimator,
@@ -78,13 +90,27 @@ def maybe_plot_estimator_and_data_points(
   if specialization_details:
     title += ' ' + str(specialization_details)
 
+  coeffs = estimator.params
+
   if len(estimator.delay_expressions) == 1:
-    fig, ax = pyplot.subplots()
+    fig, ax = pyplot.subplots(figsize=_2D_GRAPH_FIGSIZE)
 
     # Plot the real data points as circles.
     x_actual = [dp.delay_factors[0] for dp in estimator.raw_data_points]
     y_actual = [dp.delay_ps for dp in estimator.raw_data_points]
     ax.plot(x_actual, y_actual, 'o')
+
+    # Compute the R^2 score and add the score to the graph
+    r2_score = get_r2_score(np.vectorize(delay_f)(x_actual), y_actual)
+    title += f', $R^2$ = {r2_score:.2f}'
+
+    # Describe the equation
+    estimator_description = (
+        f'f(x) = {coeffs[0]:.2f} + {coeffs[1]:.2f} * x + {coeffs[2]:.2f} *'
+        ' log(x)\nwhere x ='
+        f' {delay_model.delay_expression_description(estimator.delay_expressions[0])}'
+    )
+    title += '\n' + estimator_description
 
     # Plot a curve for the delay model.
     x_range = np.linspace(1, max(x_actual), num=100)
@@ -103,8 +129,21 @@ def maybe_plot_estimator_and_data_points(
     x_actual = [dp.delay_factors[0] for dp in estimator.raw_data_points]
     y_actual = [dp.delay_factors[1] for dp in estimator.raw_data_points]
     z_actual = [dp.delay_ps for dp in estimator.raw_data_points]
-    fig = pyplot.figure()
+    fig = pyplot.figure(figsize=_3D_GRAPH_FIGSIZE)
     ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+    # Compute the R^2 score and add the score to the graph
+    r2_score = get_r2_score(np.vectorize(delay_f)(x_actual, y_actual), z_actual)
+    title += f', $R^2$ = {r2_score:.2f}'
+
+    # Describe the equation
+    estimator_description = (
+        f'f(x,y) = {coeffs[0]:.2f} + {coeffs[1]:.2f} * x + {coeffs[2]:.2f} *'
+        f' log(x) + {coeffs[3]:.2f} * y + {coeffs[4]:.2f} * log(y)\nwhere x ='
+        f' {delay_model.delay_expression_description(estimator.delay_expressions[0])}\ny'
+        f' = {delay_model.delay_expression_description(estimator.delay_expressions[1])}'
+    )
+    title += '\n' + estimator_description
 
     # Plot the surface of the delay estimate.
     x_range, y_range = np.meshgrid(
@@ -148,9 +187,9 @@ def maybe_plot_estimator_and_data_points(
     return
 
   if specialization_kind:
-    filename = '%s_%s.png' % (estimator.op, specialization_kind)
+    filename = f'{estimator.op}_{specialization_kind}.png'
   else:
-    filename = '%s.png' % estimator.op
+    filename = f'{estimator.op}.png'
 
   fig.savefig(os.path.join(FLAGS.output_dir, filename))
   pyplot.close(fig)
