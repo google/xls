d141ef97b849035bb2c901547db0ee047a71043e
XLS Team <noreply@google.com>
2024-08-28 22:04:58 -0700

    Automated Code Change
    PiperOrigin-RevId: 668759387

--- xls/ir/proc_testutils.cc
+++ xls/ir/proc_testutils.cc
@@ -246,7 +246,7 @@ absl::StatusOr<Function*> UnrollProcToFunction(Proc* p,
                                                const Value& token_value) {
   XLS_RET_CHECK_GT(activation_count, 0)
       << "At least one activation is required.";
-  XLS_RET_CHECK(!p->next_values().empty() || p->NextState().size() == 0)
+  XLS_RET_CHECK(!p->next_values().empty() || p->NextState().empty())
       << "Only procs using 'next-node' style are supported.";
   if (include_state) {
     XLS_RET_CHECK(
