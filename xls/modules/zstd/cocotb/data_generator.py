f138fbe4939e15d9caa1d6d3529ff0692225b879
Pawel Czarnecki <pczarnecki@antmicro.com>
2024-10-09 15:23:22 +0200

    modules/zstd/cocotb: Add ZSTD frame generator library
    This reverts commit 04ad379225b706ddf492d440c673e77348d7a409.

---
+++ xls/modules/zstd/cocotb/data_generator.py
@@ -0,0 +1,46 @@
+# Copyright 2024 The XLS Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+from enum import Enum
+
+from xls.common import runfiles
+import subprocess
+
+class BlockType(Enum):
+  RAW = 0
+  RLE = 1
+  COMPRESSED = 2
+  RANDOM = 3
+
+def CallDecodecorpus(args):
+  decodecorpus = Path(runfiles.get_path("decodecorpus", repository = "zstd"))
+  cmd = args
+  cmd.insert(0, str(decodecorpus))
+  cmd_concat = " ".join(cmd)
+  subprocess.run(cmd_concat, shell=True, check=True)
+
+def GenerateFrame(seed, btype, output_path):
+  args = []
+  args.append("-s" + str(seed))
+  if (btype != BlockType.RANDOM):
+    args.append("--block-type=" + str(btype.value))
+  args.append("--content-size")
+  # Test payloads up to 16KB
+  args.append("--max-content-size-log=14")
+  args.append("-p" + output_path);
+  args.append("-vvvvvvv");
+
+  CallDecodecorpus(args)
+
