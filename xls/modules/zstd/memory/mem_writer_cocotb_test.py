12bb81a30cd53691673aed1b75f00553884278ab
Pawel Czarnecki <pczarnecki@antmicro.com>
2024-12-31 15:55:34 +0100

    xls/modules/zstd: expose fifo verilog module
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- xls/modules/zstd/memory/mem_writer_cocotb_test.py
+++ xls/modules/zstd/memory/mem_writer_cocotb_test.py
@@ -442,6 +442,7 @@ def generate_padded_test_data_arbitrary(mem_size, test_cases):
 if __name__ == "__main__":
     toplevel = "mem_writer_wrapper"
     verilog_sources = [
+      "xls/modules/zstd/xls_fifo_wrapper.sv",
       "xls/modules/zstd/memory/mem_writer.v",
       "xls/modules/zstd/memory/mem_writer_wrapper.v",
     ]
