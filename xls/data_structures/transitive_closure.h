6794913370fb2881b2e2fed5b61393256ba3258a
Eric Astor <epastor@google.com>
2025-01-02 16:52:55 -0800

    [XLS] Clean up & improve performance of TransitiveClosure
    The previous implementation was focused on sticking as close as possible to the textbook implementation of Warshall's algorithm. However, the algorithm is easily tweaked to have much better performance characteristics & avoid unnecessary copies & allocations.
    
    PiperOrigin-RevId: 711559608

--- xls/data_structures/transitive_closure.h
+++ xls/data_structures/transitive_closure.h
@@ -15,13 +15,8 @@
 #ifndef XLS_DATA_STRUCTURES_TRANSITIVE_CLOSURE_H_
 #define XLS_DATA_STRUCTURES_TRANSITIVE_CLOSURE_H_
 
-#include <cstdint>
-#include <vector>
-
-#include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
-#include "absl/log/check.h"
 
 namespace xls {
 
@@ -30,80 +25,27 @@ using HashRelation = absl::flat_hash_map<V, absl::flat_hash_set<V>>;
 
 // Compute the transitive closure of a relation.
 template <typename V>
-HashRelation<V> TransitiveClosure(const HashRelation<V>& relation) {
-  using Rel = HashRelation<V>;
-
-  if (relation.empty()) {
-    return Rel();
-  }
-
-  absl::flat_hash_set<V> unordered_nodes;
-  for (const auto& [node, children] : relation) {
-    unordered_nodes.insert(node);
-    unordered_nodes.insert(children.begin(), children.end());
-  }
-
-  std::vector<V> ordered_nodes(unordered_nodes.begin(), unordered_nodes.end());
-  std::sort(ordered_nodes.begin(), ordered_nodes.end());
-
-  const int64_t n = ordered_nodes.size();
-
-  absl::flat_hash_map<V, int64_t> node_to_index;
-  node_to_index.reserve(n);
-  for (int64_t i = 0; i < n; ++i) {
-    CHECK(node_to_index.insert({ordered_nodes[i], i}).second);
-  }
-
+HashRelation<V> TransitiveClosure(HashRelation<V> relation) {
   // Warshall's algorithm; https://cs.winona.edu/lin/cs440/ch08-2.pdf
-
-  auto get = [&](const HashRelation<int64_t>& rel, int64_t i,
-                 int64_t j) -> bool {
-    return rel.contains(i) && rel.at(i).contains(j);
-  };
-
-  HashRelation<int64_t> lag;
-  lag.reserve(relation.size());
-  for (const auto& [node, children] : relation) {
-    auto [it, inserted] = lag.insert({node_to_index.at(node), {}});
-    DCHECK(inserted);
-    auto& children_indices = it->second;
-    children_indices.reserve(children.size());
-    absl::c_transform(children,
-                      std::inserter(children_indices, children_indices.end()),
-                      [&](const V& child) { return node_to_index.at(child); });
-  }
-  HashRelation<int64_t> closure = lag;
-  for (int64_t k = 0; k < n; ++k) {
-    for (int64_t i = 0; i < n; ++i) {
-      if (!get(lag, i, k)) {
-        // i doesn't relate to k (via nodes < k).
+  // modified in the typical way to avoid unnecessary copies of the expanded
+  // relation. It's safe to update the relation as we go, since at each stage k,
+  // i relates to k via nodes < k iff i relates to k via nodes <= k, and
+  // similarly for k relating to j.
+  for (const auto& [k, from_k] : relation) {
+    for (auto& [i, from_i] : relation) {
+      if (i == k) {
+        // Updating would be a no-op, so skip it.
         continue;
       }
-      for (int64_t j = 0; j < n; ++j) {
-        if (get(lag, k, j)) {
-          // i relates to k (via nodes < k), and k relates to j (via nodes < k),
-          // so i relates to j (via nodes <= k).
-          DCHECK(closure.contains(i));  // since i relates to k
-          closure.at(i).insert(j);
-        }
+      if (from_i.contains(k)) {
+        // i relates to k (via nodes < k), so:
+        //   for any j where k relates to j (via nodes < k),
+        //     i relates to j (via nodes <= k).
+        from_i.insert(from_k.begin(), from_k.end());
       }
     }
-    lag = closure;
   }
-
-  Rel result;
-  result.reserve(closure.size());
-  for (const auto& [node_index, children_indices] : closure) {
-    auto [it, inserted] = result.insert({ordered_nodes[node_index], {}});
-    DCHECK(inserted);
-    auto& children = it->second;
-    children.reserve(children_indices.size());
-    absl::c_transform(
-        children_indices, std::inserter(children, children.end()),
-        [&](int64_t child_index) { return ordered_nodes[child_index]; });
-  }
-
-  return result;
+  return relation;
 }
 
 }  // namespace xls
--- xls/data_structures/transitive_closure_test.cc
+++ xls/data_structures/transitive_closure_test.cc
@@ -14,12 +14,18 @@
 
 #include "xls/data_structures/transitive_closure.h"
 
+#include <random>
 #include <string>
+#include <vector>
 
+#include "benchmark/benchmark.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
+#include "absl/random/bit_gen_ref.h"
+#include "absl/random/distributions.h"
+#include "absl/strings/str_cat.h"
 
 namespace xls {
 namespace {
@@ -42,6 +48,36 @@ TEST(TransitiveClosureTest, Simple) {
   EXPECT_THAT(tc.at("baz"), UnorderedElementsAre("qux"));
   EXPECT_FALSE(tc.contains("qux"));
 }
+
+HashRelation<V> RandomRelation(std::vector<V> nodes, double p,
+                               absl::BitGenRef rng) {
+  HashRelation<V> rel;
+  for (const V& node : nodes) {
+    rel[node] = {};
+    for (const V& neighbor : nodes) {
+      if (absl::Bernoulli(rng, p)) {
+        rel[node].insert(neighbor);
+      }
+    }
+  }
+  return rel;
+}
+
+void BM_RandomRelation(benchmark::State& state) {
+  std::seed_seq seq = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+  std::mt19937_64 rng(seq);
+  std::vector<V> nodes;
+  nodes.reserve(state.range(0));
+  for (int i = 0; i < state.range(0); ++i) {
+    nodes.push_back(absl::StrCat(i));
+  }
+  HashRelation<V> rel = RandomRelation(nodes, 2.0 / state.range(0), rng);
+  for (auto _ : state) {
+    HashRelation<V> tc = TransitiveClosure(rel);
+    benchmark::DoNotOptimize(tc);
+  }
+}
+BENCHMARK(BM_RandomRelation)->Range(5, 500);
 
 }  // namespace
 }  // namespace xls
