09028c8ad216ac6a7b5de4f5b98f3012f7ec81ff
Chris Leary <cdleary@openai.com>
2025-03-06 14:33:26 -0800

    [dev_tools] Flag when a package has no top in check_ir_equivalence_main.

--- xls/dev_tools/check_ir_equivalence_main_test.py
+++ xls/dev_tools/check_ir_equivalence_main_test.py
@@ -28,6 +28,13 @@ top fn add(x: bits[32], y: bits[32]) -> bits[32] {
   ret add.1: bits[32] = add(x, y)
 }
 """
+# Version of ADD_IR that has no top function.
+ADD_NO_TOP_IR = """package add_no_top
+
+fn add(x: bits[32], y: bits[32]) -> bits[32] {
+  ret add.1: bits[32] = add(x, y)
+}
+"""
 NOT_ADD_IR = """package not_add
 
 top fn not_add(x: bits[32], y: bits[32]) -> bits[32] {
@@ -125,7 +132,8 @@ class CheckIrEquivalenceMainTest(absltest.TestCase):
 
   def _check_equiv(
       self, ir_a: str, ir_b: str, act_count: Union[None, int] = 0
-  ) -> Tuple[bool, str]:
+  ) -> Tuple[bool, str, str]:
+    """Returns `(success, output_message, stderr_message)`."""
     res = subprocess.run(
         [
             _CHECK_EQUIV,
@@ -135,34 +143,39 @@ class CheckIrEquivalenceMainTest(absltest.TestCase):
         ],
         check=False,
         stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
     )
-    return res.returncode == 0, res.stdout.decode("utf8")
+    return res.returncode == 0, res.stdout.decode("utf8"), res.stderr.decode("utf8")
 
   def test_detects_different_functions(self):
-    res, msg = self._check_equiv(ADD_IR, NOT_ADD_IR)
+    res, stdout, stderr = self._check_equiv(ADD_IR, NOT_ADD_IR)
     self.assertFalse(res)
-    self.assertIn("Verified NOT equivalent", msg)
+    self.assertIn("Verified NOT equivalent", stdout)
 
   def test_ignores_assert(self):
-    res, msg = self._check_equiv(PROC_IR_WITH_ASSERT, PROC_IR, act_count=3)
+    res, stdout, stderr = self._check_equiv(PROC_IR_WITH_ASSERT, PROC_IR, act_count=3)
     self.assertTrue(res)
-    self.assertIn("Verified equivalent", msg)
+    self.assertIn("Verified equivalent", stdout)
 
   def test_detects_equiv_functions(self):
-    res, msg = self._check_equiv(ADD_IR, NOT_NOT_ADD_IR)
+    res, stdout, stderr = self._check_equiv(ADD_IR, NOT_NOT_ADD_IR)
     self.assertTrue(res)
-    self.assertIn("Verified equivalent", msg)
+    self.assertIn("Verified equivalent", stdout)
 
   def test_detects_different_procs(self):
-    res, msg = self._check_equiv(PROC_IR, NEG_PROC_IR, act_count=14)
+    res, stdout, stderr = self._check_equiv(PROC_IR, NEG_PROC_IR, act_count=14)
     self.assertFalse(res)
-    self.assertIn("Verified NOT equivalent", msg)
+    self.assertIn("Verified NOT equivalent", stdout)
 
   def test_detects_equiv_procs(self):
-    res, msg = self._check_equiv(PROC_IR, NEG_NEG_PROC_IR, act_count=14)
+    res, stdout, stderr = self._check_equiv(PROC_IR, NEG_NEG_PROC_IR, act_count=14)
     self.assertTrue(res)
-    self.assertIn("Verified equivalent", msg)
+    self.assertIn("Verified equivalent", stdout)
 
+  def test_lhs_has_no_top(self):
+    res, msg, stderr = self._check_equiv(ADD_IR, ADD_NO_TOP_IR)
+    self.assertFalse(res)
+    self.assertIn("Package has no top entity: add_no_top", stderr)
 
 if __name__ == "__main__":
   absltest.main()
