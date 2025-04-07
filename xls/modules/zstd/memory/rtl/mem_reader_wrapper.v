3de5586b246bb8e5099713b9eeaf41df8644b9a3
Pawel Czarnecki <pczarnecki@antmicro.com>
2025-04-07 17:24:48 +0200

    modules/zstd/memory: Move verilog sources to rtl subdirectory
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- xls/modules/zstd/memory/rtl/mem_reader_wrapper.v
+++ xls/modules/zstd/memory/rtl/mem_reader_wrapper.v
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This module wraps the Memory Reader verilog sources generated from DSLX to
+// form a DUT for verilog tests with consistent IO.
+
 `default_nettype none
 
 module mem_reader_wrapper #(
