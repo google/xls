bfccb02e896e0449c2c48f0f22560e715c2b3f78
Jacques Pienaar <jpienaar@google.com>
2025-08-19 02:32:31 -0700

    [xls][mlir] Enable specifying xls optimization pipeline options.
    PiperOrigin-RevId: 796782338

--- xls/contrib/mlir/testdata/optimize_using_xls.mlir
+++ xls/contrib/mlir/testdata/optimize_using_xls.mlir
@@ -1,4 +1,5 @@
 // RUN: xls_opt %s -optimize-using-xls | FileCheck %s
+// RUN: xls_opt %s -optimize-using-xls=xls-pipeline='""' | FileCheck %s --check-prefix=NOP
 
 module @pkg {
 xls.import_dslx_file_package "xls/contrib/mlir/testdata/i32/dot_product.x" as @dot_product
@@ -6,13 +7,23 @@ xls.import_dslx_file_package "xls/contrib/mlir/testdata/i32/dot_product.x" as @d
 func.func private @bar2(%a: !xls.array<4 x i32>, %b: !xls.array<4 x i32>) -> i32 attributes
   {xls.linkage = #xls.translation_linkage<@dot_product:"dot_product_fixed_test">}
 
-// CHECK: module @pkg {
-// CHECK-NOT: import
-// CHECK-LABEL: func @pkg
-// CHECK: xls.smul
-func.func @pkg(%a: !xls.array<4 x i32>, %b: !xls.array<4 x i32>) -> (i32) {
+func.func private @bar(%a: !xls.array<4 x i32>, %b: !xls.array<4 x i32>) -> i32 {
   %1 = func.call @bar2(%a, %b) : (!xls.array<4 x i32>, !xls.array<4 x i32>) -> i32
   return %1 : i32
 }
 
+// Verifies all inlined post optimization roundtrip.
+// CHECK: module @pkg {
+// CHECK-NEXT: func @pkg
+// CHECK-NOT: func
+func.func @pkg(%a: !xls.array<4 x i32>, %b: !xls.array<4 x i32>) -> (i32) {
+  %1 = func.call @bar(%a, %b) : (!xls.array<4 x i32>, !xls.array<4 x i32>) -> i32
+  return %1 : i32
+}
+
+// Verifies that optimizations are skipped when the pipeline is set to <nop>.
+// NOP: module @pkg {
+// NOP: func.func
+// NOP: func.func
+// NOP: func.func
 }
--- xls/contrib/mlir/transforms/optimize_using_xls.cc
+++ xls/contrib/mlir/transforms/optimize_using_xls.cc
@@ -13,19 +13,38 @@
 // limitations under the License.
 
 #include <memory>
+#include <optional>
+#include <string>
 
 #include "absl/status/status.h"
+#include "llvm/include/llvm/Support/CommandLine.h"
+#include "llvm/include/llvm/Support/DebugLog.h"
 #include "mlir/include/mlir/IR/BuiltinOps.h"
+#include "mlir/include/mlir/IR/Diagnostics.h"
 #include "mlir/include/mlir/IR/MLIRContext.h"
 #include "mlir/include/mlir/IR/OwningOpRef.h"
 #include "mlir/include/mlir/Pass/Pass.h"  // IWYU pragma: keep
 #include "mlir/include/mlir/Support/LLVM.h"
-#include "xls/contrib/mlir/IR/xls_ops.h"  // IWYU pragma: keep
+#include "google/protobuf/text_format.h"
+#include "xls/contrib/mlir/IR/xls_ops.h"  // IWYU pragma: keep  // IWYU pragma: keep
 #include "xls/contrib/mlir/tools/xls_translate/xls_translate_from_mlir.h"
 #include "xls/contrib/mlir/tools/xls_translate/xls_translate_to_mlir.h"
+#include "xls/contrib/mlir/transforms/passes.h"  // IWYU pragma: keep
 #include "xls/tools/opt.h"
 
 #define DEBUG_TYPE "optimize-using-xls"
+
+// Consider upstreaming this as the current behavior treats the optional string
+// as an argument.
+namespace llvm::cl {
+template <>
+bool parser<std::optional<std::string>>::parse(
+    Option& O, StringRef ArgName, StringRef Arg,
+    std::optional<std::string>& Value) {
+  Value = Arg;
+  return false;
+}
+}  // namespace llvm::cl
 
 namespace mlir::xls {
 
@@ -35,6 +54,7 @@ namespace mlir::xls {
 namespace {
 struct OptimizeUsingXlsPass
     : public mlir::xls::impl::OptimizeUsingXlsPassBase<OptimizeUsingXlsPass> {
+  using OptimizeUsingXlsPassBase::OptimizeUsingXlsPassBase;
   void runOnOperation() override;
 
   LogicalResult initialize(MLIRContext* context) override {
@@ -59,12 +79,28 @@ void OptimizeUsingXlsPass::runOnOperation() {
 
   ::xls::tools::OptOptions opt_options;
   opt_options.top = module.getName().value_or("_package");
-  absl::Status status =
-      ::xls::tools::OptimizeIrForTop(package->get(), opt_options);
-  if (!status.ok()) {
-    module.emitError("failed to optimize IR: ") << status.ToString();
-    return signalPassFailure();
+  if (xls_pipeline.has_value()) {
+    ::xls::PassPipelineProto pass_pipeline;
+    if (!google::protobuf::TextFormat::ParseFromString(*xls_pipeline, &pass_pipeline)) {
+      mlir::emitError(module.getLoc())
+          << "invalid pass pipeline: " << *xls_pipeline;
+      return signalPassFailure();
+    }
+    opt_options.pass_pipeline = pass_pipeline;
   }
+
+  if (!xls_pipeline.has_value() || !xls_pipeline->empty()) {
+    LDBG() << "Optimizing IR for top: '" << opt_options.top << "using \n\t"
+           << xls_pipeline.value_or("default pipeline");
+
+    absl::Status status =
+        ::xls::tools::OptimizeIrForTop(package->get(), opt_options);
+    if (!status.ok()) {
+      module.emitError("failed to optimize IR: ") << status.ToString();
+      return signalPassFailure();
+    }
+  }
+
   OwningOpRef<Operation*> new_module_op =
       XlsToMlirXlsTranslate(*package.value(), &getContext());
   if (!new_module_op) {
