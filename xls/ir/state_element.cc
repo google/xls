e69886d3a2d1316cd4cbbd118169770454aa6a71
Alex Light <allight@google.com>
2025-12-04 15:41:45 -0800

    Remove trailing \n from StateElement::ToString
    PiperOrigin-RevId: 840432832

--- xls/ir/state_element.cc
+++ xls/ir/state_element.cc
@@ -21,9 +21,8 @@
 namespace xls {
 
 std::string StateElement::ToString() const {
-  return absl::StrFormat("state %s(%s, initial_value=%s, non_synth=%s)\n",
-                         name(), type()->ToString(),
-                         initial_value().ToHumanString(),
+  return absl::StrFormat("state %s(%s, initial_value=%s, non_synth=%s)", name(),
+                         type()->ToString(), initial_value().ToHumanString(),
                          non_synthesizable() ? "true" : "false");
 }
 
