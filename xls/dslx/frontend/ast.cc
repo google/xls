79adbd11ad198d5e12cd87ca8e58c38fab38577f
Wojciech Sipak <wsipak@antmicro.com>
2025-12-09 13:53:34 +0100

    use IsElseIf to recognize chained ifs
    Signed-off-by: Wojciech Sipak <wsipak@antmicro.com>

--- xls/dslx/frontend/ast.cc
+++ xls/dslx/frontend/ast.cc
@@ -720,7 +720,7 @@ std::vector<StatementBlock*> Conditional::GatherBlocks() {
   return blocks;
 }
 
-bool Conditional::IsPartOfLadder() const {
+bool Conditional::IsElseIf() const {
   if (parent() == nullptr) {
     return false;
   }
@@ -736,7 +736,7 @@ bool Conditional::IsPartOfLadder() const {
 
 std::string Conditional::ToStringInternal() const {
   const absl::string_view const_prefix =
-      (is_const_ && !IsPartOfLadder()) ? "const " : "";
+      (is_const_ && !IsElseIf()) ? "const " : "";
 
   auto make_string = [&](std::string (AstNode::*to_str_fn)()
                              const) -> std::string {
