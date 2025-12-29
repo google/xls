410ca92146509aa29f86c06920437600cafc9d3a
Eric Astor <epastor@google.com>
2025-12-29 06:43:15 -0800

    [codegen 1.5] Enable all passing block conversion pipeline tests
    Also fixed conditions to prevent all tests (except ImplicitToken) from crashing.
    
    PiperOrigin-RevId: 850042367

--- xls/codegen_v_1_5/function_io_lowering_pass.cc
+++ xls/codegen_v_1_5/function_io_lowering_pass.cc
@@ -35,31 +35,16 @@
 #include "xls/ir/block.h"
 #include "xls/ir/function_base.h"
 #include "xls/ir/node.h"
+#include "xls/ir/node_util.h"
 #include "xls/ir/nodes.h"
 #include "xls/ir/op.h"
 #include "xls/ir/package.h"
 #include "xls/ir/register.h"
-#include "xls/ir/value.h"
 #include "xls/ir/value_utils.h"
 #include "xls/passes/pass_base.h"
 
 namespace xls::codegen {
 namespace {
-
-absl::StatusOr<Node*> ReplaceWithAnd(Node* original, Node* new_operand) {
-  if (original->Is<Literal>() && original->As<Literal>()->value().IsAllOnes()) {
-    XLS_RETURN_IF_ERROR(original->ReplaceUsesWith(new_operand));
-    return new_operand;
-  }
-  if (original->Is<NaryOp>() && original->As<NaryOp>()->op() == Op::kAnd) {
-    std::vector<Node*> operands(original->operands().begin(),
-                                original->operands().end());
-    operands.push_back(new_operand);
-    return original->ReplaceUsesWithNew<NaryOp>(operands, Op::kAnd);
-  }
-  return original->ReplaceUsesWithNew<NaryOp>(
-      absl::MakeConstSpan({original, new_operand}), Op::kAnd);
-}
 
 absl::StatusOr<Node*> FlopNode(
     ScheduledBlock* block, std::string_view name, Node* input,
@@ -119,9 +104,9 @@ absl::StatusOr<bool> FunctionIOLoweringPass::LowerParams(
     // which will stop the pipeline from running new activations when the input
     // is invalid. This also automatically propagates validity to the output in
     // case an output-valid signal was requested.
-    XLS_RETURN_IF_ERROR(
-        ReplaceWithAnd(block->stages()[0].active_inputs_valid(), *input_valid)
-            .status());
+    XLS_RETURN_IF_ERROR(ReplaceWithAnd(block->stages()[0].active_inputs_valid(),
+                                       *input_valid, /*combine_literals=*/false)
+                            .status());
   }
 
   std::vector<Param*> params_to_remove;
@@ -182,10 +167,13 @@ absl::StatusOr<bool> FunctionIOLoweringPass::LowerReturnValue(
     return absl::UnimplementedError("Splitting outputs not supported.");
   }
 
-  // The return value is always expected to be in the last stage.
-  XLS_ASSIGN_OR_RETURN(int64_t stage_index,
-                       block->GetStageIndex(block->source_return_value()));
-  XLS_RET_CHECK_EQ(stage_index, block->stages().size() - 1);
+  // The return value is always assumed to be in the last stage - or possibly
+  // unscheduled, if (e.g.) returning a literal.
+  if (block->IsStaged(block->source_return_value())) {
+    XLS_ASSIGN_OR_RETURN(int64_t stage_index,
+                         block->GetStageIndex(block->source_return_value()));
+    XLS_RET_CHECK_EQ(stage_index, block->stages().size() - 1);
+  }
 
   std::optional<Node*> output_valid = std::nullopt;
   if (options.codegen_options.valid_control().has_value() &&
---
+++ xls/codegen_v_1_5/register_cleanup_pass.cc
@@ -0,0 +1,188 @@
+// Copyright 2025 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "xls/codegen_v_1_5/register_cleanup_pass.h"
+
+#include <memory>
+#include <optional>
+#include <vector>
+
+#include "absl/algorithm/container.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/status/statusor.h"
+#include "absl/types/span.h"
+#include "xls/codegen_v_1_5/block_conversion_pass.h"
+#include "xls/common/status/status_macros.h"
+#include "xls/data_structures/transitive_closure.h"
+#include "xls/ir/block.h"
+#include "xls/ir/node.h"
+#include "xls/ir/nodes.h"
+#include "xls/ir/package.h"
+#include "xls/ir/register.h"
+#include "xls/ir/value.h"
+#include "xls/ir/value_utils.h"
+#include "xls/passes/node_dependency_analysis.h"
+#include "xls/passes/partial_info_query_engine.h"
+#include "xls/passes/pass_base.h"
+#include "xls/passes/query_engine.h"
+
+namespace xls::codegen {
+
+namespace {
+
+absl::StatusOr<std::vector<RegisterWrite*>> GetRegisterWrites(Block* block,
+                                                              Register* reg) {
+  XLS_ASSIGN_OR_RETURN(absl::Span<RegisterWrite* const> writes,
+                       block->GetRegisterWrites(reg));
+  return std::vector<RegisterWrite*>(writes.begin(), writes.end());
+}
+
+}  // namespace
+
+absl::StatusOr<bool> RegisterCleanupPass::RemoveTrivialLoadEnables(
+    Block* block, QueryEngine& query_engine) const {
+  bool changed = false;
+  for (Register* reg : block->GetRegisters()) {
+    XLS_ASSIGN_OR_RETURN(std::vector<RegisterWrite*> writes,
+                         GetRegisterWrites(block, reg));
+    for (RegisterWrite* write : writes) {
+      if (write->load_enable().has_value() &&
+          query_engine.IsAllOnes(*write->load_enable())) {
+        XLS_RETURN_IF_ERROR(
+            write
+                ->ReplaceUsesWithNew<RegisterWrite>(write->data(), std::nullopt,
+                                                    write->reset(), reg)
+                .status());
+        XLS_RETURN_IF_ERROR(block->RemoveNode(write));
+        changed = true;
+      }
+    }
+  }
+  return changed;
+}
+
+absl::StatusOr<bool> RegisterCleanupPass::RemoveImpossibleWrites(
+    Block* block, QueryEngine& query_engine) const {
+  for (Register* reg : block->GetRegisters()) {
+    XLS_ASSIGN_OR_RETURN(std::vector<RegisterWrite*> writes,
+                         GetRegisterWrites(block, reg));
+    for (RegisterWrite* write : writes) {
+      bool write_disabled = write->load_enable().has_value() &&
+                            query_engine.IsAllZeros(*write->load_enable());
+      if (write_disabled) {
+        XLS_RETURN_IF_ERROR(
+            write->ReplaceUsesWithNew<Literal>(Value::Tuple({})).status());
+        XLS_RETURN_IF_ERROR(block->RemoveNode(write));
+      }
+    }
+
+    XLS_ASSIGN_OR_RETURN(writes, GetRegisterWrites(block, reg));
+    if (writes.empty() || (reg->reset_value().has_value() &&
+                           absl::c_all_of(writes, [&](RegisterWrite* write) {
+                             return query_engine.KnownValue(write->data()) ==
+                                    *reg->reset_value();
+                           }))) {
+      // Nothing can ever write a new value to this register, so it's equivalent
+      // to its reset value.
+      XLS_ASSIGN_OR_RETURN(RegisterRead * read, block->GetRegisterRead(reg));
+      XLS_RETURN_IF_ERROR(
+          read->ReplaceUsesWithNew<Literal>(
+                  reg->reset_value().value_or(ZeroOfType(reg->type())))
+              .status());
+      XLS_RETURN_IF_ERROR(block->RemoveNode(read));
+      XLS_RETURN_IF_ERROR(block->RemoveRegister(reg));
+    }
+  }
+  return false;
+}
+
+absl::StatusOr<bool> RegisterCleanupPass::RemoveUnreadRegisters(
+    Block* block, QueryEngine& query_engine) const {
+  absl::flat_hash_map<Register*, absl::flat_hash_set<Register*>>
+      can_receive_value_from;
+  absl::flat_hash_set<Register*> directly_read_registers;
+  NodeBackwardDependencyAnalysis nda;
+  XLS_RETURN_IF_ERROR(nda.Attach(block).status());
+  for (Register* reg : block->GetRegisters()) {
+    XLS_ASSIGN_OR_RETURN(RegisterRead * read, block->GetRegisterRead(reg));
+    for (Node* user : nda.NodesDependingOn(read)) {
+      if (user->Is<RegisterWrite>()) {
+        can_receive_value_from[reg].insert(
+            user->As<RegisterWrite>()->GetRegister());
+      } else if (user->Is<OutputPort>()) {
+        directly_read_registers.insert(reg);
+      }
+    }
+  }
+
+  absl::flat_hash_map<Register*, absl::flat_hash_set<Register*>>
+      transitive_receivers = TransitiveClosure(can_receive_value_from);
+  std::vector<Register*> unread_registers;
+  for (Register* reg : block->GetRegisters()) {
+    if (!directly_read_registers.contains(reg) &&
+        absl::c_none_of(transitive_receivers[reg], [&](Register* user) {
+          return directly_read_registers.contains(user);
+        })) {
+      unread_registers.push_back(reg);
+    }
+  }
+
+  for (Register* reg : unread_registers) {
+    XLS_ASSIGN_OR_RETURN(RegisterRead * read, block->GetRegisterRead(reg));
+    XLS_ASSIGN_OR_RETURN(absl::Span<RegisterWrite* const> writes,
+                         block->GetRegisterWrites(reg));
+    if (!read->IsDead()) {
+      XLS_RETURN_IF_ERROR(
+          read->ReplaceUsesWithNew<Literal>(
+                  reg->reset_value().value_or(ZeroOfType(reg->type())))
+              .status());
+    }
+    XLS_RETURN_IF_ERROR(block->RemoveNode(read));
+    for (RegisterWrite* write : writes) {
+      if (!write->IsDead()) {
+        XLS_RETURN_IF_ERROR(
+            write->ReplaceUsesWithNew<Literal>(Value::Tuple({})).status());
+      }
+      XLS_RETURN_IF_ERROR(block->RemoveNode(write));
+    }
+    XLS_RETURN_IF_ERROR(block->RemoveRegister(reg));
+  }
+  return !unread_registers.empty();
+}
+
+absl::StatusOr<bool> RegisterCleanupPass::RunInternal(
+    Package* package, const BlockConversionPassOptions& options,
+    PassResults* results) const {
+  bool changed = false;
+  for (const std::unique_ptr<Block>& block : package->blocks()) {
+    PartialInfoQueryEngine query_engine;
+    XLS_RETURN_IF_ERROR(query_engine.Populate(block.get()).status());
+
+    XLS_ASSIGN_OR_RETURN(bool changed_load_enables,
+                         RemoveTrivialLoadEnables(block.get(), query_engine));
+    changed |= changed_load_enables;
+
+    XLS_ASSIGN_OR_RETURN(bool changed_writes,
+                         RemoveImpossibleWrites(block.get(), query_engine));
+    changed |= changed_writes;
+
+    XLS_ASSIGN_OR_RETURN(bool changed_registers,
+                         RemoveUnreadRegisters(block.get(), query_engine));
+    changed |= changed_registers;
+  }
+  return changed;
+}
+
+}  // namespace xls::codegen
