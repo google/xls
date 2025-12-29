b82dd9dbee35f2031017c8c7d3babfbd4362cce4
Mateusz Gancarz <mgancarz@antmicro.com>
2025-12-29 09:42:15 +0100

    Add cocotb tests
    Add cocotb testing utilities:
    - XLSStruct for easier handling and serializing/deserializing XLS structs
    - XLSChannel that serves as a dummy receiving channel
    - XLSMonitor that monitors transactions on an XLS channel
    - XLSDriver that can send data on an XLS channel
    - LatencyScoreboard that can measure latency between corresponding transactions on input and output buses
    - File-backed AXI memory python model
    
    Add cocotb tests for:
    - modules/zstd/memory/MemReader
    - modules/zstd/memory/AxiWriter
    - modules/zstd/memory/MemWriter
    
    Co-authored-by: Pawel Czarnecki <pczarnecki@antmicro.com>
    Co-authored-by: Robert Winkler <rwinkler@antmicro.com>
    Co-authored-by: Michal Czyz <mczyz@antmicro.com>
    Signed-off-by: Michal Czyz <mczyz@antmicro.com>
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>
    Signed-off-by: Krzysztof Obłonczek <koblonczek@antmicro.com>

---
+++ xls/modules/zstd/cocotb/utils.py
@@ -0,0 +1,57 @@
+# Copyright 2024 The XLS Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from pathlib import Path
+
+import cocotb
+from cocotb.runner import check_results_file, get_runner
+from cocotb.triggers import ClockCycles
+
+from xls.common import runfiles
+
+
+def setup_com_iverilog():
+  iverilog_path = Path(runfiles.get_path("iverilog", repository = "com_icarus_iverilog"))
+  vvp_path = Path(runfiles.get_path("vvp", repository = "com_icarus_iverilog"))
+  os.environ["PATH"] += os.pathsep + str(iverilog_path.parent)
+  os.environ["PATH"] += os.pathsep + str(vvp_path.parent)
+  build_dir = Path(os.environ['BUILD_WORKING_DIRECTORY'], "sim_build")
+  return build_dir
+
+def run_test(toplevel, test_module, verilog_sources):
+  build_dir = setup_com_iverilog()
+  runner = get_runner("icarus")
+  runner.build(
+    verilog_sources=verilog_sources,
+    hdl_toplevel=toplevel,
+    timescale=("1ns", "1ps"),
+    build_dir=build_dir,
+    defines={"SIMULATION": "1"},
+    waves=True,
+  )
+
+  results_xml = runner.test(
+     hdl_toplevel=toplevel,
+     test_module=test_module,
+     waves=True,
+  )
+  check_results_file(results_xml)
+
+@cocotb.coroutine
+async def reset(clk, rst, cycles=1):
+  """Cocotb coroutine that performs the reset"""
+  rst.value = 1
+  await ClockCycles(clk, cycles)
+  rst.value = 0
