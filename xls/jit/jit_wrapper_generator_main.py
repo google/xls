aa5b0d877fa2142cdd648bcadaf51c2c24ab10a9
David Plass <davidplass@google.com>
2026-03-03 12:35:27 -0800

    Split most of the code in jit_wrapper_generator_main into a library so we can more easily write unit tests for its functionality.
    PiperOrigin-RevId: 878077744

--- xls/jit/jit_wrapper_generator_main.py
+++ xls/jit/jit_wrapper_generator_main.py
@@ -18,8 +18,6 @@ Output source code is saved according to output_name
 """
 
 from collections.abc import Sequence
-import dataclasses
-import enum
 import itertools
 import subprocess
 from typing import Optional, TypeVar
@@ -30,8 +28,8 @@ import jinja2
 
 from xls.common import runfiles
 from xls.ir import xls_ir_interface_pb2 as ir_interface_pb2
-from xls.ir import xls_type_pb2 as type_pb2
 from xls.jit import aot_entrypoint_pb2
+from xls.jit import jit_wrapper_generator
 
 
 _FUNCTION_TYPE = flags.DEFINE_string(
@@ -109,380 +107,6 @@ _AOT_INFO = flags.DEFINE_string(
 )
 
 
-@dataclasses.dataclass(frozen=True)
-class XlsNamedValue:
-  """A Named & typed value for the wrapped function/proc."""
-
-  name: str
-  packed_type: str
-  unpacked_type: str
-  specialized_type: Optional[str]
-
-  @property
-  def value_arg(self):
-    return f"xls::Value {self.name}"
-
-  @property
-  def unpacked_arg(self):
-    return f"{self.unpacked_type} {self.name}"
-
-  @property
-  def packed_arg(self):
-    return f"{self.packed_type} {self.name}"
-
-  @property
-  def specialized_arg(self):
-    return f"{self.specialized_type} {self.name}"
-
-
-class JitType(enum.Enum):
-  FUNCTION = 1
-  PROC = 2
-  BLOCK = 3
-
-
-@dataclasses.dataclass(frozen=True)
-class XlsChannel:
-  xls_name: str
-  proc_name: str
-  camel_name: str
-  packed_type: str
-  unpacked_type: str
-  specialized_type: Optional[str]
-
-
-@dataclasses.dataclass(frozen=True)
-class XlsPort:
-  xls_name: str
-  camel_name: str
-  snake_name: str
-  bit_count: Optional[int]
-  specialized_type: Optional[str]
-
-
-@dataclasses.dataclass(frozen=True)
-class WrappedIr:
-  """A wrapped function."""
-
-  jit_type: JitType
-  ir_text: str
-  function_name: str
-  class_name: str
-  header_guard: str
-  header_filename: str
-  namespace: str
-  aot_entrypoint: Optional[aot_entrypoint_pb2.AotPackageEntrypointsProto]
-  # Function params and result.
-  params: Optional[Sequence[XlsNamedValue]] = None
-  result: Optional[XlsNamedValue] = None
-  # proc state and channels
-  incoming_channels: Optional[Sequence[XlsChannel]] = None
-  outgoing_channels: Optional[Sequence[XlsChannel]] = None
-  state: Optional[Sequence[XlsNamedValue]] = None
-  # block ports
-  input_ports: Optional[Sequence[XlsPort]] = None
-  output_ports: Optional[Sequence[XlsPort]] = None
-
-  @property
-  def can_be_specialized(self) -> bool:
-    return (
-        all(p.specialized_type is not None for p in self.params)
-        and self.result is not None
-        and self.result.specialized_type is not None
-    )
-
-  @property
-  def params_and_result(self):
-    return list(self.params) + [self.result]
-
-
-def to_packed(t: type_pb2.TypeProto) -> str:
-  the_type = t.type_enum
-  if the_type == type_pb2.TypeProto.BITS:
-    return f"xls::PackedBitsView<{t.bit_count}>"
-  elif the_type == type_pb2.TypeProto.TUPLE:
-    inner = ", ".join(to_packed(e) for e in t.tuple_elements)
-    return f"xls::PackedTupleView<{inner}>"
-  elif the_type == type_pb2.TypeProto.ARRAY:
-    return f"xls::PackedArrayView<{to_packed(t.array_element)}, {t.array_size}>"
-  elif the_type == type_pb2.TypeProto.TOKEN:
-    return "xls::PackedBitsView<0>"
-  raise app.UsageError(
-      "Incompatible with argument of type:"
-      f" {type_pb2.TypeProto.TypeEnum.Name(t.type_enum)}"
-  )
-
-
-def to_unpacked(t: type_pb2.TypeProto, mutable: bool = False) -> str:
-  """Get the unpacked c++ view type.
-
-  Args:
-    t: The xls type
-    mutable: Whether the type is mutable.
-
-  Returns:
-    the C++ unpacked view type
-  """
-  mutable_str = "Mutable" if mutable else ""
-  the_type = t.type_enum
-  if the_type == type_pb2.TypeProto.BITS:
-    return f"xls::{mutable_str}BitsView<{t.bit_count}>"
-  elif the_type == type_pb2.TypeProto.TUPLE:
-    inner = ", ".join(to_unpacked(e, mutable) for e in t.tuple_elements)
-    return f"xls::{mutable_str}TupleView<{inner}>"
-  elif the_type == type_pb2.TypeProto.ARRAY:
-    return (
-        f"xls::{mutable_str}ArrayView<{to_unpacked(t.array_element, mutable)},"
-        f" {t.array_size}>"
-    )
-  elif the_type == type_pb2.TypeProto.TOKEN:
-    return "xls::BitsView<0>"
-  raise app.UsageError(
-      "Incompatible with argument of type:"
-      f" {type_pb2.TypeProto.TypeEnum.Name(t.type_enum)}"
-  )
-
-
-def is_floating_point(
-    t: type_pb2.TypeProto, exponent_bits: int, mantissa_bits: int
-) -> bool:
-  return (
-      t.type_enum == type_pb2.TypeProto.TUPLE
-      and len(t.tuple_elements) == 3
-      and t.tuple_elements[0].type_enum == type_pb2.TypeProto.BITS
-      and t.tuple_elements[0].bit_count == 1
-      and t.tuple_elements[1].type_enum == type_pb2.TypeProto.BITS
-      and t.tuple_elements[1].bit_count == exponent_bits
-      and t.tuple_elements[2].type_enum == type_pb2.TypeProto.BITS
-      and t.tuple_elements[2].bit_count == mantissa_bits
-  )
-
-
-def is_double_tuple(t: type_pb2.TypeProto) -> bool:
-  return is_floating_point(t, 11, 52)
-
-
-def is_float_tuple(t: type_pb2.TypeProto) -> bool:
-  return is_floating_point(t, 8, 23)
-
-
-def to_specialized(
-    t: type_pb2.TypeProto, *, int_only: bool = False
-) -> Optional[str]:
-  """Get the specialized c++ type.
-
-  Args:
-    t: The xls type
-    int_only: If true only allow 'integer/bits' typed things to be specialized
-
-  Returns:
-    the C++ type
-  """
-  the_type = t.type_enum
-  if int_only and the_type != type_pb2.TypeProto.BITS:
-    return None
-  if the_type == type_pb2.TypeProto.BITS:
-    if t.bit_count <= 8:
-      return "uint8_t"
-    elif t.bit_count <= 16:
-      return "uint16_t"
-    elif t.bit_count <= 32:
-      return "uint32_t"
-    elif t.bit_count <= 64:
-      return "uint64_t"
-  elif is_double_tuple(t):
-    return "double"
-  elif is_float_tuple(t):
-    return "float"
-  elif the_type == type_pb2.TypeProto.ARRAY:
-    is_fp = is_float_tuple(t.array_element) or is_double_tuple(t.array_element)
-    is_int = (
-        t.array_element.type_enum == type_pb2.TypeProto.BITS
-        and t.array_element.bit_count in (8, 16, 32, 64)
-    )
-    # Need to use every bit to match alignment.
-    if is_fp or is_int:
-      return f"std::array<{to_specialized(t.array_element)}, {t.array_size}>"
-    return None
-  return None
-
-
-def to_chan(
-    c: ir_interface_pb2.PackageInterfaceProto.Channel, package_name: str
-) -> XlsChannel:
-  return XlsChannel(
-      xls_name=c.name,
-      proc_name=c.proc_name,
-      camel_name=camelize(c.name.removeprefix(f"{package_name}__")),
-      packed_type=to_packed(c.type),
-      unpacked_type=to_unpacked(c.type),
-      specialized_type=to_specialized(c.type),
-  )
-
-
-def to_param(
-    p: ir_interface_pb2.PackageInterfaceProto.NamedValue,
-) -> XlsNamedValue:
-  return XlsNamedValue(
-      name=p.name,
-      packed_type=to_packed(p.type),
-      unpacked_type=to_unpacked(p.type),
-      specialized_type=to_specialized(p.type),
-  )
-
-
-def to_port(
-    p: ir_interface_pb2.PackageInterfaceProto.NamedValue, package_name: str
-) -> XlsPort:
-  return XlsPort(
-      xls_name=p.name,
-      camel_name=camelize(p.name.removeprefix(f"{package_name}__")),
-      snake_name=p.name.removeprefix(f"{package_name}__"),
-      bit_count=p.type.bit_count
-      if p.type.type_enum == type_pb2.TypeProto.BITS
-      else None,
-      specialized_type=to_specialized(p.type, int_only=True),
-  )
-
-
-def interpret_function_interface(
-    ir: str,
-    func_ir: ir_interface_pb2.PackageInterfaceProto.Function,
-    class_name: str,
-    header_guard: str,
-    header_filename: str,
-    aot_info: aot_entrypoint_pb2.AotPackageEntrypointsProto,
-) -> WrappedIr:
-  """Fill in a WrappedIr for a function from the interface.
-
-  Args:
-    ir: package IR
-    func_ir: the particular function we want to wrap
-    class_name: The class name
-    header_guard: The header-guard string
-    header_filename: The header file name.
-    aot_info: The aot info for the function.
-
-  Returns:
-    A wrapped ir for the function.
-
-  Raises:
-    UsageError: If the aot info is for a different function.
-  """
-  if func_ir.base.name != aot_info.entrypoint[0].xls_function_identifier:
-    raise app.UsageError("Aot info is for a different function.")
-  params = [to_param(p) for p in func_ir.parameters]
-  result = XlsNamedValue(
-      name="result",
-      packed_type=to_packed(func_ir.result_type),
-      unpacked_type=to_unpacked(func_ir.result_type, mutable=True),
-      specialized_type=to_specialized(func_ir.result_type),
-  )
-  namespace = _WRAPPER_NAMESPACE.value
-  return WrappedIr(
-      jit_type=JitType.FUNCTION,
-      ir_text=ir,
-      function_name=func_ir.base.name,
-      class_name=class_name,
-      header_guard=header_guard,
-      header_filename=header_filename,
-      namespace=namespace,
-      params=params,
-      result=result,
-      aot_entrypoint=aot_info,
-  )
-
-
-def interpret_proc_interface(
-    ir: str,
-    package: ir_interface_pb2.PackageInterfaceProto,
-    proc_ir: ir_interface_pb2.PackageInterfaceProto.Proc,
-    class_name: str,
-    header_guard: str,
-    header_filename: str,
-    aot_info: aot_entrypoint_pb2.AotPackageEntrypointsProto,
-) -> WrappedIr:
-  """Fill in a WrappedIr for a proc from the interface.
-
-  Args:
-    ir: package IR
-    package: the package interface proto
-    proc_ir: the particular proc we want to wrap
-    class_name: The class name
-    header_guard: The header-guard string
-    header_filename: The header file name.
-    aot_info: The aot info for the function.
-
-  Returns:
-    A wrapped ir for the proc.
-
-  Raises:
-    UsageError: If the aot info does not contain the top proc.
-  """
-  top = proc_ir.base.name
-  checks = (v.xls_function_identifier != top for v in aot_info.entrypoint)
-  if all(checks):
-    raise app.UsageError(
-        f"AOT info does not contain an entry for top proc {proc_ir.base.name}"
-    )
-  state = [to_param(p.name) for p in proc_ir.state_values]
-  input_channels = [to_chan(p, package.name) for p in package.channels]
-  output_channels = [to_chan(p, package.name) for p in package.channels]
-  return WrappedIr(
-      jit_type=JitType.PROC,
-      ir_text=ir,
-      function_name=proc_ir.base.name,
-      class_name=class_name,
-      header_guard=header_guard,
-      header_filename=header_filename,
-      namespace=_WRAPPER_NAMESPACE.value,
-      incoming_channels=input_channels,
-      outgoing_channels=output_channels,
-      state=state,
-      aot_entrypoint=aot_info,
-  )
-
-
-def interpret_block_interface(
-    ir: str,
-    package: ir_interface_pb2.PackageInterfaceProto,
-    block_ir: ir_interface_pb2.PackageInterfaceProto.Block,
-    class_name: str,
-    header_guard: str,
-    header_filename: str,
-    aot_info: aot_entrypoint_pb2.AotPackageEntrypointsProto,
-) -> WrappedIr:
-  """Fill in a WrappedIr for a block from the interface.
-
-  Args:
-    ir: package IR
-    package: the package interface proto
-    block_ir: the particular block we want to wrap
-    class_name: The class name
-    header_guard: The header-guard string
-    header_filename: The header file name.
-    aot_info: The aot info for the function.
-
-  Returns:
-    A wrapped ir for the block.
-  """
-  input_ports = [to_port(p, package.name) for p in block_ir.input_ports]
-  output_ports = [to_port(p, package.name) for p in block_ir.output_ports]
-  namespace = _WRAPPER_NAMESPACE.value
-  return WrappedIr(
-      jit_type=JitType.BLOCK,
-      ir_text=ir,
-      function_name=block_ir.base.name,
-      class_name=class_name,
-      header_guard=header_guard,
-      header_filename=header_filename,
-      namespace=namespace,
-      input_ports=input_ports,
-      output_ports=output_ports,
-      aot_entrypoint=aot_info,
-  )
-
-
 _T = TypeVar("_T")
 
 
@@ -505,7 +129,7 @@ def interpret_interface(
     class_name: str,
     function_name: str,
     aot_info: aot_entrypoint_pb2.AotPackageEntrypointsProto,
-) -> WrappedIr:
+) -> jit_wrapper_generator.WrappedIr:
   """Create a wrapped-ir representation of the IR to be rendered to source.
 
   Args:
@@ -537,19 +161,20 @@ def interpret_interface(
         interface.name,
     )
     if func_ir is not None:
-      return interpret_function_interface(
+      return jit_wrapper_generator.interpret_function_interface(
           ir,
           func_ir,
           class_name,
           header_guard,
           header_filename,
           aot_info,
+          _WRAPPER_NAMESPACE.value,
       )
   # Try to find a proc
   if _FUNCTION_TYPE.value in (None, "PROC"):
     proc_ir = find_named_entry(interface.procs, function_name, interface.name)
     if proc_ir is not None:
-      return interpret_proc_interface(
+      return jit_wrapper_generator.interpret_proc_interface(
           ir,
           interface,
           proc_ir,
@@ -557,12 +182,13 @@ def interpret_interface(
           header_guard,
           header_filename,
           aot_info,
+          _WRAPPER_NAMESPACE.value,
       )
   # Try to find a block
   if _FUNCTION_TYPE.value in (None, "BLOCK"):
     block_ir = find_named_entry(interface.blocks, function_name, interface.name)
     if block_ir is not None:
-      return interpret_block_interface(
+      return jit_wrapper_generator.interpret_block_interface(
           ir,
           interface,
           block_ir,
@@ -570,6 +196,7 @@ def interpret_interface(
           header_guard,
           header_filename,
           aot_info,
+          _WRAPPER_NAMESPACE.value,
       )
   raise app.UsageError(
       f"No function/proc/block called {function_name} in"
@@ -588,30 +215,26 @@ def top_name(ir: ir_interface_pb2.PackageInterfaceProto) -> str:
   raise app.UsageError("--function required if top is not set.")
 
 
-def camelize(name: str) -> str:
-  return name.title().replace("_", "")
-
-
 _CC_TEMPLATES = {
-    JitType.FUNCTION: runfiles.get_contents_as_text(
+    jit_wrapper_generator.JitType.FUNCTION: runfiles.get_contents_as_text(
         "xls/jit/jit_function_wrapper_cc.tmpl"
     ),
-    JitType.PROC: runfiles.get_contents_as_text(
+    jit_wrapper_generator.JitType.PROC: runfiles.get_contents_as_text(
         "xls/jit/jit_proc_wrapper_cc.tmpl"
     ),
-    JitType.BLOCK: runfiles.get_contents_as_text(
+    jit_wrapper_generator.JitType.BLOCK: runfiles.get_contents_as_text(
         "xls/jit/jit_block_wrapper_cc.tmpl"
     ),
 }
 
 _H_TEMPLATES = {
-    JitType.FUNCTION: runfiles.get_contents_as_text(
+    jit_wrapper_generator.JitType.FUNCTION: runfiles.get_contents_as_text(
         "xls/jit/jit_function_wrapper_h.tmpl"
     ),
-    JitType.PROC: runfiles.get_contents_as_text(
+    jit_wrapper_generator.JitType.PROC: runfiles.get_contents_as_text(
         "xls/jit/jit_proc_wrapper_h.tmpl"
     ),
-    JitType.BLOCK: runfiles.get_contents_as_text(
+    jit_wrapper_generator.JitType.BLOCK: runfiles.get_contents_as_text(
         "xls/jit/jit_block_wrapper_h.tmpl"
     ),
 }
@@ -639,7 +262,9 @@ def main(argv: Sequence[str]) -> None:
       _FUNCTION.value if _FUNCTION.value else top_name(ir_interface)
   ).removeprefix(f"__{ir_interface.name}__")
   class_name = (
-      _CLASS_NAME.value if _CLASS_NAME.value else camelize(function_name)
+      _CLASS_NAME.value
+      if _CLASS_NAME.value
+      else jit_wrapper_generator.camelize(function_name)
   )
   output_name = _OUTPUT_NAME.value if _OUTPUT_NAME.value else function_name
   wrapped = interpret_interface(
