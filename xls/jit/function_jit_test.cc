2fcc32ccbe376e9842b180ff9018ef1d91127d32
XLS Team <xls-github-bot@google.com>
2026-08-12 14:25:45 -0700

    [xls] Fix buffer overflow in function_jit_test for wide BitsView.
    For 80-bit BitsView, the internal storage type is absl::uint128 (16 bytes). GetValue() reads 16 bytes via reinterpret_cast. If the backing buffer is only 10 bytes, this is an out-of-bounds read. Increase test buffers to 16 bytes.
    
    PiperOrigin-RevId: 963660281

--- xls/jit/function_jit_test.cc
+++ xls/jit/function_jit_test.cc
@@ -402,8 +402,8 @@ TEST(FunctionJitTest, PackedAndUnpackedSmokeWide) {
 
   // 80-bit data is represented as an i128 with 16 byte alignment.
   // TODO(allight): 2023-11-30: The fact this is needed is unfortunate.
-  alignas(16)
-      uint8_t input_data[] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa};
+  alignas(16) uint8_t input_data[16] = {0x1, 0x2, 0x3, 0x4, 0x5,
+                                        0x6, 0x7, 0x8, 0x9, 0xa};
   {
     uint8_t output_data[10];
     PackedBitsView<80> input(input_data, 0);
@@ -416,11 +416,12 @@ TEST(FunctionJitTest, PackedAndUnpackedSmokeWide) {
   {
     // 80-bit data is represented as an i128 with 16 byte alignment.
     // TODO(allight): 2023-11-30: The fact this is needed is unfortunate.
-    alignas(16) uint8_t output_data[10];
+    alignas(16) uint8_t output_data[16];
     BitsView<80> input(input_data);
     MutableBitsView<80> output(output_data);
     XLS_ASSERT_OK(jit->RunWithUnpackedViews(input, output));
-    EXPECT_THAT(output_data,
+    // Check only the first 10 bytes (80 bits).
+    EXPECT_THAT(absl::MakeSpan(output_data, 10),
                 ElementsAre(0x2, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa));
   }
 }
