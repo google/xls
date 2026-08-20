f4beb0bd2ad7f0fe79143786705a8b59da6efd5d
David Plass <davidplass@google.com>
2026-08-20 13:10:41 -0700

    [Proc tests] Create CreateTypeAnnotation, which will be used to "promote" local variables in a function-based test to an impl-based proc field.
    PiperOrigin-RevId: 968009577

---
+++ xls/dslx/frontend/type_to_type_annotation.h
@@ -0,0 +1,31 @@
+// Copyright 2026 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef XLS_DSLX_FRONTEND_TYPE_TO_TYPE_ANNOTATION_H_
+#define XLS_DSLX_FRONTEND_TYPE_TO_TYPE_ANNOTATION_H_
+
+#include "absl/status/statusor.h"
+#include "xls/dslx/frontend/ast.h"
+#include "xls/dslx/frontend/pos.h"
+#include "xls/dslx/type_system/type.h"
+
+namespace xls::dslx {
+
+// Creates a type annotation for the given type in the module.
+absl::StatusOr<TypeAnnotation*> CreateTypeAnnotation(Module& new_module,
+                                                     const Type& type,
+                                                     const Span& span);
+}  // namespace xls::dslx
+
+#endif  // XLS_DSLX_FRONTEND_TYPE_TO_TYPE_ANNOTATION_H_
