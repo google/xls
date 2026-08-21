b943172eb08e479ed62326ba1a795a84b4148fad
XLS Team <xls-github-bot@google.com>
2026-08-21 07:09:51 -0700

    Fix delay estimation for foreign function invocations
    Fix `FfiDelayEstimator` to retrieve `delay_ps` from the callee function being
    invoked rather than the caller/enclosing function and add unit tests.
    
    PiperOrigin-RevId: 968481489

--- xls/estimators/delay_model/ffi_delay_estimator.cc
+++ xls/estimators/delay_model/ffi_delay_estimator.cc
@@ -19,7 +19,9 @@
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
+#include "xls/ir/function.h"
 #include "xls/ir/function_base.h"
+#include "xls/ir/nodes.h"
 #include "xls/ir/op.h"
 
 namespace xls {
@@ -35,9 +37,11 @@ absl::StatusOr<int64_t> FfiDelayEstimator::GetOperationDelayInPs(
   // If the user does not want to provide one, they can use the value from
   // the --ffi_fallback_delay_ps flag. However, this user-provided value
   // overrides the value from the --ffi_fallback_delay_ps flag.
-  if (node->function_base()->ForeignFunctionData().has_value() &&
-      node->function_base()->ForeignFunctionData()->has_delay_ps()) {
-    return node->function_base()->ForeignFunctionData()->delay_ps();
+  const Invoke* invoke = node->As<Invoke>();
+  Function* callee = invoke->to_apply();
+  if (callee->ForeignFunctionData().has_value() &&
+      callee->ForeignFunctionData()->has_delay_ps()) {
+    return callee->ForeignFunctionData()->delay_ps();
   }
   if (!fallback_delay_estimate_.has_value()) {
     return absl::NotFoundError("No --ffi_fallback_delay_ps provided.");
