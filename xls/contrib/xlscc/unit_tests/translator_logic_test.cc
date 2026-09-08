7d3d33f1fc8a0592edad706c63c63c2e411e9206
Sean Purser-Haskell <seanhaskell@google.com>
2026-09-08 12:59:28 -0700

    Infer unrolling for loops nested in unrolled loops
    PiperOrigin-RevId: 978069597

--- xls/contrib/xlscc/unit_tests/translator_logic_test.cc
+++ xls/contrib/xlscc/unit_tests/translator_logic_test.cc
@@ -4138,10 +4138,7 @@ TEST_F(TranslatorLogicTest, NonPragmaNestedLoop) {
       return a;
     })";
 
-  ASSERT_THAT(SourceToIr(content).status(),
-              absl_testing::StatusIs(
-                  absl::StatusCode::kUnimplemented,
-                  testing::HasSubstr("missing #pragma or attribute")));
+  Run({{"a", 20}}, 120, content);
 }
 
 TEST_F(TranslatorLogicTest, Label) {
