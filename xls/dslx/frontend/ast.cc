xlsynth/xlsynth

dslx: typecheck semantic sum construction
Adds semantic sum construction and typechecking support for Phase 1.

xls/dslx/frontend/ast.cc
@@ -1163,11 +1163,13 @@ TypeAnnotation::~TypeAnnotation() = default;
 TypeRefTypeAnnotation::TypeRefTypeAnnotation(
     Module* owner, Span span, TypeRef* type_ref,
     std::vector<ExprOrType> parametrics,
-    std::optional<const StructInstanceBase*> instantiator)
+    std::optional<const StructInstanceBase*> instantiator,
+    std::optional<const Expr*> sum_instantiator)
     : TypeAnnotation(owner, std::move(span), kAnnotationKind),
       type_ref_(type_ref),
       parametrics_(std::move(parametrics)),
-      instantiator_(instantiator) {}
+      instantiator_(instantiator),
+      sum_instantiator_(sum_instantiator) {}
 
 TypeRefTypeAnnotation::~TypeRefTypeAnnotation() = default;
 

xls/dslx/type_system_v2/type_annotation_utils.h
@@ -39,6 +39,13 @@ struct StructOrProcRef {
   std::optional<const TypeRefTypeAnnotation*> type_ref_type_annotation;
 };
 
+struct SumRef {
+  const SumDef* def;
+  std::vector<ExprOrType> parametrics;
+  std::optional<const Expr*> instantiator;
+  std::optional<const TypeRefTypeAnnotation*> type_ref_type_annotation;
+};
+
 // The signedness and bit count extracted from a `TypeAnnotation`. The
 // `TypeAnnotation` may use primitive values or exprs; we convey the
 // representation as is.
@@ -105,6 +112,15 @@ TypeAnnotation* CreateStructOrProcAnnotation(
 // Variant that converts a `StructOrProcRef` into an annotation.
 TypeAnnotation* CreateStructOrProcAnnotation(Module& module,
                                              const StructOrProcRef& ref);
+
+// Creates an annotation referring to the given sum definition with the given
+// parametric arguments.
+TypeAnnotation* CreateSumAnnotation(
+    Module& module, SumDef* def, std::vector<ExprOrType> parametrics,
+    std::optional<const Expr*> instantiator = std::nullopt);
+
+// Variant that converts a `SumRef` into an annotation.
+TypeAnnotation* CreateSumAnnotation(Module& module, const SumRef& ref);
 
 // Returns the element channel type of the given channel array type.
 ChannelTypeAnnotation* GetChannelArrayElementType(

google/xls

xls/dslx/frontend/ast.cc
@@ -1166,11 +1166,13 @@ TypeAnnotation::~TypeAnnotation() = default;
 TypeRefTypeAnnotation::TypeRefTypeAnnotation(
     Module* owner, Span span, TypeRef* type_ref,
     std::vector<ExprOrType> parametrics,
-    std::optional<const StructInstanceBase*> instantiator)
+    std::optional<const StructInstanceBase*> instantiator,
+    std::optional<const SumInstance*> sum_instantiator)
     : TypeAnnotation(owner, std::move(span), kAnnotationKind),
       type_ref_(type_ref),
       parametrics_(std::move(parametrics)),
-      instantiator_(instantiator) {}
+      instantiator_(instantiator),
+      sum_instantiator_(sum_instantiator) {}
 
 TypeRefTypeAnnotation::~TypeRefTypeAnnotation() = default;
 

xls/dslx/type_system_v2/type_annotation_utils.h
@@ -45,6 +45,12 @@ struct StructOrProcRef {
   bool is_generic;
 };
 
+struct SumRef {
+  const SumDef* def;
+  std::vector<ExprOrType> parametrics;
+  std::optional<const SumInstance*> instantiator;
+};
+
 // The signedness and bit count extracted from a `TypeAnnotation`. The
 // `TypeAnnotation` may use primitive values or exprs; we convey the
 // representation as is.
@@ -111,6 +117,15 @@ TypeAnnotation* CreateStructOrProcAnnotation(
 // Variant that converts a `StructOrProcRef` into an annotation.
 TypeAnnotation* CreateStructOrProcAnnotation(Module& module,
                                              const StructOrProcRef& ref);
+
+// Creates an annotation referring to the given sum definition with the given
+// parametric arguments.
+TypeAnnotation* CreateSumAnnotation(
+    Module& module, SumDef* def, std::vector<ExprOrType> parametrics,
+    std::optional<const SumInstance*> instantiator = std::nullopt);
+
+// Variant that converts a `SumRef` into an annotation.
+TypeAnnotation* CreateSumAnnotation(Module& module, const SumRef& ref);
 
 // Returns the element channel type of the given channel array type.
 ChannelTypeAnnotation* GetChannelArrayElementType(